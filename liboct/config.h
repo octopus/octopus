420620f555d18ce1b5a375602853881a7af14c0d
Miguel Marques <marques@tddft.org>
2002-03-09 01:24:23 +0000

    Build was still not OK. Let us see if it improves now!

--- liboct/config.h
+++
@@ -1,8 +0,0 @@
-/* liboct/config.h.  Generated automatically by configure.  */
-#define HAVE_GSL
-
-/* #undef FORTRANIZE_LOWERCASE */
-#define FORTRANIZE_LOWERCASE_UNDERSCORE 1
-/* #undef FORTRANIZE_UPPERCASE */
-/* #undef FORTRANIZE_UPPERCASE_UNDERSCORE */
-/* #undef FORTRANIZE_EXTRA_UNDERSCORE */
