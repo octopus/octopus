df641aa800ae1c7cdb3b4f8592f32c289b1ba4b2
Miguel Marques <marques@tddft.org>
2002-07-23 11:42:05 +0000

    now this functional seems to work :)

--- src/xc_HJU.F90
+++ src/xc_HJU.F90
@@ -24,72 +24,65 @@ subroutine R_FUNC(kli_hju) (m, st, hartr, type, Vx, ex)
   integer, intent(in) :: type
   real(r8), intent(out) :: Vx(m%np, st%nspin), ex
 
-  integer :: ia, is, i, k, i1, i2
+  integer :: ia, is, i, k
   real(r8) :: ex2, N_alpha
 
-  real(r8), allocatable :: rho2(:,:), Vx2(:, :)
-  real(r8), allocatable, target :: rho(:,:)
-  real(r8), pointer :: p(:,:)
+  real(r8), allocatable :: rho(:,:), Vx2(:, :)
 
-  allocate(rho(m%np, 2), rho2(m%np, 2), Vx2(m%np, 2))
+  allocate(rho(m%np, 2), Vx2(m%np, 2))
 
-  ! first we get the true density
-  do is = 1, st%nspin
-     do k = 1, m%np
-        !rho(k, is) = sum(st%occ(:, is)*R_ABS(st%R_FUNC(psi) (k, 1, :, is))**2)
-       rho(k, is) = st%rho(k, is)
-     end do
-  end do
+  ! save rho
+  rho = st%rho
 
+  ex = 0._r8
   call R_FUNC(xc_lda) (type, m, st, Vx, ex)
 
-  ! now store some stuff
-  i1 = st%ispin; st%ispin = 2
-  i2 = st%nspin; st%nspin = 2
-  p  =>st%rho;   st%rho   =>rho
-
-  rho2(:, 2) = 0._r8
+  st%rho(:, 2) = 0._r8
   do is = 1, st%nspin
     do ia = 1, 2 ! loop over atoms (only dimers! ;)
 
-      rho2(:, 1) = 0._r8
+      st%rho(:, 1) = 0._r8
       do k = 1, m%np
         if((ia == 1.and.m%Lz(k)>0).or.(ia == 2.and.m%Lz(k)<0)) then
-          rho2(k, 1) = rho(k, is)
+          st%rho(k, 1) = rho(k, is)
         end if
-        if(m%Lz(k) == 0) rho2(k, 1) = 0.5_r8*rho(k, is)
+        if(m%Lz(k) == 0) st%rho(k, 1) = 0.5_r8*rho(k, is)
       end do
-      N_alpha = dmesh_integrate(m, rho2(:, 1))
+      N_alpha = dmesh_integrate(m, st%rho(:, 1))
 
       ! first the lda term
       Vx2 = 0.0_r8; Ex2 = 0.0_r8
-      call R_FUNC(xc_lda) (type, m, st, Vx2, ex2)
+      call R_FUNC(xc_lda) (type, m, st, Vx2, Ex2)
       
       ! The LDA is local, so we can just add the whole array
       Vx(1:m%np, is) = Vx(1:m%np, is) - N_alpha*Vx2(1:m%np, 1)
-      Ex = Ex - N_alpha*ex2
+      Ex = Ex - N_alpha*Ex2
       
       ! should we add the SI Hartree correction?
       if(type == X_FUNC_LDA_NREL) then
-        call hartree_solve(hartr, m, Vx2(:, 1), rho(:, 1:1))
+        write(message(1), '(a,i2,a,f14.6)') 'N(', is, ') = ', N_alpha
+        call write_info(1)
+
+        call hartree_solve(hartr, m, Vx2(:, 1), st%rho(:, 1:1))
+        Ex2 = 0._r8
         do k = 1, m%np
           if((ia == 1.and.m%Lz(k)>=0).or.(ia == 2.and.m%Lz(k)<=0)) then
             Vx(k, is) = Vx(k, is) - N_alpha*Vx2(k, 1)
+            Ex2 = Ex2 + Vx2(k, 1)*st%rho(k, 1)
           end if
         end do
         
-        Ex = Ex - 0.5_r8*N_alpha*sum(Vx2(1:m%np, 1)*rho2(1:m%np, 1))*m%vol_pp
+        Ex = Ex - 0.5_r8*N_alpha*Ex2*m%vol_pp
       end if
 
     end do
   end do
 
-  ! restore state variables
-  st%ispin = i1; st%nspin = i2
-  st%rho => p
+  ! restore rho
+  st%rho = rho
 
   ! deallocate the rest of the vars..
-  deallocate(rho, rho2, Vx2)
+  deallocate(rho, Vx2)
 
   return
 end subroutine R_FUNC(kli_hju)
