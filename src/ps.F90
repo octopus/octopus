611d62b783fe2137b815d258dfee136d056c345b
Alberto Castro <acastro@bifi.es>
2003-03-03 18:50:07 +0000

    Bug fix. Description: wrong limits defining the spin-orbit coupling constants. Fortunately you warned me you had changed that! I don't know what the ifc complained about, but this is the way it should be. Anyway, before code release I will try to clean up a bit more that part.

--- src/ps.F90
+++ src/ps.F90
@@ -97,7 +97,7 @@ subroutine ps_init(ps, label, flavour, z, lmax, lloc, ispin)
     ps%l_max = min(pstm%npotd - 1, lmax)   ! Maybe the file has not enough components.
     ps%l_max_occ = ps%l_max
     ps%l_loc = lloc
-    ps%so_l_max = min(pstm%npotu - 1, lmax) ! is this the corect value?
+    ps%so_l_max = min(pstm%npotu, lmax) ! is this the corect value?
     ps%z = z
     call tm_process(pstm, lmax, lloc)
     if(conf%verbose > 999) call tm_debug(pstm)
@@ -335,8 +335,10 @@ subroutine tm_load(ps, pstm)
   ps%dknrm (0:ps%l_max) = pstm%dknrm (0:ps%l_max)
 
   if(ps%so_l_max >= 0) then
-    ps%k(0:ps%so_l_max, 1, 1) = pstm%so_dkbcos(1:ps%so_l_max+1)
-    ps%so_dknrm(0:ps%so_l_max) = pstm%so_dknrm(1:ps%so_l_max+1)
+    !ps%k(0:ps%so_l_max, 1, 1) = pstm%so_dkbcos(1:ps%so_l_max+1)
+    !ps%so_dknrm(0:ps%so_l_max) = pstm%so_dknrm(1:ps%so_l_max+1)
+    ps%k(1:ps%so_l_max, 1, 1) = pstm%so_dkbcos(1:ps%so_l_max)
+    ps%so_dknrm(1:ps%so_l_max) = pstm%so_dknrm(1:ps%so_l_max)
   end if
 
   ! Increasing radius a little, just in case.
