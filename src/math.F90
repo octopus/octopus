a13b511c52dd28b34e357bdfc2c5bb256eadd52c
Carlo Andrea Rozzi <carloandrea.rozzi@nano.cnr.it>
2004-03-24 14:27:44 +0000

    added shosrtcuts to the fourier space cutoff functions

--- src/math.F90
+++ src/math.F90
@@ -291,6 +291,54 @@ subroutine weights(N, M, cc)
 
 end subroutine weights
 
+function cutoff0(x)
+  FLOAT, intent(in) ::  x
+  FLOAT :: cutoff0
+
+    cutoff0 = M_ONE - cos(x)
+
+end function cutoff0
+
+function cutoff1(x, p)
+  FLOAT, intent(in) ::  x, p
+  FLOAT :: cutoff1
+
+  if ( x == M_ZERO ) then
+    cutoff1 = M_ZERO
+  else
+    cutoff1 = M_ONE + p*loct_bessel_j1(p)*loct_bessel_k0(x) &
+                    - x*loct_bessel_j0(p)*loct_bessel_k1(x)
+  end if
+
+end function cutoff1
+
+function cutoff2(p, z)
+  FLOAT, intent(in) ::  p, z
+  FLOAT :: cutoff2
+
+  if ( p == M_ZERO ) then
+    cutoff2 = M_ZERO
+  else
+    cutoff2 = M_ONE + exp(-p)*(z*sin(z)/p-cos(z))
+  end if          
+
+end function cutoff2
+
+!function phaseshift(perdim, ix)
+!  FLOAT, intent(in) ::  perdim, ix(3)
+!  CMPLX :: phaseshift
+!  INTEGER :: nhalf
+
+!  if (  ) then
+!    nhalf = floor()
+!  else
+!    nhalf = ceiling()
+!  end if          
+!  phaseshift = exp(M_PI*M_ZI*sum(ixx(:)))
+!  phaseshift = cmplx(nhalf)
+
+!end function phaseshift
+
 #include "undef.F90"
 #include "complex.F90"
 #include "math_inc.F90"
