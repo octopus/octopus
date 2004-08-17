2834217f0bc4242f78d94979b67627d7d71ef6f1
Miguel Marques <marques@tddft.org>
2004-08-17 21:26:01 +0000

    implemented the variational laplacian of Maragakis et al.

--- src/derivatives.F90
+++ src/derivatives.F90
@@ -41,8 +41,9 @@ module derivatives
      DER_BC_ZERO_DF = 1,   &  ! first derivative of the function is zero
      DER_BC_PERIOD  = 2       ! boundary is periodic
 
-  integer, parameter :: &
-     DER_STAR = 0
+  integer, parameter ::    &
+     DER_STAR        = 0,  &
+     DER_VARIATIONAL = 1
 
 contains
 
@@ -124,7 +125,7 @@ contains
     
     ! get size of stencil
     select case(der%stencil_type)
-    case(DER_STAR)
+    case(DER_STAR, DER_VARIATIONAL)
       n = 2*conf%dim*der%order + 1
     end select
     
@@ -134,7 +135,7 @@ contains
     
     ! create stencil
     select case(der%stencil_type)
-    case(DER_STAR)
+    case(DER_STAR, DER_VARIATIONAL)
       call star(der%lapl%stencil)
     end select
 
@@ -171,7 +172,7 @@ contains
     
     ! get size of stencil
     select case(der%stencil_type)
-    case(DER_STAR)
+    case(DER_STAR, DER_VARIATIONAL)
       n = 2*der%order + 1
     end select
     
@@ -182,7 +183,7 @@ contains
 
       ! create stencil
       select case(der%stencil_type)
-      case(DER_STAR)
+      case(DER_STAR, DER_VARIATIONAL)
         call star(i, der%grad(i)%stencil)
       end select
     end do
@@ -226,12 +227,14 @@ contains
     case(DER_STAR)  
       call nl_operator_build(der%m, der%lapl, der%m%np, .true.)
       call star(der%lapl)
+    case(DER_VARIATIONAL)
+      call nl_operator_build(der%m, der%lapl, der%m%np, .true.)
+      call variational(der%lapl)
     end select
 
   contains
     subroutine star(lapl)
       type(nl_operator_type), intent(inout) :: lapl
-
 
       integer :: k, i, j, morder
       FLOAT, allocatable :: cc(:,:,:)
@@ -257,8 +260,73 @@ contains
       end do
 
       deallocate(cc)
-   end subroutine star
+    end subroutine star
 
+    ! ----------------------------------------
+    ! Implements the variational discretization of the laplacian
+    ! as proposed by P. Maragakis, J. Soler, and E. Kaxiras, PRB 64, 193101 (2001)
+    ! ----------------------------------------
+    subroutine variational(lapl)
+      type(nl_operator_type), intent(inout) :: lapl
+
+      integer :: i, j, k
+      FLOAT, parameter   :: pi2 = M_PI**2
+      FLOAT, allocatable :: fp(:)
+
+      allocate(fp(der%order+1))
+      select case(der%order)
+      case(1) 
+        fp(1) = -pi2/M_TWO
+        fp(2) =  pi2/M_FOUR
+      case(2) 
+        fp(1) = -M_HALF-M_THREE*pi2/M_EIGHT
+        fp(2) =  pi2/M_FOUR
+        fp(3) =  M_ONE/M_FOUR - pi2/CNST(16.0)
+      case(3)
+        fp(1) = -M_FIVE/M_SIX - M_FIVE*pi2/CNST(16.0)
+        fp(2) =  M_ONE/CNST(12.0) + CNST(15.0)*pi2/CNST(64.0)
+        fp(3) =  M_FIVE/CNST(12.0) - M_THREE*pi2/CNST(32.0)
+        fp(4) = -M_ONE/CNST(12.0) + pi2/CNST(64.0)
+      case(4)
+        fp(1) = -CNST(77.0)/CNST(72.0) - CNST(35.0)*pi2/CNST(128.0)
+        fp(2) =  M_EIGHT/CNST(45.0) + M_SEVEN*pi2/CNST(32.0)
+        fp(3) =  CNST(23.0)/CNST(45.0) - M_SEVEN*pi2/CNST(64.0)
+        fp(4) = -M_EIGHT/CNST(45.0) + pi2/CNST(32.0)
+        fp(5) =  CNST(17.0)/CNST(720.0) - pi2/CNST(256.0)
+      case(5)
+        fp(1) = -CNST(449.0)/CNST(360.0) - CNST(63.0)*pi2/CNST(256.0)
+        fp(2) =  M_FOUR/CNST(15.0) + CNST(105.0)*pi2/CNST(512.0)
+        fp(3) =  CNST(59.0)/CNST(105.0) - CNST(15.0)*pi2/CNST(128.0)
+        fp(4) = -CNST(82.0)/CNST(315.0) + CNST(45.0)*pi2/CNST(1024.0)
+        fp(5) =  CNST(311.0)/CNST(5040.0) - M_FIVE*pi2/CNST(512.0)
+        fp(6) = -M_TWO/CNST(315.0) + pi2/CNST(1024.0)
+      case(6)
+        fp(1) = -CNST(2497.0)/CNST(1800.0) - CNST(231.0)*pi2/CNST(1024.0)
+        fp(2) =  CNST(26.0)/CNST(75.0) + CNST(99.0)*pi2/CNST(512.0)
+        fp(3) =  CNST(493.0)/CNST(840.0) - CNST(495.0)*pi2/CNST(4096.0)
+        fp(4) = -CNST(103.0)/CNST(315.0) + CNST(55.0)*pi2/CNST(1024.0)
+        fp(5) =  CNST(2647.0)/CNST(25200.0) - CNST(33.0)*pi2/CNST(2048.0)
+        fp(6) = -CNST(31.0)/CNST(1575.0) + M_THREE*pi2/CNST(1024.0)
+        fp(7) =  M_ONE/CNST(600.0) - pi2/CNST(4096.0)
+      end select
+
+      lapl%w(1,1) = fp(1)*sum(1/der%m%h(1:conf%dim)**2)
+
+      k = 1
+      do i = 1, conf%dim
+        do j = -der%order, -1
+          k = k + 1
+          lapl%w(k,1) = fp(-j+1) / der%m%h(i)**2
+        end do
+
+        do j = 1, der%order
+          k = k + 1
+          lapl%w(k,1) = fp( j+1) / der%m%h(i)**2
+        end do
+      end do
+    
+      deallocate(fp)
+    end subroutine variational
   end subroutine derivatives_build_lapl
 
   ! ---------------------------------------------------------
@@ -271,7 +339,7 @@ contains
 
     do i = 1, conf%dim
       select case(der%stencil_type)
-      case(DER_STAR)  
+      case(DER_STAR, DER_VARIATIONAL)  
         call nl_operator_build(der%m, der%grad(i), der%m%np, .true.)
         call star(der%grad(i))
       end select
