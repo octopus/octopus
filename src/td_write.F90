648dc5daaf41ba23af851621e458bc251f3b711a
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2004-12-03 16:02:23 +0000

    *) I do not know why, but this change solved a segfault.

--- src/td_write.F90
+++ src/td_write.F90
@@ -32,40 +32,40 @@ subroutine td_write_spin(out, m, st, td, iter)
   call zstates_calculate_magnetization(m, st, spin)
   spin = M_HALF*spin
 
-  if(mpiv%node.ne.0) then ! only first node outputs
-    call pop_sub(); return
+  if(mpiv%node == 0) then ! only first node outputs
+
+    if(iter ==0) then
+      !empty file
+      call write_iter_clear(out)
+
+      !fist line -> now unused.
+      write(aux, '(a)') '#'
+      call write_iter_string(out, aux)
+      call write_iter_nl(out)
+
+      !second line -> columns name
+      call write_iter_header_start(out)
+      if (st%d%ispin == SPINORS) then
+        write(aux, '(a2,18x)') 'Sx'
+        call write_iter_header(out, aux)
+        write(aux, '(a2,18x)') 'Sy'
+        call write_iter_header(out, aux)
+      end if
+      write(aux, '(a2,18x)') 'Sz'
+      call write_iter_header(out, aux)
+      call write_iter_nl(out)
+    endif
+
+    call write_iter_start(out)
+    select case (st%d%ispin)
+    case (SPIN_POLARIZED)
+      call write_iter_double(out, spin(3), 1)
+    case (SPINORS)
+      call write_iter_double(out, spin(1:3), 3)
+    end select
+    call write_iter_nl(out)
+
   end if
-
-  if(iter ==0) then
-    !empty file
-    call write_iter_clear(out)
-
-    !fist line -> now unused.
-    write(aux, '(a)') '#'
-    call write_iter_string(out, aux)
-    call write_iter_nl(out)
-
-    !second line -> columns name
-    call write_iter_header_start(out)
-    if (st%d%ispin == SPINORS) then
-      write(aux, '(a2,18x)') 'Sx'
-      call write_iter_header(out, aux)
-      write(aux, '(a2,18x)') 'Sy'
-      call write_iter_header(out, aux)
-    end if
-    write(aux, '(a2,18x)') 'Sz'
-    call write_iter_header(out, aux)
-    call write_iter_nl(out)
-  endif
-
-  call write_iter_start(out)
-  select case (st%d%ispin)
-  case (SPIN_POLARIZED)
-    call write_iter_double(out, spin(3), 1)
-  case (SPINORS)
-    call write_iter_double(out, spin(1:3), 3)
-  end select
-  call write_iter_nl(out)
 
   call pop_sub()
 end subroutine td_write_spin
