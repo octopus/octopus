55bfae596dc1731d3615a36ff6b2af8be98284a3
Heiko Appel <appel@fhi-berlin.mpg.de>
2005-05-05 12:54:19 +0000

    This is a basic infrastructure for Octopus regression testing. I've written a man page for the Perl script that controls the regression tests. After a make in the testsuite direcory you can read the man page with "man -l run_regression.pl.1" or after you did make install simply by "man run_regression.pl" if your MANPATH ist set properly.
    To start the regression test type "make regression_test" in the testsuite
    directory. This will search recursively for files with extension .test
    and run the corresponding regression for each of them. A summary of
    the regression test can be found in regression_test.log.
    
    If we plan to run the regression tests nightly the cron job could check
    if there's a FAIL in this logfile and send it to octopus-devel.
    
    I've added two simple tests. Please use the -c option of the Perl
    script to add own tests. This facilitates the generation of tests
    considerably.

---
+++ testsuite/run_regression.pl
@@ -0,0 +1,170 @@
+#!/usr/bin/perl
+#
+# $Id$
+
+use Getopt::Std;
+
+
+sub usage {
+
+  print <<EndOfUsage;
+
+ Copyright (C) 2005 by Heiko Appel
+
+Usage: run_regression.pl [options]
+
+    -n        dry-run
+    -v        verbose
+    -h        this usage
+    -e        name of octopus executable
+    -c        create template
+    -f        filename of testsuite
+    -d        working directory for the tests
+    -i        print inputfile
+    -p        preserve working directories
+
+Report bugs to <appel\@physik.fu-berlin.de>.
+EndOfUsage
+  exit 0;
+}
+
+
+sub create_template {
+  $date = `date +"%d.%m.%y"`;
+  chomp($date);
+  $arch = `uname -a`;
+  chomp($arch);
+  $author = `whoami`;
+  chomp($author);
+  $author =~ s/^(\w)(.*)/\u$1$2/;
+
+  open(TEMPLATE, ">".$opt_c );
+
+  print TEMPLATE <<EndOfTemplate;
+Test    :
+Author  : $author
+Date    : $date
+Arch    : $arch
+Release :
+
+INP
+CalculationMode = gs
+fromScratch = yes
+%Coordinates
+  "Na" | 0 | 0 | -1.54
+  "Na" | 0 | 0 |  1.54
+%
+EOF
+
+RUN
+
+# add your own matches
+#
+# Examples (of course you have to uncomment the lines :)
+# Match ; TotalEnergy ; grep -A 6 '^Energy:' static/info ; \\s*Total\\s*=\\s*-0.33802679
+# Match ; Eigenvalues ; grep -A 2 'Eigenvalues \\[' static/info | tail -1 ; \\s*-0.13735\\d*\\s*2.0000\\d*
+EndOfTemplate
+
+  close(TEMPLATE);
+
+  print "Template written to: $opt_c \n";
+
+  exit 0;
+
+}
+
+
+getopts("nvhe:c:f:d:ip");
+
+# Default values
+$workdir = `mktemp -d`;
+chomp($workdir);
+
+# Handle options
+$opt_h && usage;
+$opt_c && create_template;
+if($opt_d) { $workdir = $opt_d; }
+print "Using workdir    : $workdir \n";
+
+$octopus_exe = "octopus";
+if($opt_e) { $octopus_exe = $opt_e; };
+die "could not find executable: $octopus_exe \n" if (`which $octopus_exe` eq "");
+print "Using executable : $octopus_exe \n\n";
+
+system ("rm -rf $workdir");
+mkdir $workdir;
+
+open(TESTSUITE, "<".$opt_f ) or die "cannot open testsuite file \n";
+
+
+while ($_ = <TESTSUITE>) {
+
+ # skip comments
+ next if /^#/;
+
+ if ( $_ =~ /Test\s*:\s*(.*)\s*$/) {
+  %test = ();
+  $test{"name"} = $1;
+  print "\033[34m$test{'name'} \033[0m \n";
+ }
+
+ if ( $_ =~ /Author\s*:\s*(.*)\s*$/) {
+  %test = ();
+  $test{"author"} = $1;
+ }
+
+ if ( $_ =~ /Date\s*:\s*(.*)\s*$/) {
+  %test = ();
+  $test{"date"} = $1;
+ }
+
+ if ( $_ =~ /Release\s*:\s*(.*)\s*$/) {
+  %test = ();
+  $test{"release"} = $1;
+ }
+
+ # generating input file for test run
+ if ( $_ =~ /^INP/) {
+   $test{"inp"} = 1;
+   open(INP,">$workdir/inp");
+   while ( ($_ = <TESTSUITE>) ) {
+     last if ( $_ =~ /^EOF/ );
+     print INP $_;
+     if ($opt_i) { print STDOUT $_; }
+
+   }
+   close(INP);
+ }
+
+ if ( $_ =~ /^RUN/) {
+   die "inp not defined" if !$test{"inp"};
+   if (!$opt_n) {
+     print "\nStarting test run ...\n";
+     system("cd $workdir; $octopus_exe < inp &> out"); 
+     print "Finished test run.\n"; }
+   else {
+     print "cd $workdir; $octopus_exe < inp &> out \n";
+   }
+   $test{"run"} = 1;
+ }
+
+ if ( $_ =~ /^Match/ && !$opt_n) {
+   ($match, $name, $pre_command , $regexp) = split(/\;/,$_);
+   if ($opt_v) { print "$pre_command \n"; }
+   if ($opt_v) { print "$regexp \n"; }
+   $regexp =~ s/^\s*//;
+   $regexp =~ s/\s*$//;
+   $lineout = `cd $workdir; $pre_command`;
+
+
+   if ( $lineout =~ /$regexp/ ) {
+     print "$name: \t [ \033[32m  OK  \033[0m ] \n";
+   } else {
+     print "$name: \t [ \033[31m FAIL \033[0m ] \n";
+   }
+ }
+}
+
+print "\n\n\n";
+if (!$opt_p) { system ("rm -rf $workdir"); }
+
