e13490dc34fcef67d0887b735c3d4ce67c4011bd
Heiko Appel <appel@fhi-berlin.mpg.de>
2005-05-23 11:15:09 +0000

    In my last commit this one was slipping in. Undo changes.

--- src/global.h
+++ src/global.h
@@ -22,8 +22,8 @@
 #define __STRING(x)     #x
 
 #if defined(DEBUG)
-#  define ASSERT(expr) 
-!!#  define IN inout
+#  define ASSERT(expr) \
+  if(.not.(expr)) call assert_die (__STRING(expr), __FILE__, __LINE__)
 #else
 #  define ASSERT(expr)
 !!#  define IN inout
