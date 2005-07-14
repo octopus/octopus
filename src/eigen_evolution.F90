0c5a0d543d98f7a81daec6d1f18e9f39c74049e3
Alberto Castro <acastro@bifi.es>
2005-07-14 11:32:39 +0000

    Bug fixed. The code did not compile in the complex mode. Thanks, Carlo.

--- src/eigen_evolution.F90
+++ src/eigen_evolution.F90
@@ -76,13 +76,13 @@ subroutine eigen_solver_evolution(gr, st, h, tol, niter, converged, diff, tau, v
     integer :: n, m, itrace, iflag, lwsp, liwsp
     integer, allocatable :: iwsp(:)
     FLOAT :: t, tolerance, anorm
-    FLOAT, allocatable :: w(:, :), wsp(:)
+    R_TYPE, allocatable :: w(:, :), wsp(:)
 
 
     n = st%d%dim * gr%m%np
     m = 25 ! maximum size for the Krylov basis.
     t = -tau
-    tolerance = CNST(1.0e-6)
+    tolerance = CNST(0.0)
     anorm = M_ONE
     itrace = 0
     lwsp = n*(m+1)+n+(m+2)**2+4*(m+2)**2+6+1
@@ -94,7 +94,7 @@ subroutine eigen_solver_evolution(gr, st, h, tol, niter, converged, diff, tau, v
     h_  => h
     gr_ => gr
     ik_ =  ik
-    call dgexpv(n, m, t, psi(:, 1), w(:, 1), tolerance, anorm, &
+    call X(gexpv)(n, m, t, psi(:, 1), w(:, 1), tolerance, anorm, &
                 wsp, lwsp, iwsp, liwsp, mv, itrace, iflag)
     nullify(h_)
     nullify(gr_)
@@ -108,10 +108,10 @@ end subroutine eigen_solver_evolution
 
 
 subroutine mv(x, y)
-  FLOAT, intent(in) :: x(*)
-  FLOAT, intent(out) :: y(*)
+  R_TYPE, intent(in) :: x(*)
+  R_TYPE, intent(out) :: y(*)
   integer :: idim
-  FLOAT, allocatable :: psi(:, :), hpsi(:, :)
+  R_TYPE, allocatable :: psi(:, :), hpsi(:, :)
   allocate(psi(gr_%m%np, h_%d%dim), hpsi(gr_%m%np, h_%d%dim))
   do idim = 1, h_%d%dim
      psi(1:gr_%m%np, idim) = x((idim-1)*gr_%m%np+1:idim*gr_%m%np)
