5536aeebc09348127d1c01dc9bdbedfe99da1916
Xavier Andrade <xavier@tddft.org>
2005-07-31 20:32:28 +0000

    Improvements in the multigrid poisson solver:
    * The module checks if it has been initialized.
    
    * Now the old solution is saved and is used as the starting point for the
    next one. This speeds up things a lot.

--- src/poisson_multigrid.F90
+++ src/poisson_multigrid.F90
@@ -30,7 +30,12 @@ module poisson_multigrid
   integer :: maxmulti,maxcycles
   FLOAT :: threshold
   integer :: presteps, poststeps
+
+  FLOAT, pointer :: old_solution(:)
+  logical :: initialized=.false.
+  
   private
+
   public  :: poisson_multigrid_solver, &
        poisson_multigrid_init, &
        poisson_multigrid_end
@@ -56,14 +61,20 @@ contains
     call loct_parse_int(check_inp('PoissonSolverMGPostsmoothingSteps'), 3, poststeps)
     call loct_parse_int(check_inp('PoissonSolverMGMaxCycles'), 20, maxcycles)
 
-
     call build_aux(m)
     call build_phi(m)
     call pop_sub()
+    
+    allocate(old_solution(1:m%np))
+
+    initialized=.true.
+
   end subroutine poisson_multigrid_init
 
   subroutine poisson_multigrid_end()
     deallocate(aux, phi)
+    deallocate(old_solution)
+    initialized=.false.
   end subroutine poisson_multigrid_end
 
   subroutine poisson_multigrid_solver(gr, pot, rho)
@@ -78,13 +89,16 @@ contains
     type(float_pointer), allocatable :: tau(:)
     type(float_pointer), allocatable :: err(:)
 
-
-
     FLOAT, allocatable :: rho_corrected(:), vh_correction(:)
-
+    
     call push_sub('poisson_multigrid.multigrid_poisson_solver');
+    
+    if(.not. initialized) then
+       message(1) = 'Multigrid Poisson not initialized.'
+       call write_fatal(1)
+    end if
 
-    !!correction for treating boundaries
+    !!correction for treating boundaries 
     allocate(rho_corrected(gr%m%np), vh_correction(gr%m%np))
     call correct_rho(gr%m, maxmulti, rho, rho_corrected, vh_correction)
     rho_corrected = - M_FOUR*M_PI*rho_corrected
@@ -102,63 +116,62 @@ contains
        allocate(err(l)%p(1:gr%mgrid%level(l)%m%np))
     end do
 
-    phi(0)%p(1:gr%m%np)=pot(1:gr%m%np)
+!    phi(0)%p(1:gr%m%np)=pot(1:gr%m%np)
+    phi(0)%p(1:gr%m%np)=old_solution(1:gr%m%np)
+
     phi(0)%p(gr%m%np+1:gr%m%np_tot)=M_ZERO
     tau(0)%p(:)=rho_corrected(:)
+    
+    do t=0,maxcycles
 
-    do t=1,maxcycles
-
-#if 0
-       l=0
-       call multigrid_relax(gr%mgrid%level(l)%m,gr%mgrid%level(l)%f_der, &
-            phi(l)%p, tau(l)%p , 1000)
-
-#else
-
-       do l=0,(cl-1)
-
-          if(l /= 0) then
-             phi(l)%p=0.0
-          end if
-
-          !presmoothing
-          call multigrid_relax(gr%mgrid%level(l)%m,gr%mgrid%level(l)%f_der, &
-               phi(l)%p, tau(l)%p , presteps)
-
-          !error calcultion
-          call df_laplacian(gr%sb, gr%mgrid%level(l)%f_der, phi(l)%p, err(l)%p);
-          err(l)%p=tau(l)%p-err(l)%p
-
-          !transfer error to coarser grid
-          call multigrid_fine2coarse(gr%mgrid, l+1, err(l)%p, tau(l+1)%p)
-
-       end do
-
-       do l=cl,0,-1
-
-          !postsmoothing
-          call multigrid_relax(gr%mgrid%level(l)%m,gr%mgrid%level(l)%f_der, &
-               phi(l)%p, tau(l)%p , poststeps)
-
-          if(l/= 0) then
-             !transfer correction to finer level
-             call multigrid_coarse2fine(gr%mgrid, l, phi(l)%p, err(l-1)%p)
-             np=gr%mgrid%level(l-1)%m%np;
-             phi(l-1)%p(1:np)=phi(l-1)%p(1:np)+err(l-1)%p(1:np);
-          end if
-
-       end do
-
-#endif
-
-       call df_laplacian(gr%sb, gr%mgrid%level(0)%f_der, phi(0)%p, err(0)%p);
-       err(0)%p=tau(0)%p-err(0)%p
-       res=sqrt(sum((err(0)%p(1:gr%m%np)*gr%m%vol_pp(1:gr%m%np))**2))
-       if(res < threshold) then
-          exit
+       if(t > 0) then !in the first cycle only the residue is calculated
+          do l=0,(cl-1)
+             
+             if(l /= 0) then 
+                phi(l)%p=0.0
+             end if
+          
+             !presmoothing 
+             call multigrid_relax(gr%mgrid%level(l)%m,gr%mgrid%level(l)%f_der, &
+                  phi(l)%p, tau(l)%p , presteps)
+             
+             !error calcultion
+             call df_laplacian(gr%sb, gr%mgrid%level(l)%f_der, phi(l)%p, err(l)%p);
+             err(l)%p=tau(l)%p-err(l)%p
+             
+             !transfer error to coarser grid
+             call multigrid_fine2coarse(gr%mgrid, l+1, err(l)%p, tau(l+1)%p)
+             
+          end do
+          
+          call multigrid_relax(gr%mgrid%level(cl)%m,gr%mgrid%level(cl)%f_der, &
+               phi(cl)%p, tau(cl)%p , presteps)
+          
+          do l=cl,0,-1
+             
+             !postsmoothing 
+             call multigrid_relax(gr%mgrid%level(l)%m,gr%mgrid%level(l)%f_der, &
+                  phi(l)%p, tau(l)%p , poststeps)
+             
+             if(l/= 0) then
+                !transfer correction to finer level
+                call multigrid_coarse2fine(gr%mgrid, l, phi(l)%p, err(l-1)%p)
+                np=gr%mgrid%level(l-1)%m%np;
+                phi(l-1)%p(1:np)=phi(l-1)%p(1:np)+err(l-1)%p(1:np);
+             end if
+             
+          end do
        end if
+       
+       call df_laplacian(gr%sb, gr%mgrid%level(0)%f_der, phi(0)%p, err(0)%p);
+       err(0)%p=err(0)%p-tau(0)%p
+       res=sqrt(sum((err(0)%p(1:gr%m%np)*gr%m%vol_pp(1:gr%m%np))**2))
 
-       print*, "mgcycle", t, " res ", res! dot_product(err(0)%p,err(0)%p)
+!       print*, "mgcycle", t, " res ", res
+
+       if(res < threshold) then; 
+          exit
+       endif
 
     end do
 
@@ -167,11 +180,12 @@ contains
        write(message(2), '(a,e14.6)') '  Res = ', res
        call write_warning(2)
     endif
-
+    
+    old_solution(1:gr%m%np)=phi(0)%p(1:gr%m%np)
     pot(1:gr%m%np)=phi(0)%p(1:gr%m%np)+vh_correction(1:gr%m%np);
 
     deallocate(rho_corrected, vh_correction)
-
+    
     do l=0,cl
        deallocate(phi(l)%p)
        deallocate(tau(l)%p)
@@ -180,12 +194,13 @@ contains
     deallocate(phi)
     deallocate(tau)
     deallocate(err)
-
+    
     call pop_sub();
-
+    
   end subroutine poisson_multigrid_solver
 
 #define LAP f_der%der_discr%lapl
+
 
   subroutine multigrid_relax(m,f_der,pot,rho,steps)
 
@@ -200,21 +215,19 @@ contains
     FLOAT :: point_lap, h2, factor
     FLOAT, allocatable :: w(:)
 
-    factor=(-1.0/(CNST(-6.0)));
+    factor=(-M_ONE/(-M_SIX));
 
     !!search for the diagonal term
     !!DISABLED: currently using the diagonal term prevents convergence
-!    if(LAP%const_w) then
-!       do i=1,LAP%n
-!          if( 1 == LAP%i(i,1)) then
-!             factor=-1.0/LAP%w_re(i,1);
-!             exit
-!          end if
-!       end do
-!    end if
-
-
-
+    !    if(LAP%const_w) then 
+    !       do i=1,LAP%n
+    !          if( 1 == LAP%i(i,1)) then
+    !             factor=-1.0/LAP%w_re(i,1);
+    !             exit
+    !          end if
+    !       end do
+    !    end if
+    
     h2=m%h(1)*m%h(1)
     n=LAP%n
     allocate(w(1:n))
@@ -241,31 +254,7 @@ contains
     deallocate(w)
 
   end subroutine multigrid_relax
+
 #undef LAP
+
 end module poisson_multigrid
-
-
-#if 0
-    ! test transfer between coarse and fine grids
-    !  allocate(test(m_coarse%np), ltest(m_coarse%np), test2(m_finest%np), ltest2(m_finest%np))
-    !  call multigrid_fine2coarse(gr%mgrid, 1, rho, test)
-    !  call multigrid_coarse2fine(gr%mgrid, 1, test, test2)
-
-    !  call doutput_function(output_fill_how("AxisX_and_PlaneX_Gnuplot"), &
-    !     "lixo", "fine", m_finest, m_finest%sb, rho, M_ONE, ierr)
-!  call doutput_function(output_fill_how("AxisX_and_PlaneX_Gnuplot"), &
-    !     "lixo", "coarse", m_coarse, m_coarse%sb, test, M_ONE, ierr)
-    !  call doutput_function(output_fill_how("AxisX_and_PlaneX_Gnuplot"), &
-    !     "lixo", "fine2", m_finest, m_finest%sb, test2, M_ONE, ierr)
-
-    ! test laplacians
-    call df_laplacian(gr%sb, gr%mgrid%level(0)%f_der, rho, ltest2)
-    call doutput_function(output_fill_how("AxisX_and_PlaneX_Gnuplot"), &
-         "lixo", "l-fine", m_finest, m_finest%sb, ltest2, M_ONE, ierr)
-
-    call df_laplacian(gr%sb, gr%mgrid%level(1)%f_der, test, ltest)
-    call doutput_function(output_fill_how("AxisX_and_PlaneX_Gnuplot"), &
-         "lixo", "l-coarse", m_coarse, m_coarse%sb, ltest, M_ONE, ierr)
-
-#endif
-
