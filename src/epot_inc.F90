266eb3465774b069bf7cdf56e82e1e476b03147d
Heiko Appel <appel@fhi-berlin.mpg.de>
2005-09-26 08:15:19 +0000

    Remove SIGFPE that occurred for periodic systems during force calculations.

--- src/epot_inc.F90
+++ src/epot_inc.F90
@@ -99,86 +99,90 @@ subroutine X(epot_forces) (gr, ep, st, t, reduce_)
 
   ! init to 0
   do i = 1, geo%natoms
-    geo%atom(i)%f = M_ZERO
+     geo%atom(i)%f = M_ZERO
   end do
 
-  ! non-local component of the potential.
-  allocate(ppsi(gr%m%np, st%d%dim))
-  atm_loop: do i = 1, geo%natoms
-     atm => geo%atom(i)
-     if(atm%spec%local) cycle
-     do ivnl = 1, ep%nvnl
-        if(ep%p(ivnl)%index .ne. i) cycle
-        ik_loop: do ik = 1, st%d%nik
-          st_loop: do ist = st%st_start, st%st_end
+  if(.not.geo%only_user_def) then
+     ! non-local component of the potential.
+     allocate(ppsi(gr%m%np, st%d%dim))
+     atm_loop: do i = 1, geo%natoms
+        atm => geo%atom(i)
+        if(atm%spec%local) cycle
+        do ivnl = 1, ep%nvnl
+           if(ep%p(ivnl)%index .ne. i) cycle
+           ik_loop: do ik = 1, st%d%nik
+              st_loop: do ist = st%st_start, st%st_end
 
-             do j = 1, NDIM
-                ppsi = R_TOTYPE(M_ZERO)
-                do idim = 1, st%d%dim
-                   call X(project)(gr%m, ep%dp(j, ivnl:ivnl), 1, st%X(psi)(:, idim, ist, ik), ppsi(:, idim), &
-                                   periodic = .false., ik = ik)
-                enddo
-                atm%f(j) = atm%f(j) + M_TWO * st%occ(ist, ik) * &
-                           X(states_dotp)(gr%m, st%d%dim, st%X(psi)(:, :, ist, ik), ppsi)
-             enddo
+                 do j = 1, NDIM
+                    ppsi = R_TOTYPE(M_ZERO)
+                    do idim = 1, st%d%dim
+                       call X(project)(gr%m, ep%dp(j, ivnl:ivnl), 1, st%X(psi)(:, idim, ist, ik), ppsi(:, idim), &
+                            periodic = .false., ik = ik)
+                    enddo
+                    atm%f(j) = atm%f(j) + M_TWO * st%occ(ist, ik) * &
+                         X(states_dotp)(gr%m, st%d%dim, st%X(psi)(:, :, ist, ik), ppsi)
+                 enddo
 
-          end do st_loop
-        end do ik_loop
-     enddo
-  end do atm_loop
-  deallocate(ppsi)
+              end do st_loop
+           end do ik_loop
+        enddo
+     end do atm_loop
+     deallocate(ppsi)
+  endif
 
 #if defined(HAVE_MPI)
   do i = 1, geo%natoms
-    atm => geo%atom(i)
-    if(atm%spec%local) cycle
-    if(present(reduce_)) then
-      if(reduce_) then
-        call MPI_ALLREDUCE(atm%f(1), f(1), NDIM, &
-             MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD, ierr)
-        atm%f = f
-      end if
-    end if
+     atm => geo%atom(i)
+     if(atm%spec%local) cycle
+     if(present(reduce_)) then
+        if(reduce_) then
+           call MPI_ALLREDUCE(atm%f(1), f(1), NDIM, &
+                MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD, ierr)
+           atm%f = f
+        end if
+     end if
   enddo
 #endif
 
-  ! Now the ion, ion force term
-  do i = 1, geo%natoms
-    zi = geo%atom(i)%spec%Z_val
-    do j = 1, geo%natoms
-      if(i .ne. j) then
-        zj = geo%atom(j)%spec%Z_val
-        r = sqrt(sum((geo%atom(i)%x(1:NDIM) - geo%atom(j)%x(1:NDIM))**2))
-        d = zi * zj/r**3
+  if(.not.geo%only_user_def) then ! exclude user defined species for the moment
+     ! Now the ion, ion force term
+     do i = 1, geo%natoms
+        zi = geo%atom(i)%spec%Z_val
+        do j = 1, geo%natoms
+           if(i .ne. j) then
+              zj = geo%atom(j)%spec%Z_val
+              r = sqrt(sum((geo%atom(i)%x(1:NDIM) - geo%atom(j)%x(1:NDIM))**2))
+              d = zi * zj/r**3
 
-        geo%atom(i)%f(1:NDIM) = geo%atom(i)%f(1:NDIM) + &
-             d*(geo%atom(i)%x(1:NDIM) - geo%atom(j)%x(1:NDIM))
-      end if
-    end do
-  end do
+              geo%atom(i)%f(1:NDIM) = geo%atom(i)%f(1:NDIM) + &
+                   d*(geo%atom(i)%x(1:NDIM) - geo%atom(j)%x(1:NDIM))
+           end if
+        end do
+     end do
+  end if
 
   ! now comes the local part of the PP
   if(.not.simul_box_is_periodic(gr%sb).or.geo%only_user_def) then ! Real space
-    call local_RS()
+     call local_RS()
 #ifdef HAVE_FFT
   else ! Fourier space
-    call local_FS()
+     call local_FS()
 #endif
   end if
 
   if(present(t).and.ep%no_lasers>0) then
-    call laser_field(gr%sb, ep%no_lasers, ep%lasers, t, x)
-    do i = 1, geo%natoms
-      geo%atom(i)%f(1:NDIM) = geo%atom(i)%f(1:NDIM) + &
-           geo%atom(i)%spec%Z_val * x(1:NDIM)
-    end do
+     call laser_field(gr%sb, ep%no_lasers, ep%lasers, t, x)
+     do i = 1, geo%natoms
+        geo%atom(i)%f(1:NDIM) = geo%atom(i)%f(1:NDIM) + &
+             geo%atom(i)%spec%Z_val * x(1:NDIM)
+     end do
   end if
 
   if(associated(ep%e)) then
-    do i = 1, geo%natoms
-      geo%atom(i)%f(1:NDIM) = geo%atom(i)%f(1:NDIM) + &
-           geo%atom(i)%spec%Z_val * ep%e(1:NDIM)
-    end do
+     do i = 1, geo%natoms
+        geo%atom(i)%f(1:NDIM) = geo%atom(i)%f(1:NDIM) + &
+             geo%atom(i)%spec%Z_val * ep%e(1:NDIM)
+     end do
   end if
 
   !TODO: forces due to the magnetic fields (static and time-dependent)
@@ -193,17 +197,17 @@ contains
     ns = min(2, st%d%nspin)
 
     do i = 1, geo%natoms
-      atm => geo%atom(i)
-      do j = 1, NP
-        call mesh_r(gr%m, j, r, x=x, a=atm%x)
-        if(r < r_small) cycle
+       atm => geo%atom(i)
+       do j = 1, NP
+          call mesh_r(gr%m, j, r, x=x, a=atm%x)
+          if(r < r_small) cycle
 
-        call specie_get_glocal(atm%spec, x, gv)
-        ! FIXME: When running with partitions, vol_pp is local
-        ! to the node. It is likely, that this code need changes.
-        d = sum(st%rho(j, 1:ns))*gr%m%vol_pp(j)
-        atm%f(1:NDIM) = atm%f(1:NDIM) - d*gv(1:NDIM)
-      end do
+          call specie_get_glocal(atm%spec, x, gv)
+          ! FIXME: When running with partitions, vol_pp is local
+          ! to the node. It is likely, that this code need changes.
+          d = sum(st%rho(j, 1:ns))*gr%m%vol_pp(j)
+          atm%f(1:NDIM) = atm%f(1:NDIM) - d*gv(1:NDIM)
+       end do
     end do
   end subroutine local_RS
 
@@ -218,20 +222,20 @@ contains
     call dcf_alloc_RS(cf_for)
 
     do i = 1, geo%natoms
-      atm => geo%atom(i)
-      do j = 1, NDIM
-        cf_for%FS = M_z0
-        call cf_phase_factor(gr%sb, gr%m, atm%x, ep%local_cf(atm%spec%index), cf_for)
+       atm => geo%atom(i)
+       do j = 1, NDIM
+          cf_for%FS = M_z0
+          call cf_phase_factor(gr%sb, gr%m, atm%x, ep%local_cf(atm%spec%index), cf_for)
 
-        call dcf_FS_grad(gr%sb, gr%m, cf_for, j)
-        call dcf_FS2RS(cf_for)
-        call dcf2mf(gr%m, cf_for, force)
-        do l = 1, st%d%nspin
-          ! FIXME: When running with partitions, vol_pp is local
-          ! to the node. It is likely, that this code need changes.
-          atm%f(j) = atm%f(j) + sum(force(1:NP)*st%rho(1:NP, l)*gr%m%vol_pp(1:NP))
-        end do
-      end do
+          call dcf_FS_grad(gr%sb, gr%m, cf_for, j)
+          call dcf_FS2RS(cf_for)
+          call dcf2mf(gr%m, cf_for, force)
+          do l = 1, st%d%nspin
+             ! FIXME: When running with partitions, vol_pp is local
+             ! to the node. It is likely, that this code need changes.
+             atm%f(j) = atm%f(j) + sum(force(1:NP)*st%rho(1:NP, l)*gr%m%vol_pp(1:NP))
+          end do
+       end do
     end do
 
     call dcf_free(cf_for)
