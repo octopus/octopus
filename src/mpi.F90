1c684da2d1c0b208c91a77036309b3bfa1457f06
Florian Lorenzen <florian.lorenzen@posteo.de>
2005-10-06 11:31:05 +0000

    It is now possible to have more index-combinations than nodes (i. e. n_node < product(index_range)). In this case, the indices are mapped onto the available nodes with respect to communication costs of the different indices. This mapping is also done if n_node = product(index_range) to keep things simple but in this case the outcome is just a one-to-one mapping. The contraction is done using METIS.
    The multicomm_sanity_check has to be refreshed as I have removed the
    input variable NumberOfProcessorsForDomain. I think this variable
    leads to too much confusion. The number of processors per domain
    is implicitly set by n_node/n_domain_comm (and n_domain_comm
    is either product(range_index) or n_node if n_node < product(range_index)).

--- src/mpi.F90
+++ src/mpi.F90
@@ -172,24 +172,30 @@ module mpi_mod
 
   ! Stores all communicators and groups.
   type multicomm_type
-    integer          :: n_node          ! Total number of nodes.
-    integer          :: n_domain_node   ! Number of nodes per domain
-    integer          :: n_index         ! Number of parallel indices.
-    integer          :: n_domain_comm   ! Number of domain communicators.
-    integer          :: n_index_comm    ! Number of index communicators.
+    integer          :: n_node               ! Total number of nodes.
+    integer          :: n_domain_node        ! Number of nodes per domain
+    integer          :: n_index              ! Number of parallel indices.
+    integer          :: n_domain_comm        ! Number of domain communicators.
+    integer          :: n_index_comm         ! Number of index communicators.
 
-    integer          :: par_strategy    ! What kind of parallelization strategy should we use?
-    logical          :: use_domain_par  ! Should we use domain parallelization?
+    integer          :: par_strategy         ! What kind of parallelization strategy should we use?
+    logical          :: use_domain_par       ! Should we use domain parallelization?
 
-    integer, pointer :: index_range(:)  ! Range of index i is
-                                        ! 1, ..., index_range(i).
+    integer, pointer :: index_range(:)       ! Range of index i is
+                                             ! 1, ..., index_range(i).
+    integer, pointer :: index_weight(:)      ! How communication intensive
+                                             ! are the indices (big weight ->
+                                             ! lots of communication)?
 
-    integer, pointer :: domain_comm(:)  ! Domain communicators
-    integer, pointer :: domain_root(:)  ! Ranks of roots from every domain
-    integer, pointer :: index_comm(:)   ! Index communicators
+    integer, pointer :: domain_comm(:)       ! Domain communicators.
+    integer, pointer :: domain_root(:)       ! Ranks of roots from every domain.
+
+    integer, pointer :: index_comm(:)        ! Index communicators
+    integer, pointer :: index_contraction(:) ! Maps index-combinations to
+                                             ! to domain parallelizations.
 
     ! mapping for comm_world ranks
-    integer, pointer :: domain_comm_of_node(:)  ! Domain communicator that node belongs to
+    integer, pointer :: domain_comm_of_node(:) ! Domain communicator that node belongs to
   end type multicomm_type
 
 
@@ -264,6 +270,8 @@ contains
     call loct_parse_int(check_inp('ParallelizationStrategy'),  &
          P_STRATEGY_SERIAL, mc%par_strategy)
 
+    mc%index_weight  = 1
+
     select case(mc%par_strategy)
     case(P_STRATEGY_SERIAL)
        mc%use_domain_par = .false.
@@ -296,14 +304,18 @@ contains
        mc%index_range(2) = n_states
        message(1) = 'Info: Octopus will run parallel in states and domains.'
     case(P_STRATEGY_STATES_KPOINTS)
-       mc%use_domain_par = .false.
-       mc%index_range(1) = n_kpoints
-       mc%index_range(2) = n_states
+       mc%use_domain_par  = .false.
+       mc%index_range(1)  = n_kpoints
+       mc%index_range(2)  = n_states
+       mc%index_weight(1) = 1
+       mc%index_weight(2) = 100000
        message(1) = 'Info: Octopus will run parallel in states and k-points.'
     case(P_STRATEGY_FULL)
-       mc%use_domain_par = .true.
-       mc%index_range(1) = n_kpoints
-       mc%index_range(2) = n_states
+       mc%use_domain_par  = .true.
+       mc%index_range(1)  = n_kpoints
+       mc%index_range(2)  = n_states
+       mc%index_weight(1) = 1
+       mc%index_weight(2) = 100000
        message(1) = 'Info: Octopus will run parallel in states, k-points and domains.'
     case default
        call input_error('ParallelizationStrategy')
@@ -387,20 +399,20 @@ contains
     mc%n_node  = n_node
 
     allocate(mc%index_range(mc%n_index))
+    allocate(mc%index_weight(mc%n_index))
     allocate(a(mc%n_index))
 
     ! query parallelization strategy
     call multicomm_strategy(index_range, mc)
 
     ! number of domain communicators
-    mc%n_domain_comm = calc_domain_comm(mc%index_range)
+    mc%n_domain_comm = calc_domain_comm(mc%n_node, mc%index_range)
 
     ! calculate number of index communicators
     mc%n_index_comm = calc_index_comm(mc%n_index, mc%index_range)
 
     ! how many nodes per domain
-    call loct_parse_int(check_inp('NumberOfProcessorsForDomain'), &
-         mc%n_node/mc%n_domain_comm, mc%n_domain_node)
+    mc%n_domain_node = mc%n_node/mc%n_domain_comm
 
     ! sanity check of input
     call multicomm_sanity_check(mc)
@@ -415,8 +427,9 @@ contains
     ! parallelizations (or nodes).
     ! If mc%n_index_comm*m%n_domain_node are available then
     ! each index combination will be on a seperate domain parallelization.
-    ! To be implemented:
-    ! call contract_indices(n_node, n_domain_node, n_index, 
+    allocate(mc%index_contraction(product(mc%index_range)))
+    call contract_indices(mc%n_index, mc%index_weight, mc%index_range, &
+                          mc%n_domain_comm, mc%index_contraction)
 
     ! allocate space to hold the handles of all required communicators 
     ! and groups
@@ -461,7 +474,7 @@ contains
        enddo
     enddo
     message(1) = 'Info: Root nodes of domain groups:'
-    write(message(2),'(a,100i10)') 'Info: ',mc%domain_root
+    write(message(2),'(a,100i10)') 'Info: ', mc%domain_root
     call write_info(2)
 
     if (mc%n_index_comm.gt.1) then
@@ -478,17 +491,16 @@ contains
         ! This is a do-while loop which iterates over all
         ! indices except the i-th index. The counters for each
         ! index are in a.
-        do
-          ! Get number of communicator address by (a, i).
+        do ! Get number of communicator address by (a, i).
           group = index_comm_number(mc%n_index, mc%index_range, a, i)
           ! Ranks in MPI_COM_WORLD of future member nodes of 
           ! the communicator index_comm(group) are collected in this array.
           allocate(index_ranks(mc%index_range(i)))
           ! Get all members.
           do j = 1, mc%index_range(i)
-            a(i) = j
-            index_ranks(j) = mc%domain_root(                                    &
-                             domain_comm_number(mc%n_index, mc%index_range, a))
+            a(i)           = j
+            index_ranks(j) = mc%domain_root(mc%index_contraction(                &
+                             domain_comm_number(mc%n_index, mc%index_range, a)))
           end do
           ! Create communicator.
           call MPI_Group_incl(MPI_COMM_WORLD_GROUP, mc%index_range(j), &
@@ -516,6 +528,124 @@ contains
     call pop_sub()
   end subroutine multicomm_init
 
+  
+  ! ---------------------------------------------------------
+  ! Contracts the index combinations to the available domain
+  ! communicators.
+  ! This is done by building a graph with index combinations
+  ! as vertices. This graph is split into n_domain_comm
+  ! parts using METIS. Because the communication intensity
+  ! for different indices may be different a weight for each
+  ! index may be specified. Heavier weights indicate more
+  ! communication so that these indices end up on the same
+  ! domain communicator.
+  ! The result is a mapping m -> n
+  ! where m is a number of an index communicator (with
+  ! m = index_comm_number(..., a, i) for suitable a, i) and
+  ! n is an index into domain_comm.
+  !
+  ! Example:
+  ! n_index = 2
+  ! index_range = (/2, 3/)
+  ! index_weight = (/1000, 1/)
+  ! n_domain_comm = 4
+  !
+  ! This looks like this
+  !
+  !        index 1
+  !        +-->
+  !        |  (1)   (4)
+  ! index 2|  (2)   (5)
+  !        V  (3)   (6)
+  !
+  ! Forming the following graph:
+  !     (1)-1000-(4)
+  !     1|        |1
+  !     (2)-1000-(5)
+  !     1|        |1
+  !     (3)-1000-(6)
+  ! which has to be split into four parts.
+  !
+  ! The result may be something like
+  ! index_contraction(1) = 1
+  ! index_contraction(2) = 2
+  ! index_contraction(3) = 3
+  ! index_contraction(4) = 1
+  ! index_contraction(5) = 2
+  ! index_contraction(6) = 4
+  subroutine contract_indices(n_index, index_weight, index_range, &
+                              n_domain_comm, index_contraction) 
+    integer, intent(in)  :: n_index
+    integer, intent(in)  :: index_weight(:)
+    integer, intent(in)  :: index_range(:)
+    integer, intent(in)  :: n_domain_comm
+    integer, intent(out) :: index_contraction(:)
+
+    integer              :: i, j, k, edgecut
+    integer              :: nv         ! Number of vertices.
+    integer              :: ne         ! Number of edges.
+    integer              :: a(n_index) ! Indexing array.
+    integer, allocatable :: xadj(:)    ! Adjacency index.
+    integer, allocatable :: adjncy(:)  ! Adjacency lists.
+    integer, allocatable :: weight(:)  ! Edge weights.
+    integer              :: options(5) ! Options to METIS.
+
+    call push_sub('mpi.contract_indices')
+
+    options = (/1, 2, 1, 1, 0/) ! Use heavy edge matching in METIS.
+
+    ! Space for adjacency and index lists.
+    nv = product(index_range)
+    allocate(xadj(nv+1))
+    allocate(adjncy(2*nv*n_index))
+    allocate(weight(2*nv*n_index))
+
+    ! Build graph with index combinations as vertices and
+    ! edges as index communicators.
+    ne = 1
+    a  = 1
+    ! Iterate over all index combinations.
+    ! The following code is very similair to mesh_partition in
+    ! mesh_create.F90. But instead of restricting the graph
+    ! to a vertex degree of <= 3 this graph has vertex degree n_index.
+    j = 1
+    do
+      xadj(j) = ne
+      ! Consider all neighbours.
+      do i = 1, n_index
+        ! This is a little bit dirty: Set a to next-neighbour.
+        a(i) = a(i)+1
+        k    = domain_comm_number(n_index, index_range, a)
+        ! If k is existant, add edge.
+        if(k.ne.0) then
+          adjncy(ne) = k
+          weight(ne) = index_weight(i)
+          ne         = ne+1
+        end if
+        ! Set a to prev-neighbour.
+        a(i) = a(i)-2
+        k    = domain_comm_number(n_index, index_range, a)
+        if(k.ne.0) then
+          adjncy(ne) = k
+          weight(ne) = index_weight(i)
+          ne         = ne+1
+        end if
+        ! Restore a to not mix up iterating over a.
+        a(i) = a(i)+1
+      end do
+      j = j+1
+      if(.not.iterate_indices(n_index, index_range, a, 0)) exit
+    end do
+    xadj(nv+1) = ne
+    ne         = ne-1
+
+    call oct_metis_part_graph_recursive(nv, xadj, adjncy, 0,            &
+      weight, 1, 1, n_domain_comm, options, edgecut, index_contraction)
+
+    call pop_sub()
+
+  end subroutine contract_indices
+
 
   ! ---------------------------------------------------------
   subroutine multicomm_end(mc)
@@ -540,10 +670,12 @@ contains
     enddo
 
     deallocate(mc%domain_comm_of_node)
-    deallocate(mc%index_range )
-    deallocate(mc%domain_comm )
-    deallocate(mc%domain_root )
-    deallocate(mc%index_comm  )
+    deallocate(mc%index_range)
+    deallocate(mc%domain_comm)
+    deallocate(mc%domain_root)
+    deallocate(mc%index_comm)
+    deallocate(mc%index_contraction)
+    deallocate(mc%index_weight)
 
     call pop_sub()
   end subroutine multicomm_end
@@ -556,6 +688,10 @@ contains
   ! ---------------------------------------------------------
   ! Return number of index communicator for index i and all other
   ! indices set to values a(x), x|=i.
+  ! Returns 0 for invalid (out of range) indices in a. If the
+  ! result is later on used as an index the bounds
+  ! checking of the compiler will alert as the index_comm array
+  ! starts with 1.
   integer function index_comm_number(n_index, index_range, a, i) result(number)
     integer, intent(in) :: n_index
     integer, intent(in) :: index_range(:) ! index_range(n_index)
@@ -570,6 +706,12 @@ contains
     integer :: x, y
 
     call push_sub('mpi.index_comm_number')
+
+    ! Check ranges first.
+    if(.not.range_check(n_index, index_range, a, i)) then
+      number = 0
+      return
+    end if
 
     ! See comment at top of this file for the formula computed
     ! and a description what is happening.
@@ -611,11 +753,12 @@ contains
 
 
   ! ---------------------------------------------------------
-  ! Return the number of domain communicator for indeices set to
+  ! Return the number of domain communicator for indices set to
   ! a(x), x=1, ..., n_index.
   ! This maps (i1, i2, ..., i_n_index) -> n, i. e. computes how to
   ! store a multidimensional array in a linear field (i_n_index is
   ! stored consecutively).
+  ! Returns 0 for invalid (out of range) indices in a.
   integer function domain_comm_number(n_index, index_range, a) result(number)
     integer, intent(in) :: n_index
     integer, intent(in) :: index_range(:)
@@ -624,6 +767,12 @@ contains
     integer :: x
     
     call push_sub('mpi.domain_comm_number')
+
+    ! Check ranges first.
+    if(.not.range_check(n_index, index_range, a, 0)) then
+      number = 0
+      return
+    end if
 
     number = 0
 
@@ -637,6 +786,35 @@ contains
     call pop_sub()
 
   end function domain_comm_number 
+
+
+  ! ---------------------------------------------------------
+  ! Checks, if indices a(j), j=1, ..., n_index, j|=i
+  ! are inside their ranges index_range(j).
+  ! One index being out of range is sufficient for returning false.
+  logical function range_check(n_index, index_range, a, i) result(inbound)
+    integer, intent(in) :: n_index
+    integer, intent(in) :: index_range(:)
+    integer, intent(in) :: a(:)
+    integer, intent(in) :: i
+
+    integer :: j
+
+    call push_sub('mpi.range_check')
+
+    inbound = .true.
+    j       = 1
+
+    do while(inbound.and.j.le.n_index)
+      if(j.ne.i) then
+        inbound = a(j).le.index_range(j).and.a(j).gt.0
+      end if
+      j = j+1
+    end do
+
+    call pop_sub()
+
+  end function range_check
 
 
   ! ---------------------------------------------------------
@@ -655,7 +833,7 @@ contains
   ! a = 1
   ! do
   !   ! Something useful.
-  !   if(.not.iterate_indices(n_index, index_ranges, a, i) exit
+  !   if(.not.iterate_indices(n_index, index_ranges, a, i)) exit
   ! end do
   !
   ! If Fortran had a non repelling loop this looked like
@@ -702,7 +880,8 @@ contains
 
   ! ---------------------------------------------------------
   ! compute the number of required domain communicators
-  integer function calc_domain_comm(index_range) result(n_domain_comm)
+  integer function calc_domain_comm(n_node, index_range) result(n_domain_comm)
+    integer, intent(in) :: n_node
     integer, intent(in) :: index_range(:)
 
     call push_sub('mpi.calc_domain_comm')
@@ -710,6 +889,10 @@ contains
     ! In this case, ranges of 1 are no problem as they do not
     ! change the product of index_range(1)*...*index_range(n_index).
     n_domain_comm = product(index_range)
+
+    if(n_domain_comm.gt.n_node) then
+      n_domain_comm = n_node
+    end if
 
     call pop_sub()
 
