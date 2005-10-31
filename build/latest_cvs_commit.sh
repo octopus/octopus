e8f96800dd31a4b0552f80549ca40a5d0e0bc500
Heiko Appel <appel@fhi-berlin.mpg.de>
2005-10-31 14:53:24 +0000

    The "latest cvs changes" info that octopus prints at startup was not set correctly anymore. The reason were multiple cvs Id tags that are contained in the ChangeLog file. To fix this I added --exclude="./ChangeLog" to the grep call.

--- build/latest_cvs_commit.sh
+++ build/latest_cvs_commit.sh
@@ -3,4 +3,4 @@
 # $Id$
 
 # this is meant to be called from octopus/src/Makefile.am
-cd ../ && find . -type f -exec grep --binary-files=without-match 'Exp \$' \{\} \; | tr -d \#\! | awk '{print $4" "$5" "$2" "$3}' | sort | tail -1
+cd ../ && find . -type f -exec grep --exclude="./ChangeLog" --binary-files=without-match 'Exp \$' \{\} \; | tr -d \#\! | awk '{print $4" "$5" "$2" "$3}' | sort | tail -1
