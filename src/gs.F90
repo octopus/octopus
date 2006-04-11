84eaaecdf1afbb3ae2b0238f1e1e4b959b5737ca
Alberto Castro <acastro@bifi.es>
2006-04-11 12:07:47 +0000

    I put back essentially the same changes in gs.F90 that Micael put yesterday and later removed. I just added a call to X(system_h_setup), that should be there. But the rest of the changes were actually good, this way it is better looking.
    I also had to change the last figures of the numbers of a couple of tests.

--- src/gs.F90
+++ src/gs.F90
@@ -52,15 +52,19 @@ contains
     type(hamiltonian_t), intent(inout) :: h
     logical,             intent(inout) :: fromScratch
 
+    logical :: lcao_start, lcao_start_default = .true.
+    type(lcao_t) :: lcao_data
     type(scf_t) :: scfv
     integer     :: ierr
+#if defined(HAVE_MPI)
+    integer :: mpi_err
+#endif
 
     call push_sub('gs.ground_state_run')
 
     call X(states_allocate_wfns)(sys%st, sys%gr%m)
 
     if(.not.fromScratch) then
-
       ! load wave-functions
       call X(restart_read) (trim(tmpdir)//'restart_gs', sys%st, sys%gr, ierr)
       if(ierr.ne.0) then
@@ -71,14 +75,60 @@ contains
       end if
     end if
 
+    ! set barrier before the first communication takes place
+    ! this ensures proper debug timing of MPI calls
+#if defined(HAVE_MPI)
+    call TS(MPI_Barrier)(MPI_COMM_WORLD, mpi_err)
+#endif
+
     if(fromScratch) then
-      call ground_state_init(sys%gr, sys%st, sys%ks, h)
+      call states_generate_random(sys%st, sys%gr%m)
     end if
 
     ! setup Hamiltonian
     message(1) = 'Info: Setting up Hamiltonian.'
     call write_info(1)
     call X(system_h_setup) (sys, h)
+
+    if(fromScratch) then
+      ! Get a better guess for the density
+      call system_guess_density(sys%gr%m, sys%gr%sb, sys%gr%geo, sys%st%qtot, sys%st%d%nspin, &
+           sys%st%d%spin_channels, sys%st%rho)
+
+      ! The initial LCAO calculation is done by default if we have pseudopotentials.
+      ! Otherwise, it is not the default value and has to be enforced in the input file.
+      if(sys%gr%geo%only_user_def) lcao_start_default = .false.
+
+      !%Variable LCAOStart
+      !%Type logical
+      !%Default yes
+      !%Section SCF
+      !%Description
+      !% Before starting a SCF calculation, performs
+      !% a LCAO calculation. These should provide <tt>octopus</tt> with a good set
+      !% of initial wave-functions, and help the convergence of the SCF cycle.
+      !% (Up to current version, only a minimal basis set used.)
+      !%End
+      call loct_parse_logical(check_inp('LCAOStart'), lcao_start_default, lcao_start)
+      if(lcao_start) then
+        call X(v_ks_calc)(sys%gr, sys%ks, h, sys%st, calc_eigenval=.true.)
+          
+        lcao_data%state = 0 ! Uninitialized here.
+        call lcao_init(sys%gr, lcao_data, sys%st, h)
+        if(lcao_data%state == 1) then
+          write(message(1),'(a,i4,a)') 'Info: Performing initial LCAO calculation with ', &
+               lcao_data%st%nst,' orbitals.'
+          call write_info(1)
+          
+          call lcao_wf(lcao_data, sys%st, sys%gr%m, sys%gr%sb, h)
+          call lcao_end(lcao_data, sys%st%nst)
+          
+          call X(system_h_setup) (sys, h)
+          call states_write_eigenvalues(stdout, sys%st%nst, sys%st, sys%gr%sb)
+        end if
+      end if
+
+    end if
 
     ! run self consistency
 #ifdef COMPLEX_WFNS
@@ -96,71 +146,5 @@ contains
     deallocate(sys%st%X(psi)); nullify(sys%st%X(psi))
     call pop_sub()
   end subroutine ground_state_run
-
-
-  ! ---------------------------------------------------------
-  subroutine ground_state_init(gr, st, ks,  h)
-    type(grid_t),        intent(inout) :: gr
-    type(states_t),      intent(inout) :: st
-    type(v_ks_t),        intent(inout) :: ks
-    type(hamiltonian_t), intent(inout) :: h
-
-    integer :: err
-    logical :: lcao_start, lcao_start_default = .true.
-    type(lcao_t) :: lcao_data
-#if defined(HAVE_MPI)
-    integer :: mpi_err
-#endif
-
-    call push_sub('gs.ground_state_init')
-
-    ! set barrier before the first communication takes place
-    ! this ensures proper debug timing of MPI calls
-#if defined(HAVE_MPI)
-    call TS(MPI_Barrier)(MPI_COMM_WORLD, mpi_err)
-#endif
-
-    call system_guess_density(gr%m, gr%sb, gr%geo, st%qtot, st%d%nspin, &
-      st%d%spin_channels, st%rho)
-
-    ! The initial LCAO calculation is done by default if we have pseudopotentials.
-    ! Otherwise, it is not the default value and has to be enforced in the input file.
-    if(gr%geo%only_user_def) lcao_start_default = .false.
-
-    !%Variable LCAOStart
-    !%Type logical
-    !%Default yes
-    !%Section SCF
-    !%Description
-    !% Before starting a SCF calculation, performs
-    !% a LCAO calculation. These should provide <tt>octopus</tt> with a good set
-    !% of initial wave-functions, and help the convergence of the SCF cycle.
-    !% (Up to current version, only a minimal basis set used.)
-    !%End
-    call loct_parse_logical(check_inp('LCAOStart'), lcao_start_default, lcao_start)
-    if(lcao_start) then
-      call X(v_ks_calc)(gr, ks, h, st, calc_eigenval=.true.)
-
-      lcao_data%state = 0 ! Uninitialized here.
-      call lcao_init(gr, lcao_data, st, h)
-      if(lcao_data%state == 1) then
-        write(message(1),'(a,i4,a)') 'Info: Performing initial LCAO calculation with ', &
-          lcao_data%st%nst,' orbitals.'
-        call write_info(1)
-
-        call lcao_wf(lcao_data, st, gr%m, gr%sb, h)
-        call lcao_end(lcao_data, st%nst)
-
-        call states_fermi(st, gr%m)                         ! occupations
-        call states_write_eigenvalues(stdout, st%nst, st, gr%sb)
-      else
-        call states_generate_random(st, gr%m)
-      end if
-    else 
-      call states_generate_random(st, gr%m)
-    end if
-
-    call pop_sub()
-  end subroutine ground_state_init
 
 end module ground_state_m
