2e59bfa2ff4cbbf12eafe91668d09c56a34344af
Alberto Castro <acastro@bifi.es>
2006-04-11 14:28:08 +0000

    Changed the way in which the KLI terms are calculated. This new way it is way faster, especially in parallel and for large systems, for which the KLI part of the calculation dominates all the others. So it is crucial in those cases.
    *However*: the new scheme requires much more memory (essentially one full
    new array of KS states). This may be a problem. So maybe we could think
    of putting back the other scheme and leaving the option for the user. This
    choice may show up in more places (memory vs. CPU, I can think of a couple
    of other places), so we could think of a general input variable
    (MinimizeMemory, or something like that) which may be switched on if not
    enough memory is available, but at the cost of potentially more CPU time.

--- src/xc_KLI.F90
+++ src/xc_KLI.F90
@@ -25,13 +25,11 @@ subroutine X(xc_KLI_solve) (m, st, is, oep)
   type(xc_oep_t), intent(inout) :: oep
 
 #if defined(HAVE_MPI)
-  integer :: status(MPI_STATUS_SIZE), ierr
+  integer :: ierr
 #endif
-  integer :: i, j, n
-  FLOAT, allocatable :: d(:)
-  FLOAT, allocatable :: rho_sigma(:), v_bar_S(:)
+  integer :: i, j, n, kssi, kssj, proc
+  FLOAT, allocatable :: rho_sigma(:), v_bar_S(:), sqphi(:, :, :), d(:)
   FLOAT, allocatable :: Ma(:,:), x(:,:), y(:,:)
-  R_TYPE, allocatable :: phi1(:,:), phi2(:,:)
   R_TYPE :: occ
 
   call profiling_in(C_PROFILING_XC_KLI)
@@ -40,15 +38,22 @@ subroutine X(xc_KLI_solve) (m, st, is, oep)
   ! some intermediate quantities
   ! vxc contains the Slater part!
   ALLOCATE(rho_sigma(m%np), m%np)
+  ALLOCATE(sqphi(m%np, st%d%dim, st%nst), m%np*st%d%dim*st%nst)
+
+  do i = st%st_start, st%st_end
+    sqphi(1:m%np, 1:st%d%dim, i) = R_REAL(st%X(psi)(1:m%np, 1:st%d%dim, i, is))**2 + &
+                                   R_AIMAG(st%X(psi)(1:m%np, 1:st%d%dim, i, is))**2
+  end do
 
   do i = 1, m%np
     rho_sigma(i) = max(sum(oep%socc*st%occ(st%st_start:st%st_end, is) * &
-      R_ABS(st%X(psi)(i, 1, st%st_start:st%st_end, is))**2), CNST(1e-20))
+      sqphi(i, 1, st%st_start:st%st_end)), CNST(1e-20))
   end do
+
 #if defined(HAVE_MPI)
   if(st%parallel_in_states) then
     ALLOCATE(d(m%np), m%np)
-    call MPI_Allreduce(rho_sigma(:), d(:), m%np, MPI_FLOAT, MPI_SUM, st%mpi_grp%comm, ierr)
+    call MPI_Allreduce(rho_sigma(1), d(1), m%np, MPI_FLOAT, MPI_SUM, st%mpi_grp%comm, ierr)
     rho_sigma(1:m%np) = d(1:m%np)
   end if
 #endif
@@ -62,14 +67,16 @@ subroutine X(xc_KLI_solve) (m, st, is, oep)
   end do
 #if defined(HAVE_MPI)
   if(st%parallel_in_states) then
-    call MPI_Allreduce(oep%vxc(:),   d(:), m%np, MPI_FLOAT, MPI_SUM, st%mpi_grp%comm, ierr)
+    call MPI_Allreduce(oep%vxc(1),   d(1), m%np, MPI_FLOAT, MPI_SUM, st%mpi_grp%comm, ierr)
     oep%vxc(1:m%np)   = d(1:m%np)
     deallocate(d)
   end if
 #endif
 
   if(oep%level == XC_OEP_SLATER) then
-    deallocate(rho_sigma)
+    deallocate(rho_sigma, sqphi)
+    call pop_sub()
+    call profiling_out(C_PROFILING_XC_KLI)
     return
   end if
 
@@ -78,154 +85,79 @@ subroutine X(xc_KLI_solve) (m, st, is, oep)
   ALLOCATE(v_bar_S(st%nst), st%nst)
   do i = st%st_start, st%st_end
     if(st%occ(i, is) .gt. small) then
-      v_bar_S(i) = dmf_dotp(m, (R_ABS(st%X(psi)(1:m%np, 1, i, is)))**2 , oep%vxc)
+      v_bar_S(i) = dmf_dotp(m, sqphi(:, 1, i) , oep%vxc)
     end if
   end do
 
 #if defined(HAVE_MPI)
   if(st%parallel_in_states) then
-    ! Broadcast the vector v_bar_S to all processors
-    if(st%st_end - st%st_start + 1 .ne. st%nst) then ! This holds only in the td part.
-      do i = 1, st%nst
-        call MPI_Bcast(v_bar_S(i), 1, MPI_FLOAT, st%node(i), st%mpi_grp%comm, ierr)
-      end do
-    end if
+    ! Broadcast the vector v_bar_S  and sqphi to all processors
+    do i = 1, st%nst
+      call MPI_Bcast(v_bar_S(i), 1, MPI_FLOAT, st%node(i), st%mpi_grp%comm, ierr)
+    end do
+    do i = 1, n
+      kssi = oep%eigen_index(i)
+      call MPI_Bcast(sqphi(1, 1, kssi), m%np, MPI_FLOAT, st%node(kssi), st%mpi_grp%comm, ierr)
+    end do
   end if
 #endif
 
   ! If there is more than one state, so solve linear equation.
   linear_equation: if(n > 0) then
-    ALLOCATE(phi1(m%np, st%d%dim), m%np*st%d%dim)
-    ALLOCATE(phi2(m%np, st%d%dim), m%np*st%d%dim)
-    phi1 = M_ZERO; phi2 = M_ZERO
-
     ALLOCATE(d(m%np), m%np)
     ALLOCATE(x(n, 1), n*1)
-    x = M_ZERO
-
     ALLOCATE(Ma(n, n), n*n)
     ALLOCATE(y(n, 1), n*1)
+    x = M_ZERO; y = M_ZERO; Ma = M_ZERO; d = M_ZERO
+    proc = st%mpi_grp%rank
 
-    do i = 1, n
-
-#if defined(HAVE_MPI)
-      if(st%parallel_in_states) then
-        if(.not.mpi_grp_is_root(st%mpi_grp)) then
-          if(st%node(oep%eigen_index(i)) == st%mpi_grp%rank) then
-            call MPI_Send(st%X(psi)(1, 1, oep%eigen_index(i), is), st%d%dim*m%np, R_MPITYPE, &
-              0, i, st%mpi_grp%comm, ierr)
-          end if
-        else ! master node
-          if(st%node(oep%eigen_index(i)).ne.0) then
-            call MPI_Recv(phi1(1, 1), st%d%dim*m%np, R_MPITYPE, st%node(oep%eigen_index(i)), &
-              i, st%mpi_grp%comm, status, ierr)
-          else
-            phi1(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(i), is)
-          end if
-        end if
-        call MPI_Barrier(st%mpi_grp%comm, ierr)
-      else
-        phi1(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(i), is)
-      endif
-#else
-      phi1(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(i), is)
-#endif
-
-      if(mpi_grp_is_root(st%mpi_grp)) then
-        d(1:m%np) = (R_REAL(phi1(1:m%np, 1))**2 + &
-          R_AIMAG(phi1(1:m%np, 1))**2) / rho_sigma(1:m%np)
-      end if
-
-      do j = i, n
-#if defined(HAVE_MPI)
-        if(st%parallel_in_states) then
-          if(.not.mpi_grp_is_root(st%mpi_grp)) then
-            if(st%node(oep%eigen_index(j)) == st%mpi_grp%rank) then
-              call MPI_Send(st%X(psi)(1, 1, oep%eigen_index(j), is), st%d%dim*m%np, R_MPITYPE, &
-                0, j, st%mpi_grp%comm, ierr)
-            end if
-          else
-            if(st%node(oep%eigen_index(j)).ne.0) then
-              call MPI_Recv(phi2(1, 1), st%d%dim*m%np, R_MPITYPE, st%node(oep%eigen_index(j)), &
-                j, st%mpi_grp%comm, status, ierr)
-            else
-              phi2(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(j), is)
-            end if
-          end if
-          call MPI_Barrier(st%mpi_grp%comm, ierr)
-        else
-          phi2(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(j), is)
-        end if
-#else
-        phi2(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(j), is)
-#endif
-
-        if(mpi_grp_is_root(st%mpi_grp)) then
-          Ma(i, j) = - dmf_dotp(m, d, (R_REAL(phi2(1:m%np, 1))**2 + R_AIMAG(phi2(1:m%np, 1))**2) )
-          Ma(j,i) = Ma(i,j)
-        end if
-      end do
-
-      if(mpi_grp_is_root(st%mpi_grp)) then
+    i_loop: do i = 1, n
+      kssi = oep%eigen_index(i)
+      if(proc .eq. st%node(kssi)) then
+        d(1:m%np) = sqphi(1:m%np, 1, kssi) / rho_sigma(1:m%np)
+        j_loop: do j = i, n
+          kssj = oep%eigen_index(j)
+          Ma(i, j) = - dmf_dotp(m, d, sqphi(:, 1, kssj) )
+        end do j_loop
         Ma(i,i) = M_ONE + Ma(i,i)
-        y(i, 1) = v_bar_S(oep%eigen_index(i)) - oep%uxc_bar(oep%eigen_index(i))
+        y(i, 1) = v_bar_S(kssi) - oep%uxc_bar(kssi)
       end if
-    end do
-
-    if(mpi_grp_is_root(st%mpi_grp)) then
-      call lalg_linsyssolve(n, 1, Ma, y, x)
-    end if
-    deallocate(Ma, y)
+    end do i_loop
 
 #if defined(HAVE_MPI)
-    if(st%parallel_in_states)  call MPI_Barrier(st%mpi_grp%comm, ierr)
+    if(st%parallel_in_states) then
+      do i = 1, n
+        kssi = oep%eigen_index(i)
+        call MPI_Bcast(y(i, 1), 1, MPI_FLOAT, st%node(kssi), st%mpi_grp%comm, ierr)
+        do j = 1, n
+           call MPI_Bcast(Ma(i, j), 1, MPI_FLOAT, st%node(kssi), st%mpi_grp%comm, ierr)
+        end do
+     end do
+    end if
 #endif
 
     do i = 1, n
-      ! add contribution of low lying states. Once again, we have to send every state
-      ! that is not in node zero to node 0
-#if defined(HAVE_MPI)
-      if(st%parallel_in_states) then
-        if(.not.mpi_grp_is_root(st%mpi_grp)) then
-          if(st%node(oep%eigen_index(i)) == st%mpi_grp%rank) then
-            call MPI_Send(st%X(psi)(1, 1, oep%eigen_index(i), is), st%d%dim*m%np, R_MPITYPE, &
-              0, i, st%mpi_grp%comm, ierr)
-          end if
-        else ! master
-          if(st%node(oep%eigen_index(i)).ne.0) then
-            call MPI_Recv(phi1(1, 1), st%d%dim*m%np, R_MPITYPE, st%node(oep%eigen_index(i)), &
-              i, st%mpi_grp%comm, status, ierr)
-          else
-            phi1(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(i), is)
-          end if
-        end if
-        call MPI_Barrier(st%mpi_grp%comm, ierr)
-      else
-        phi1(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(i), is)
-      end if
-#else
-      phi1(1:m%np, 1:st%d%dim) = st%X(psi)(1:m%np, 1:st%d%dim, oep%eigen_index(i), is)
-#endif
-      occ = st%occ(oep%eigen_index(i), is)
-
-      if(mpi_grp_is_root(st%mpi_grp)) then
-        oep%vxc(1:m%np) = oep%vxc(1:m%np) + &
-          oep%socc * occ * x(i,1) * R_ABS(phi1(1:m%np, 1))**2 / rho_sigma(1:m%np)
-      end if
+      do j = i, n
+        Ma(j, i) = Ma(i, j)
+      end do
     end do
 
-    deallocate(d, x, phi1, phi2)
+    call lalg_linsyssolve(n, 1, Ma, y, x)
+
+    do i = 1, n
+      kssi = oep%eigen_index(i)
+      occ = st%occ(kssi, is)
+      oep%vxc(1:m%np) = oep%vxc(1:m%np) + &
+        oep%socc * occ * x(i,1) * sqphi(1:m%np, 1, kssi ) / rho_sigma(1:m%np)
+    end do
+
+    deallocate(d, x, Ma, y)
+
   end if linear_equation
   ! The previous stuff is only needed if n>0.
 
-#if defined(HAVE_MPI)
-  if(st%parallel_in_states) then
-    ! Since only node 0 holds the true vxc, broadcast it to all processors.
-    call MPI_Bcast(oep%vxc(:), m%np, MPI_FLOAT, 0, st%mpi_grp%comm, ierr)
-  end if
-#endif
-
-  deallocate(v_bar_S, rho_sigma)
+  deallocate(v_bar_S, rho_sigma, sqphi)
   call pop_sub()
   call profiling_out(C_PROFILING_XC_KLI)
 end subroutine X(xc_KLI_solve)
+
