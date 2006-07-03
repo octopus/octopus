fcdae42fd47dbb835db862fa6c7a39aaf715f52c
Alberto Castro <acastro@bifi.es>
2006-07-03 18:25:55 +0000

    Bug fixed.
    A couple of problems in the lcao part, that could something
    to do with ticket #37. But I do not know if it solves it since
    I did not attempt to reproduce it.
    
    The uninitialized variables problem can be usually tracked well
    by passing the -fpe0 -ftrapuv options when using the Intel
    compilers.

--- src/lcao_inc.F90
+++ src/lcao_inc.F90
@@ -113,7 +113,7 @@ subroutine X(lcao_init) (lcao_data, gr, h, norbs)
   ALLOCATE(lcao_data%X(v)      (norbs, norbs, st%d%nik), norbs*norbs*st%d%nik)
 
   ! Overlap and kinetic+so matrices.
-  ALLOCATE(hpsi(NP, st%d%dim), NP*st%d%dim)
+  ALLOCATE(hpsi(NP_PART, st%d%dim), NP_PART*st%d%dim)
   do ik = 1, st%d%nik
     do n1 = 1, st%nst
       call X(kinetic) (h, gr, st%X(psi)(:, :, n1, ik), hpsi(:, :), ik)
@@ -128,8 +128,10 @@ subroutine X(lcao_init) (lcao_data, gr, h, norbs)
 
       do n2 = n1, st%nst
         lcao_data%X(k)(n1, n2, ik) = X(states_dotp)(gr%m, st%d%dim, hpsi, st%X(psi)(:, : ,n2, ik))
+        lcao_data%X(k)(n2, n1, ik) = lcao_data%X(k)(n1, n2, ik)
         lcao_data%X(s)(n1, n2, ik) = X(states_dotp)(gr%m, st%d%dim, st%X(psi)(:, :, n1, ik), &
              st%X(psi)(:, : ,n2, ik))
+        lcao_data%X(s)(n2, n1, ik) = lcao_data%X(s)(n1, n2, ik)
       end do
 
     end do
@@ -160,7 +162,7 @@ subroutine X(lcao_wf) (lcao_data, st, m, h, start)
   nst = st%nst
 
   ! Hamiltonian and overlap matrices.
-  ALLOCATE(hpsi(np, dim), np*dim)
+  ALLOCATE(hpsi(m%np_part, dim), m%np_part*dim)
   do ik = 1, st%d%nik
     do n1 = 1, lcao_data%st%nst
       hpsi = M_ZERO
