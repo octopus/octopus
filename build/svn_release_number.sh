3ed75a3e826c31712b3311d367533ecea4db1fde
Heiko Appel <appel@fhi-berlin.mpg.de>
2006-07-28 18:34:07 +0000

    If available use svn info to extract the current revision number. In this way the find run is avoided. Gives a noticeable speedup on slow disks (e.g. Laptops).

--- build/svn_release_number.sh
+++ build/svn_release_number.sh
@@ -3,4 +3,8 @@
 # $Id$
 
 # this is meant to be called from octopus/src/Makefile.am
-find . -type f -exec grep --exclude="./ChangeLog" --binary-files=without-match '$Id:' \{\} \; | tr -d \#\!\* | awk '{print $3,"["$2,$4"]"}' | grep -v 'qw(' | sort | tail -1
+if [ -x "$(which svn)" ]; then
+	svn info | grep Revision | awk -F: '{print $2}' | tr -d [:space:]
+else
+	find . -type f -exec grep --exclude="./ChangeLog" --binary-files=without-match '$Id:' \{\} \; | tr -d \#\!\* | awk '{print $3,"["$2,$4"]"}' | grep -v 'qw(' | sort | tail -1
+fi
