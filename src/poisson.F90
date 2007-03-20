745bd8e0a620087053f316c01eab0915c89ace05
Xavier Andrade <xavier@tddft.org>
2007-03-20 18:05:21 +0000

    * Other line that had to be fixed.

--- src/poisson.F90
+++ src/poisson.F90
@@ -353,7 +353,7 @@ contains
       call correct_rho(corrector, gr%m, rho, rho_corrected, vh_correction)
       call poisson_fft(gr%m, pot, rho_corrected, average_to_zero = .true.)
 
-      pot = pot + vh_correction
+      pot(1:NP) = pot(1:NP) + vh_correction(1:NP)
       deallocate(rho_corrected, vh_correction)
 
     case(ISF)
