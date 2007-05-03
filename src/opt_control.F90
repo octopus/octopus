efe816e02498de1a06e3147279a87e4af3e16f6d
Alberto Castro <acastro@bifi.es>
2007-05-03 10:56:56 +0000

    Bug fixed; laser_end was called twice which led to a segmentation fault. The change in the test input file is just cosmetic; I have put the variable names instead of their number values, to make their meaning more clear.

--- src/opt_control.F90
+++ src/opt_control.F90
@@ -176,7 +176,6 @@ contains
 
     ! clean up
     nullify(h%ep%lasers(1)%numerical)
-    call laser_end(1, h%ep%lasers)
     deallocate(laser)
     deallocate(laser_tmp)
     call oct_iterator_end(iterator)
@@ -696,52 +695,6 @@ contains
 
       call pop_sub()
     end subroutine update_field
-
-
-!!$    ! ---------------------------------------------------------
-!!$    subroutine output()
-!!$      integer :: iunit, loop, ierr
-!!$
-!!$      call push_sub('opt_control.output')
-!!$      
-!!$      iunit = io_open('opt-control/info', action='write')
-!!$      write(iunit, '(a,i4)')    'Total Iterations = ', iterator%ctr_iter
-!!$      write(iunit, '(a,f14.8)') 'Last Overlap    = ', iterator%overlap
-!!$      write(iunit, '(a,f14.8)') 'Last Functional = ', iterator%functional
-!!$      write(iunit, '(a)') 
-!!$      write(iunit, '(a)')       'Best value of functional'
-!!$      write(iunit, '(a,i4)')    'Iteration  = ', iterator%bestJ_ctr_iter
-!!$      write(iunit, '(a,f14.8)') 'Overlap    = ', iterator%bestJ_J1
-!!$      write(iunit, '(a,f14.8)') 'Functional = ', iterator%bestJ
-!!$      write(iunit, '(a,f14.8)') 'Fluence = ',    iterator%bestJ_fluence
-!!$      write(iunit, '(a)') 
-!!$      write(iunit, '(a)')       'Best value of target functional'
-!!$      write(iunit, '(a,i4)')    'Iteration  = ', iterator%bestJ1_ctr_iter
-!!$      write(iunit, '(a,f14.8)') 'Overlap    = ', iterator%bestJ1
-!!$      write(iunit, '(a,es18.8)') 'Functional = ', iterator%bestJ1_J
-!!$      write(iunit, '(a,f14.8)') 'Fluence = ',    iterator%bestJ1_fluence
-!!$      call io_close(iunit)
-!!$      message(1) = "Info: Output States"
-!!$      call write_info(1)
-!!$      ! should output wavefunctions ;)
-!!$      
-!!$      ! dump convergence: J,P,fluence,penalty
-!!$      iunit = io_open('opt-control/convergence', action='write')
-!!$      ! header
-!!$      write(iunit, '(4(a))') '# iteration ','functional ','overlap ','penalty '
-!!$      ! data
-!!$      do loop=1,oct%ctr_iter_max
-!!$         write(iunit, '(i6,3f18.8,es20.10)') loop, iterator%convergence(1,loop), iterator%convergence(2,loop), &
-!!$           iterator%convergence(3,loop), iterator%convergence(4,loop)
-!!$      end do
-!!$      call io_close(iunit)
-!!$
-!!$      call states_output(psi, gr, 'opt-control', sys%outp)
-!!$      call zoutput_function(sys%outp%how, 'opt-control', 'target', gr%m, gr%sb, target_st%zpsi(:,1,1,1), M_ONE, ierr) 
-!!$      call zoutput_function(sys%outp%how, 'opt-control', 'final', gr%m, gr%sb, psi%zpsi(:,1,1,1), M_ONE, ierr) 
-!!$
-!!$      call pop_sub()
-!!$    end subroutine output
 
 
     !----------------------------------------------------------
