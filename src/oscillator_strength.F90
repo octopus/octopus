3a20e2bc3c0d237de1100b55e21acf4f206f270f
Alberto Castro <acastro@bifi.es>
2007-06-11 10:20:38 +0000

    Fixed a compilation issue: apparently in correct Fortran one should not pass an internal function as an argument to other procedure; it should be either external or contained in a module.

--- src/oscillator_strength.F90
+++ src/oscillator_strength.F90
@@ -19,7 +19,7 @@
 
 #include "global.h"
 
-program oscillator_strength
+module oscillator_strength_m
   use global_m
   use messages_m
   use datasets_m
@@ -33,13 +33,49 @@ program oscillator_strength
 
   implicit none
 
-  integer :: in_file(3), out_file(3), i, eq_axis, nspin, lmax, time_steps
-  logical :: calculate_tensor
-  type(spec_t) :: s
-  integer :: ierr
-  FLOAT :: omega, power, leftbound, rightbound, search_interval, total_time, dt
-  character(len=100) :: filename
+  FLOAT, allocatable :: ot(:)
+  type(kick_t) :: kick
+  integer :: time_steps, ntiter
+  FLOAT :: dt
 
+  contains
+
+    subroutine ft(omega, power)
+      FLOAT, intent(in)   :: omega
+      FLOAT, intent(out)  :: power
+      integer :: i, j
+      FLOAT :: x
+      power = M_ZERO
+      do j = 0, time_steps
+        x = sin(omega*j*dt)
+        power = power + x*ot(j)
+      end do
+      power = - ( power*dt / (-kick%delta_strength) ) / (dt*ntiter)
+    end subroutine ft
+
+end module oscillator_strength_m
+
+
+program oscillator_strength
+  use global_m
+  use messages_m
+  use datasets_m
+  use lib_oct_m
+  use lib_oct_parser_m
+  use io_m
+  use units_m
+  use spectrum_m
+  use lib_oct_m
+  use getopt_m
+  use oscillator_strength_m
+
+  implicit none
+
+  integer :: in_file(3), out_file(3), i, eq_axis, lmax
+  logical :: calculate_tensor
+  integer :: ierr, nspin
+  FLOAT :: omega, power, leftbound, rightbound, search_interval, total_time
+  character(len=100) :: filename
 
   ! Reads the information passed through the command line options (if available).
   call getopt_init(ierr)
@@ -63,14 +99,6 @@ program oscillator_strength
      call io_mkdir('debug')
   end if
 
-  s%damp        = SPECTRUM_DAMP_NONE
-  s%transform   = SPECTRUM_TRANSFORM_SIN
-  s%start_time  = M_ZERO
-  s%end_time    = -M_ONE
-  s%energy_step = CNST(0.001)
-  s%max_energy  = CNST(1.0)
-  s%damp_factor = M_ZERO
-
   call read_files(trim(filename))
 
   omega = omega * units_inp%energy%factor
@@ -78,13 +106,14 @@ program oscillator_strength
   leftbound = omega - search_interval
   rightbound = omega + search_interval
 
-  call loct_1dminimize(leftbound, rightbound, omega, func, ierr)
+  call loct_1dminimize(leftbound, rightbound, omega, ft, ierr)
   if(ierr.ne.0) then
     write(0,'(a)') 'Could not find a maximum.'
     stop
   end if
 
-  call ft(in_file(1), s, omega, power)
+  call ft(omega, power)
+  power = -power
   total_time = dt*time_steps
   power = power / (M_ONE - sin(M_TWO*omega*total_time)/(M_TWO*omega*total_time))
 
@@ -96,6 +125,7 @@ program oscillator_strength
                           ' '//trim(units_inp%length%abbrev)
   write(*, '(a,f14.8)')   'f[O->I]  = ', M_TWO*omega*power
 
+  deallocate(ot)
   call io_end()
   call datasets_end()
   call parser_end()
@@ -103,21 +133,15 @@ program oscillator_strength
 
   contains
 
-    subroutine func(x, fx)
-      FLOAT, intent(in) :: x
-      FLOAT, intent(out) :: fx
-      call ft(in_file(1), s, x, fx)
-      fx = -fx
-    end subroutine func
-
     subroutine read_files(fname)
       character(len=*), intent(in) :: fname
-      type(kick_t) :: kick
+      FLOAT :: dump
+      integer :: j, isp
+      FLOAT, allocatable :: dipole(:, :, :)
       
       in_file(1) = io_open(trim(fname), action='read', status='old', die=.false.)
       if(in_file(1) < 0) in_file(1) = io_open('td.general/'//trim(fname), action='read', status='old', die=.false.)
       if(in_file(1) >= 0) then
-        ! OK, so we only have one file. Now we have to see what it has inside.
         call spectrum_mult_info(in_file(1), nspin, kick, time_steps, dt, units_inp, lmax=lmax)
         ! This should not be like this.
         call units_get(units_out, 'eVA')
@@ -126,40 +150,21 @@ program oscillator_strength
         call write_fatal(1)
       end if
 
-    end subroutine read_files
-
-    subroutine ft(in_file, s, omega, power)
-      integer, intent(in) :: in_file
-      type(spec_t),  intent(inout) :: s
-      FLOAT, intent(in)   :: omega
-      FLOAT, intent(out)  :: power
-  
-      integer :: nspin, lmax, time_steps, is, ie, ntiter, i, j, jj, isp
-      type(kick_t) :: kick
-      FLOAT :: dt, dump, w, x
-      FLOAT, allocatable :: dipole(:, :, :)
-      type(unit_system_t) :: file_units
-  
-      ! This function gives us back the unit connected to the "multipoles" file, the header information,
-      ! the number of time steps, and the time step.
-      call spectrum_mult_info(in_file, nspin, kick, time_steps, dt, file_units, lmax=lmax)
-  
       ! Find out the iteration numbers corresponding to the time limits.
-      is = 0
-      ie = time_steps
-      ntiter = ie - is + 1
-  
+      ntiter = time_steps + 1
+
       ! Read the dipole.
-      call io_skip_header(in_file)
+      call io_skip_header(in_file(1))
       ALLOCATE(dipole(3, 0:time_steps, nspin), 3*(time_steps+1)*nspin)
+      ALLOCATE(ot(0:time_steps), time_steps+1)
       do i = 0, time_steps
         select case(nspin)
         case(1)
-          read(in_file, *) j, dump, dump, dipole(1:3, i, 1)
+          read(in_file(1), *) j, dump, dump, dipole(1:3, i, 1)
         case(2)
-          read(in_file, *) j, dump, dump, dipole(1:3, i, 1), dump, dipole(1:3, i, 2)
+          read(in_file(1), *) j, dump, dump, dipole(1:3, i, 1), dump, dipole(1:3, i, 2)
         case(4)
-          read(in_file, *) j, dump, dump, dipole(1:3, i, 1), dump, dipole(1:3, i, 2), &
+          read(in_file(1), *) j, dump, dump, dipole(1:3, i, 1), dump, dipole(1:3, i, 2), &
             dump, dipole(1:3, i, 3), dump, dipole(1:3, i, 4)
         end select
   
@@ -169,33 +174,17 @@ program oscillator_strength
       do i = time_steps, 0, -1
         dipole(:, i, :) = dipole(:, i, :) - dipole(:, 0, :)
       end do
-  
-      w = omega
-  
-      power = M_ZERO
-  
-      do j = is, ie
-        jj = j - is
-  
-        select case(s%transform)
-        case(SPECTRUM_TRANSFORM_SIN)
-          x = sin(w*jj*dt)
-        case(SPECTRUM_TRANSFORM_COS)
-          x = cos(w*jj*dt)
-        case(SPECTRUM_TRANSFORM_EXP)
-          x = exp(-w*jj*dt)
-        end select
-  
+
+      do i = 0, time_steps
+        ot(i) = M_ZERO
         do isp = 1, nspin
-          power = power + x*sum(dipole(1:3, j, isp) * kick%pol(1:3,kick%pol_dir))
+          ot(i) = ot(i) + sum(dipole(1:3, i, isp) * kick%pol(1:3,kick%pol_dir))
         end do
-  
       end do
-  
-      power = ( power*dt / (-kick%delta_strength) ) / (dt*ntiter)
-      call pop_sub()
-    end subroutine ft
-    
+
+      deallocate(dipole)
+    end subroutine read_files
+
 end program oscillator_strength
 
 !! Local Variables:
