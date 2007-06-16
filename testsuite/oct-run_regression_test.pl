dfa6abc5e58fcdf7563aaaaf9e0936845b4d3349
Florian Lorenzen <florian.lorenzen@posteo.de>
2007-06-16 12:56:04 +0000

    Generalized the short runtime and long runtime splitting of the testsuite. The Runtime tag in test test files has been replaced by a TestGroups tag which allows for the specification of a ';' separated lists of groupnames. The rationale is to split the testsuite into groups of tests like OCT tests, LR tests, but also weekly or daily tests. This is one step to overcome the problems with the long runtime of the full suite. The groups to be run are chosen by a -g flag of oct-run_testuite which takes a comma separated list of groupnames. I have converted the Runtime: indication into the two groups 'short-run' and 'long-run' but this is only preliminary. They should probably be changed into daily and weekly and the cron test runs should be changed to this as soon as they are switched on again. I have added a file TestGroups which simply contains a list of groups. Please refer to this file before adding another group to see if there is already something appropriate (and keep the file up to date); this is to avoid a one test per group scenario... (like for our variables/categories ratio). There is also a -q option which lists the testfiles of the groups specified by -g.
    Feel free to change the grouping I have done or to add new groups.
    
    (If no -g is given on the command line all tests are run.)

--- testsuite/oct-run_regression_test.pl
+++ testsuite/oct-run_regression_test.pl
@@ -1,5 +1,22 @@
 #!/usr/bin/perl
 #
+# Copyright (C) 2005 Heiko Appel
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
 # $Id$
 
 use Getopt::Std;
@@ -54,14 +71,14 @@ sub create_template {
 # -*- coding: utf-8 mode: shell-script -*-
 # $cvs_id
 
-Test     : $opt_c
-Author   : $author
-Date     : $date
-Arch     : $arch
-Release  : 2.2.0pre1
-Programs : octopus
-Runtime  : short-run
-Enabled  : Yes
+Test       : $opt_c
+Author     : $author
+Date       : $date
+Arch       : $arch
+Release    : 2.2.0pre1
+Programs   : octopus
+TestGroups : short-run
+Enabled    : Yes
 
 Input: 01-template.01-ground_state.inp
 
