f3ecad975865fedba954a2b7a043678fed9734b6
Florian Lorenzen <florian.lorenzen@posteo.de>
2007-09-20 21:13:23 +0000

    This implements the second block operation fully parallel. Now, the ground state is properly parallelized, apart from the fact, that these routines will have to be tuned and that the scaling behaviour of the implementation is currently unknown. But I still have one week left at MareNostrum to deal with these issues (and to find the leftover bugs).

--- src/states/states_inc.F90
+++ src/states/states_inc.F90
@@ -34,9 +34,14 @@ subroutine X(states_blockt_mul)(mesh, st, psi1, psi2, res, xpsi1, xpsi2, symm)
 
   logical              :: symm_
   integer              :: i, j
-  integer              :: m, n
+  integer              :: psi1_col, psi2_col
   integer, allocatable :: xpsi1_(:), xpsi2_(:)
-  R_TYPE, pointer      :: blk1(:, :, :), blk2(:, :, :)
+#if defined(HAVE_MPI)
+  integer              :: size, rank, node, round, partner, ist, res_col_offset, res_row_offset
+  integer, allocatable :: xpsi1_count(:), xpsi2_count(:), xpsi1_node(:, :), xpsi2_node(:, :)
+  integer              :: stat(MPI_STATUS_SIZE)
+  R_TYPE, allocatable  :: sendbuf(:, :, :), recvbuf(:, :, :), res_tmp(:, :)
+#endif
 
   call profiling_in(C_PROFILING_LOBPCG_BLOCKT)
   call push_sub('states_inc.Xstates_blockt_mul')
@@ -46,82 +51,176 @@ subroutine X(states_blockt_mul)(mesh, st, psi1, psi2, res, xpsi1, xpsi2, symm)
     symm_ = symm
   end if
 
+  ! Calculate index sets.
+  ! FIXME: this is the same as in Xstates_block_matr_mul_add and should be
+  ! put into an extra routine.
   if(present(xpsi1)) then
-    m = ubound(xpsi1, 1)
-    ALLOCATE(xpsi1_(m), m)
+    psi1_col = ubound(xpsi1, 1)
+    ALLOCATE(xpsi1_(psi1_col), psi1_col)
     xpsi1_ = xpsi1
   else
-    m = st%nst
-    ALLOCATE(xpsi1_(m), m)
-    do i = 1, m
+    psi1_col = st%nst
+    ALLOCATE(xpsi1_(psi1_col), psi1_col)
+    do i = 1, psi1_col
       xpsi1_(i) = i
     end do
   end if
   if(present(xpsi2)) then
-    n = ubound(xpsi2, 1)
-    ALLOCATE(xpsi2_(n), n)
+    psi2_col = ubound(xpsi2, 1)
+    ALLOCATE(xpsi2_(psi2_col), psi2_col)
     xpsi2_ = xpsi2
   else
-    n = st%nst
-    ALLOCATE(xpsi2_(n), n)
-    do i = 1, n
+    psi2_col = st%nst
+    ALLOCATE(xpsi2_(psi2_col), psi2_col)
+    do i = 1, psi2_col
       xpsi2_(i) = i
     end do
   end if
 
-  ! FIXME: write something sensible here!
-  ! Quick and dirty parallelization. In the parallel case, we simply copy
-  ! all states to all nodes. Otherwise, we put pointers to the input blocks.
   if(st%parallel_in_states) then
 #if defined(HAVE_MPI)
-    ! Allocate space for the blocks.
-    ALLOCATE(blk1(mesh%np_part, st%d%dim, st%nst), mesh%np_part*st%d%dim*st%nst)
-    ALLOCATE(blk2(mesh%np_part, st%d%dim, st%nst), mesh%np_part*st%d%dim*st%nst)
+    ! Shortcuts.
+    size = st%mpi_grp%size
+    rank = st%mpi_grp%rank
 
-    call X(states_gather)(mesh, st, psi1, blk1)
-    call X(states_gather)(mesh, st, psi2, blk2)
+    ! Calculate the index sets per node,
+    ! xpsi1_node(1:xpsi1_count(node), node) and
+    ! xpsi2_node(1:xpsi2_count(node), node) are the index sets.
+    ! FIXME: this is the same as in Xstates_block_matr_mul_add and should be
+    ! put into an extra routine.
+    ALLOCATE(xpsi1_count(0:size-1), size)
+    ALLOCATE(xpsi2_count(0:size-1), size)
+    ! Count the how many vectors each node has.
+    xpsi1_count = 0
+    xpsi2_count = 0
+    do i = 1, psi1_col
+      xpsi1_count(st%node(xpsi1_(i))) = xpsi1_count(st%node(xpsi1_(i))) + 1
+    end do
+    do i = 1, psi2_col
+      xpsi2_count(st%node(xpsi2_(i))) = xpsi2_count(st%node(xpsi2_(i))) + 1
+    end do
+    ! Allocate space, it is a bit more than really required.
+    ALLOCATE(xpsi1_node(maxval(xpsi1_count), 0:size-1), maxval(xpsi1_count)*size)
+    ALLOCATE(xpsi2_node(maxval(xpsi2_count), 0:size-1), maxval(xpsi2_count)*size)
+    ! Now set up the index sets.
+    xpsi1_count = 0
+    xpsi2_count = 0
+    xpsi1_node  = 0
+    xpsi2_node  = 0
+    do ist = 1, st%nst
+      node = st%node(ist)
+      ! A state ist is only included if its in the global index set.
+      if(member(ist, xpsi1_)) then
+        xpsi1_count(node)                   = xpsi1_count(node) + 1
+        xpsi1_node(xpsi1_count(node), node) = ist
+      end if
+      if(member(ist, xpsi2_)) then
+        xpsi2_count(node)                   = xpsi2_count(node) + 1
+        xpsi2_node(xpsi2_count(node), node) = ist
+      end if
+    end do
 
+    ! Has to be zero because we use an allreduce on this at the end.
+    res = R_TOTYPE(M_ZERO)
+
+    ! The local part.
+    res_row_offset = sum(xpsi1_count(0:rank-1))
+    res_col_offset = sum(xpsi2_count(0:rank-1))
     if(symm_) then
-      do i = 1, m
-        res(i, i) = X(states_dotp)(mesh, st%d%dim, blk1(:, :, xpsi1_(i)), blk2(:, :, xpsi2_(i)))
-        do j = i+1, n
-          res(i, j) = X(states_dotp)(mesh, st%d%dim, blk1(:, :, xpsi1_(i)), blk2(:, :, xpsi2_(j)))
-          res(j, i) = R_CONJ(res(i, j))
+      do i = 1, xpsi1_count(rank)
+        res(i+res_row_offset, i+res_col_offset) =                         &
+          X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_node(i, rank)), &
+          psi2(:, :, xpsi2_node(i, rank)), reduce=.false.)
+        do j = i+1, xpsi2_count(rank)
+          res(i+res_row_offset, j+res_col_offset) =                         &
+            X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_node(i, rank)), &
+            psi2(:, :, xpsi2_node(j, rank)), reduce=.false.)
+          res(j+res_col_offset, i+res_row_offset) = R_CONJ(res(i+res_row_offset, j+res_col_offset))
         end do
       end do
     else
-      do i = 1, m
-        do j = 1, n
-          res(i, j) = X(states_dotp)(mesh, st%d%dim, blk1(:, :, xpsi1_(i)), blk2(:, :, xpsi2_(j)))
+      do i = 1, xpsi1_count(rank)
+        do j = 1, xpsi2_count(rank)
+          res(i+res_row_offset, j+res_col_offset) =                         &
+            X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_node(i, rank)), &
+            psi2(:, :, xpsi2_node(j, rank)), reduce=.false.)
         end do
       end do
     end if
+  
+    ! The remote parts.
+    do round = 1, st%ap%rounds
+      partner = st%ap%schedule(rank, round)
 
-    deallocate(blk1, blk2)
+      if(partner.eq.rank) then ! We are idle this round.
+        ! Just do nothing.
+      else ! We are not idle.
+        ! Both nodes send psi2 to each other and then calculate one block of the
+        ! result.
+        ALLOCATE(sendbuf(mesh%np, st%d%dim, xpsi2_count(rank)), mesh%np*st%d%dim*xpsi2_count(rank))
+        ALLOCATE(recvbuf(mesh%np, st%d%dim, xpsi2_count(partner)), mesh%np*st%d%dim*xpsi2_count(partner))
+
+        ! Copy block to sendbuffer. This is necessaery because the vectors in the block may not
+        ! be contiguous.
+        do i = 1, xpsi2_count(rank)
+          call lalg_copy(mesh%np*st%d%dim, psi2(:, 1, xpsi2_node(i, rank)), sendbuf(:, 1, i))
+        end do
+
+        call MPI_Sendrecv(sendbuf, mesh%np*st%d%dim*xpsi2_count(rank), R_MPITYPE, partner, 12, &
+          recvbuf, mesh%np*st%d%dim*xpsi2_count(partner), R_MPITYPE, partner, 12, st%mpi_grp%comm, stat, mpi_err)
+
+        res_row_offset = sum(xpsi1_count(0:rank-1))
+        res_col_offset = sum(xpsi2_count(0:partner-1))
+        ! Do the matrix multiplication.
+        do i = 1, xpsi1_count(rank)
+          do j = 1, xpsi2_count(partner)
+            res(i+res_row_offset, j+res_col_offset) = &
+              X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_node(i, rank)), recvbuf(:, :, j), reduce=.false.)
+          end do
+        end do
+        deallocate(sendbuf, recvbuf)
+      end if
+    end do
+
+    ! Add up all the individual blocks.
+    ALLOCATE(res_tmp(psi1_col, psi2_col), psi1_col*psi2_col)
+    call MPI_Allreduce(res, res_tmp, psi1_col*psi2_col, R_MPITYPE, MPI_SUM, st%mpi_grp%comm, mpi_err)
+    ! What is the problem with this BLAS call?
+    ! call lalg_copy(psi1_col*psi2_col, res_tmp(:, 1), res(:, 1))
+    res = res_tmp
+    deallocate(res_tmp)
 #else
     message(1) = 'Running gs parallel in states without MPI. This is a bug!'
     call write_fatal(1)
 #endif
-  else
-    ! FIXME: This works with domain parallelization but the dotps should be blocked.
+  else ! No states parallelization.
     if(symm_) then
-      do i = 1, m
-        res(i, i) = X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_(i)), psi2(:, :, xpsi2_(i)))
-        do j = i+1, n
-          res(i, j) = X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_(i)), psi2(:, :, xpsi2_(j)))
+      do i = 1, psi1_col
+        res(i, i) = X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_(i)), psi2(:, :, xpsi2_(i)), reduce=.false.)
+        do j = i+1, psi2_col
+          res(i, j) = X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_(i)), psi2(:, :, xpsi2_(j)), reduce=.false.)
           res(j, i) = R_CONJ(res(i, j))
         end do
       end do
     else
-      do i = 1, m
-        do j = 1, n
-          res(i, j) = X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_(i)), psi2(:, :, xpsi2_(j)))
+      do i = 1, psi1_col
+        do j = 1, psi2_col
+          res(i, j) = X(states_dotp)(mesh, st%d%dim, psi1(:, :, xpsi1_(i)), psi2(:, :, xpsi2_(j)), reduce=.false.)
         end do
       end do
     end if
   end if
 
-  deallocate(xpsi1_, xpsi2_)
+  ! This has to be done because no reduction is done in Xstates_dotp above.
+  ! Rationale: this way, all the reduce operations are done at once (hopefully)
+  ! saving latency overhead.
+  ! FIXME: test this!
+  if(mesh%parallel_in_domains) then
+    ALLOCATE(res_tmp(psi1_col, psi2_col), psi1_col*psi2_col)
+    call MPI_Allreduce(res, res_tmp, psi1_col*psi2_col, R_MPITYPE, MPI_SUM, mesh%mpi_grp%comm, mpi_err)
+    call lalg_copy(psi1_col*psi2_col, res_tmp(:, 1), res(:, 1))
+    deallocate(res_tmp)
+  end if
 
   call pop_sub()
   call profiling_out(C_PROFILING_LOBPCG_BLOCKT)
@@ -159,7 +258,6 @@ subroutine X(states_gather)(mesh, st, in, out)
   call MPI_Alltoallv(in(:, 1, 1), sendcnts, sdispls, R_MPITYPE, &
     out(:, 1, 1), recvcnts, rdispls, R_MPITYPE, st%mpi_grp%comm, mpi_err)
   call MPI_Debug_Out(st%mpi_grp%comm, C_MPI_ALLTOALLV)
-
   deallocate(sendcnts, sdispls, recvcnts, rdispls)
 
   call pop_sub()
@@ -271,7 +369,7 @@ subroutine X(states_block_matr_mul_add)(mesh, st, alpha, psi, matr, beta, res, x
     xres_node  = 0
     do ist = 1, st%nst
       node = st%node(ist)
-      ! A state ist is ony incuded if its in the global index set.
+      ! A state ist is ony included if its in the global index set.
       if(member(ist, xpsi_)) then
         xpsi_count(node)                  = xpsi_count(node) + 1
         xpsi_node(xpsi_count(node), node) = ist
@@ -322,6 +420,9 @@ subroutine X(states_block_matr_mul_add)(mesh, st, alpha, psi, matr, beta, res, x
       to   = mod(rank + round, size)
       from = mod(size + rank - round, size)
 
+      ! MPI_Sendrecv might be interesting here (because we do a shift).
+      ! But interleaving computation with non-blocking communication is
+      ! probaby better.
       if(xres_count(to).gt.0) then
         matr_row_offset = sum(xpsi_count(0:rank-1))
         matr_col_offset = sum(xres_count(0:to-1))
@@ -378,7 +479,7 @@ subroutine X(states_block_matr_mul_add)(mesh, st, alpha, psi, matr, beta, res, x
         end do
       end do
       call profiling_out(C_PROFILING_LOBPCG_LOOP)
-      ! And also for alpha=beta=1.
+    ! And also for alpha=beta=1.
     else if(alpha.eq.R_TOTYPE(M_ONE).and.beta.eq.R_TOTYPE(M_ONE)) then
       call profiling_in(C_PROFILING_LOBPCG_LOOP)
       do j = 1, matr_col
@@ -391,7 +492,7 @@ subroutine X(states_block_matr_mul_add)(mesh, st, alpha, psi, matr, beta, res, x
         end do
       end do
       call profiling_out(C_PROFILING_LOBPCG_LOOP)
-      ! The general case.
+    ! The general case.
     else
       call profiling_in(C_PROFILING_LOBPCG_LOOP)
       do j = 1, matr_col
@@ -466,6 +567,7 @@ subroutine X(states_gram_schmidt1)(nst, m, dim, psi, start)
   call profiling_out(C_PROFILING_GRAM_SCHMIDT1)
 end subroutine X(states_gram_schmidt1)
 
+
 ! ---------------------------------------------------------
 ! Orthonormalizes phi to the nst orbitals psi.
 ! It also permits to do only the orthogonalization (no normalization).
@@ -518,10 +620,11 @@ end subroutine X(states_gram_schmidt2)
 
 
 ! ---------------------------------------------------------
-R_TYPE function X(states_dotp)(m, dim, f1, f2) result(dotp)
-  type(mesh_t),    intent(in) :: m
-  integer,         intent(in) :: dim
-  R_TYPE,          intent(in) :: f1(:,:), f2(:,:)
+R_TYPE function X(states_dotp)(m, dim, f1, f2, reduce) result(dotp)
+  type(mesh_t),      intent(in) :: m
+  integer,           intent(in) :: dim
+  R_TYPE,            intent(in) :: f1(:,:), f2(:,:)
+  logical, optional, intent(in) :: reduce
 
   integer :: idim
 
@@ -529,7 +632,7 @@ R_TYPE function X(states_dotp)(m, dim, f1, f2) result(dotp)
 
   dotp = R_TOTYPE(M_ZERO)
   do idim = 1, dim
-    dotp = dotp + X(mf_dotp)(m, f1(:, idim), f2(:, idim))
+    dotp = dotp + X(mf_dotp)(m, f1(:, idim), f2(:, idim), reduce)
   end do
 
   call pop_sub()
