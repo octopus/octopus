0ad9f9ba7a6a160b4c06d0e2a31f85ff5900b159
Xavier Andrade <xavier@tddft.org>
2008-02-25 19:12:00 +0000

    Bugfix: A non-initialized iunit was passed to hamiltonian_energy by non-root processes.

--- src/scf/scf.F90
+++ src/scf/scf.F90
@@ -630,6 +630,8 @@ contains
         write(iunit, '(1x)')
 
         write(iunit, '(a)') 'Energy:'
+      else
+        iunit = 0
       end if
 
       call hamiltonian_energy(h, gr, geo, st, iunit, full = .true.)
