d05c3e62a87076a4b9cb1c6d2a05f9eee29fc998
Xavier Andrade <xavier@tddft.org>
2008-02-27 18:50:54 +0000

    Implemented the separation of nl_operator for itanium

--- src/grid/nl_operator_inc.F90
+++ src/grid/nl_operator_inc.F90
@@ -241,7 +241,7 @@ subroutine X(nl_operator_operate)(op, fi, fo, ghost_update, profile, points)
   
   real(8) :: ws(100)
   logical :: profile_
-  integer :: points_, nri, nri_loc, ini
+  integer :: points_, nri, nri_loc, ini, nns(1:2)
   integer, pointer :: imin(:), imax(:), ri(:, :)
 #if defined(HAVE_MPI)
   logical :: update
@@ -311,16 +311,18 @@ subroutine X(nl_operator_operate)(op, fi, fo, ghost_update, profile, points)
       nri_loc = nri
 #endif
 
-      select case(op%X(function))
-      case(OP_FORTRAN)
-        call X(operate)(op%n, nri, op%w_re(:, 1), ri, imin, imax, fi, fo)
-      case(OP_C)
-        call X(operate_ri)(op%n, op%w_re(1, 1), nri_loc, ri(1, ini), imin(ini), imax(ini), fi(1), fo(1))
-      case(OP_VEC)
-        call X(operate_ri_vec)(op%n, op%w_re(1, 1), nri_loc, ri(1, ini), imin(ini), imax(ini), fi(1), fo(1))
-      case(OP_AS)
-        call X(operate_as)(op%n, op%w_re(1, 1), nri_loc, ri(1, ini), imin(ini), fi(1), fo(1), ws(1))
-      end select
+!      select case(op%X(function))
+!      case(OP_FORTRAN)
+!        call X(operate)(op%n, nri, op%w_re(:, 1), ri, imin, imax, fi, fo)
+!      case(OP_C)
+!        call X(operate_ri)(op%n, op%w_re(1, 1), nri_loc, ri(1, ini), imin(ini), imax(ini), fi(1), fo(1))
+!      case(OP_VEC)
+!        call X(operate_ri_vec)(op%n, op%w_re(1, 1), nri_loc, ri(1, ini), imin(ini), imax(ini), fi(1), fo(1))
+!      case(OP_AS)
+        nns(1) = op%n
+        nns(2) = nri_loc
+        call X(operate_as)(nns, op%w_re(1, 1), ri(1, ini), imin(ini), imax(ini), fi(1), fo(1), ws(1))
+!      end select
       !$omp end parallel
 
     end if
