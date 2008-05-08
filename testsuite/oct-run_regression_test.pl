da5f60e4db527d128fec3712462cbd15afa8a006
Xavier Andrade <xavier@tddft.org>
2008-05-08 11:18:56 +0000

    * Updated version tags to 3.0.1.

--- testsuite/oct-run_regression_test.pl
+++ testsuite/oct-run_regression_test.pl
@@ -80,7 +80,7 @@ Test       : $opt_c
 Author     : $author
 Date       : $date
 Arch       : $arch
-Release    : 3.0.0pre1
+Release    : 3.0.1
 Programs   : octopus
 TestGroups : short-run
 Enabled    : Yes
