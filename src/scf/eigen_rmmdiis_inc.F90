b62077be0442f1095d79e799345d7ddd7255dac7
Xavier Andrade <xavier@tddft.org>
2009-05-12 16:26:56 +0000

    The first iteration of diis is also done by blocks.

--- src/scf/eigen_rmmdiis_inc.F90
+++ src/scf/eigen_rmmdiis_inc.F90
@@ -346,87 +346,141 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
 end subroutine X(eigensolver_rmmdiis)
 
 subroutine X(eigensolver_rmmdiis_start) (gr, st, hm, pre, tol, niter, converged, ik, diff, blocksize)
-  type(grid_t),        intent(inout) :: gr
-  type(states_t),      intent(inout) :: st
-  type(hamiltonian_t), intent(inout) :: hm
-  type(preconditioner_t), intent(in) :: pre
-  FLOAT,               intent(in)    :: tol
-  integer,             intent(inout) :: niter
-  integer,             intent(inout) :: converged
-  integer,             intent(in)    :: ik
-  FLOAT,               intent(out)   :: diff(1:st%nst)
-  integer,             intent(in)    :: blocksize
+  type(grid_t),           intent(inout) :: gr
+  type(states_t),         intent(inout) :: st
+  type(hamiltonian_t),    intent(inout) :: hm
+  type(preconditioner_t), intent(in)    :: pre
+  FLOAT,                  intent(in)    :: tol
+  integer,                intent(inout) :: niter
+  integer,                intent(inout) :: converged
+  integer,                intent(in)    :: ik
+  FLOAT,                  intent(out)   :: diff(1:st%nst)
+  integer,                intent(in)    :: blocksize
 
   integer, parameter :: sweeps = 5
   integer, parameter :: sd_steps = 2
 
-  integer :: isweep, isd, ist, idim
-  R_TYPE  :: lambda
-  R_TYPE  :: ca, cb, cc, fr, rr, fhr, rhr
+  integer :: isweep, isd, ist, idim, sst, est, bsize, ib
+  R_TYPE  :: lambda, ca, cb, cc
 
-  R_TYPE, pointer     :: psi(:, :)
-  R_TYPE, allocatable :: res(:, :)
-  R_TYPE, allocatable :: kres(:, :)
+  R_TYPE, allocatable :: res(:, :, :)
+  R_TYPE, allocatable :: kres(:, :, :)
+  R_TYPE, allocatable :: me1(:, :), me2(:, :)
+#ifdef HAVE_MPI
+  R_TYPE, allocatable :: metmp(:, :)
+#endif
 
-  FLOAT :: fhf, ff
+  type(batch_t) :: psib, resb, kresb
 
   call push_sub('eigen_rmmdiis.Xeigensolver_rmmdiis_start')
 
-  SAFE_ALLOCATE( res(1:gr%mesh%np_part, 1:st%d%dim))
-  SAFE_ALLOCATE(kres(1:gr%mesh%np_part, 1:st%d%dim))
+  SAFE_ALLOCATE(me1(1:2, 1:blocksize))
+  SAFE_ALLOCATE(me2(1:4, 1:blocksize))
+  SAFE_ALLOCATE(res(1:gr%mesh%np_part, 1:st%d%dim, 1:blocksize))
+  SAFE_ALLOCATE(kres(1:gr%mesh%np_part, 1:st%d%dim, 1:blocksize))
+
   niter = 0
 
   do isweep = 1, sweeps
 
     if(isweep > 1) call X(subspace_diag)(gr, st, hm, ik)
 
-    do ist = st%st_start, st%st_end
+    do sst = st%st_start, st%st_end, blocksize
+      est = min(sst + blocksize - 1, st%st_end)
+      bsize = est - sst + 1
+
+      call batch_init(psib, st%d%dim, sst, est, st%X(psi)(:, :, sst:, ik))
+      call batch_init(resb, st%d%dim, sst, est, res)
+      call batch_init(kresb, st%d%dim, sst, est, kres)
+
       do isd = 1, sd_steps
-        
-        psi => st%X(psi)(:, :, ist, ik)
 
-        call X(hamiltonian_apply)(hm, gr, psi, res, ist, ik)
+        call X(hamiltonian_apply_batch)(hm, gr, psib, resb, ik)
 
-        fhf = X(mf_dotp)(gr%mesh, st%d%dim, psi, res)
-        ff = X(mf_dotp)(gr%mesh, st%d%dim, psi, psi)
+        do ist = sst, est
+          ib = ist - sst + 1
 
-        st%eigenval(ist, ik) = fhf/ff
+          me1(1, ib) = X(mf_dotp)(gr%mesh, st%d%dim, st%X(psi)(:, :, ist, ik), res(:, :, ib), reduce = .false.)
+          me1(2, ib) = X(mf_dotp)(gr%mesh, st%d%dim, st%X(psi)(:, :, ist, ik), st%X(psi)(:, :, ist, ik), reduce = .false.)
 
-        do idim = 1, st%d%dim
-          call lalg_axpy(gr%mesh%np, -st%eigenval(ist, ik), psi(:, idim), res(:, idim))
         end do
+
+#ifdef HAVE_MPI
+        if(gr%mesh%parallel_in_domains) then
+          SAFE_ALLOCATE(metmp(1:2, 1:blocksize))
+          metmp = me1
+          call MPI_Allreduce(metmp, me1, 2*blocksize, R_MPITYPE, MPI_SUM, gr%mesh%mpi_grp%comm, mpi_err)
+          SAFE_DEALLOCATE_A(metmp)
+        end if
+#endif
+
+        do ist = sst, est
+          ib = ist - sst + 1
+
+          st%eigenval(ist, ik) = me1(1, ib)/me1(2, ib)
+
+          do idim = 1, st%d%dim
+            call lalg_axpy(gr%mesh%np, -st%eigenval(ist, ik), st%X(psi)(:, idim, ist, ik), res(:, idim, ib))
+          end do
+        end do
+
+        call X(preconditioner_apply_batch)(pre, gr, hm, resb, kresb)
         
-        call X(preconditioner_apply)(pre, gr, hm, res, kres)
+        call X(hamiltonian_apply_batch)(hm, gr, kresb, resb, ik)
 
-        call X(hamiltonian_apply)(hm, gr, kres, res, ist, ik)
-        niter = niter + 2
+        niter = niter + 2*bsize
 
-        rr  = X(mf_dotp)(gr%mesh, st%d%dim, kres, kres)
-        fr  = X(mf_dotp)(gr%mesh, st%d%dim, psi,  kres)
-        rhr = X(mf_dotp)(gr%mesh, st%d%dim, kres, res)
-        fhr = X(mf_dotp)(gr%mesh, st%d%dim, psi,  res)
+        do ist = sst, est
+          ib = ist - sst + 1
 
-        ca = rr*fhr - rhr*fr
-        cb = ff*rhr - fhf*rr
-        cc = fhf*fr - ff*fhr
+          me2(1, ib) = X(mf_dotp)(gr%mesh, st%d%dim, kres(:, :, ib), kres(:, :, ib), reduce = .false.)
+          me2(2, ib) = X(mf_dotp)(gr%mesh, st%d%dim, st%X(psi)(:, :, ist, ik),  kres(:, :, ib), reduce = .false.)
+          me2(3, ib) = X(mf_dotp)(gr%mesh, st%d%dim, kres(:, :, ib), res(:, :, ib), reduce = .false.)
+          me2(4, ib) = X(mf_dotp)(gr%mesh, st%d%dim, st%X(psi)(:, :, ist, ik),  res(:, :, ib), reduce = .false.)
 
-        lambda = 2*cc/(cb + sqrt(cb**2 - CNST(4.0)*ca*cc))
+        end do
 
-        do idim = 1, st%d%dim
-          call lalg_axpy(gr%mesh%np, lambda, kres(:, idim), psi(:, idim))
+#ifdef HAVE_MPI
+        if(gr%mesh%parallel_in_domains) then
+          SAFE_ALLOCATE(metmp(1:4, 1:blocksize))
+          metmp = me2
+          call MPI_Allreduce(metmp, me2, 4*blocksize, R_MPITYPE, MPI_SUM, gr%mesh%mpi_grp%comm, mpi_err)
+          SAFE_DEALLOCATE_A(metmp)
+        end if
+#endif
+
+        do ist = sst, est
+          ib = ist - sst + 1
+
+          ca = me2(1, ib)*me2(4, ib) - me2(3, ib)*me2(2, ib)
+          cb = me1(2, ib)*me2(3, ib) - me1(1, ib)*me2(1, ib)
+          cc = me1(1, ib)*me2(2, ib) - me1(2, ib)*me2(4, ib)
+
+          lambda = CNST(2.0)*cc/(cb + sqrt(cb**2 - CNST(4.0)*ca*cc))
+
+          do idim = 1, st%d%dim
+            call lalg_axpy(gr%mesh%np, lambda, kres(:, idim, ib), st%X(psi)(:, idim, ist, ik))
+          end do
+
         end do
 
       end do
 
+      call batch_end(psib)
+      call batch_end(resb)
+      call batch_end(kresb)
+
     end do
-    
+
     if(mpi_grp_is_root(mpi_world)) then
-      call loct_progress_bar(st%nst*(ik - 1) +  (ist*(isweep -1))/sweeps, st%nst*st%d%nik)
+      call loct_progress_bar(st%nst*(ik - 1) +  (ist*(isweep - 1))/sweeps, st%nst*st%d%nik)
     end if
 
     call X(states_gram_schmidt_full)(st, st%nst, gr%mesh, st%d%dim, st%X(psi)(:, :, :, ik))
   end do
-  
+
+  SAFE_DEALLOCATE_A(me1)
+  SAFE_DEALLOCATE_A(me2)
   SAFE_DEALLOCATE_A(res)
   SAFE_DEALLOCATE_A(kres)
 
