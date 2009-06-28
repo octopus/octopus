a7ffb59e7e68ca33190452d6fdef6b1c360ab56f
Xavier Andrade <xavier@tddft.org>
2009-06-28 20:58:31 +0000

    Small improvements in zoperate_bg.

--- src/beak/operate_bg.c
+++ src/beak/operate_bg.c
@@ -36,44 +36,38 @@ void FC_FUNC_(zoperate_bg,ZOPERATE_BG)(const int * opn,
 				       const int * opri,
 				       const int * rimap_inv,
 				       const int * rimap_inv_max,
-				       const double * fi, 
+				       const double * restrict fi, 
 				       double * restrict fo){
+
   const int n = opn[0];
   const int nri = opnri[0];
 
-  int l, i, j;
+  int l, i, j, indexj;
   const int * restrict index;
-  const int * restrict index1;
-  const double * ffi[MAX_OP_N];
   register double _Complex a0, a1, a2, a3, a4, a5, a6, a7;
-  register ww;
+
+  __alignx(16, fi);
+  __alignx(16, fo);
+  __alignx(16, w);
 
   for (l = 0; l < nri ; l++) {
 
     index = opri + n*l;
 
     i = rimap_inv[l];
-
-    a0 = __cmplx(0.0, 0.0);
-    for(j = 0; j < n ; j++) {
-      ffi[j] = fi + 2*index[j];
-      a0 = __fxcpmadd(a0, __lfpd(ffi[j] + 2*i), w[j]);
-    }
-    __stfpd(fo + 2*i, a0);
-    i++;
-
-    for (; i < (rimap_inv_max[l] - 8 + 1) ; i+=8){
+    for (; i < (rimap_inv_max[l] - 8 + 1) ; i += 8){
 
       a0 = a1 = a2 = a3 = a4 = a5 = a6 = a7 = __cmplx(0.0, 0.0);
       for(j = 0; j < n; j++) {
-	a0 = __fxcpmadd(a0, __lfpd(ffi[j] + 2*i +  0), w[j]);
-	a1 = __fxcpmadd(a1, __lfpd(ffi[j] + 2*i +  2), w[j]);
-	a2 = __fxcpmadd(a2, __lfpd(ffi[j] + 2*i +  4), w[j]);
-	a3 = __fxcpmadd(a3, __lfpd(ffi[j] + 2*i +  6), w[j]);
-	a4 = __fxcpmadd(a4, __lfpd(ffi[j] + 2*i +  8), w[j]);
-	a5 = __fxcpmadd(a5, __lfpd(ffi[j] + 2*i + 10), w[j]);
-	a6 = __fxcpmadd(a6, __lfpd(ffi[j] + 2*i + 12), w[j]);
-	a7 = __fxcpmadd(a7, __lfpd(ffi[j] + 2*i + 14), w[j]);
+	indexj = 2*(index[j] + i);
+	a0 = __fxcpmadd(a0, __lfpd(fi + indexj +  0), w[j]);
+	a1 = __fxcpmadd(a1, __lfpd(fi + indexj +  2), w[j]);
+	a2 = __fxcpmadd(a2, __lfpd(fi + indexj +  4), w[j]);
+	a3 = __fxcpmadd(a3, __lfpd(fi + indexj +  6), w[j]);
+	a4 = __fxcpmadd(a4, __lfpd(fi + indexj +  8), w[j]);
+	a5 = __fxcpmadd(a5, __lfpd(fi + indexj + 10), w[j]);
+	a6 = __fxcpmadd(a6, __lfpd(fi + indexj + 12), w[j]);
+	a7 = __fxcpmadd(a7, __lfpd(fi + indexj + 14), w[j]);
       }
       __stfpd(fo + 2*i +  0, a0);
       __stfpd(fo + 2*i +  2, a1);
@@ -88,7 +82,7 @@ void FC_FUNC_(zoperate_bg,ZOPERATE_BG)(const int * opn,
     for (; i < rimap_inv_max[l]; i++){
       a0 = __cmplx(0.0, 0.0);
       for(j = 0; j < n; j++) {
-	a0 = __fxcpmadd(a0, __lfpd(ffi[j] + 2*i), w[j]);
+	a0 = __fxcpmadd(a0, __lfpd(fi + 2*index[j] + 2*i), w[j]);
       }
       __stfpd(fo + 2*i, a0);
     }
