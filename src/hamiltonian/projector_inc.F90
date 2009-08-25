e835fe429d28bf99f426ee471f65df8001929f29
Xavier Andrade <xavier@tddft.org>
2009-08-25 07:16:38 +0000

    Several lines of the parallel code were too long.

--- src/hamiltonian/projector_inc.F90
+++ src/hamiltonian/projector_inc.F90
@@ -159,7 +159,8 @@ subroutine X(project_psi_batch)(mesh, pj, npj, dim, psib, ppsib, ik)
     SAFE_ALLOCATE(reduce_buffer_dest(1:nreduce))
     call lalg_copy(nreduce, reduce_buffer, reduce_buffer_dest)
 #endif
-    call MPI_Allreduce(MPI_IN_PLACE_OR(reduce_buffer_dest(1)), reduce_buffer(1), nreduce, R_MPITYPE, MPI_SUM, mesh%vp%comm, mpi_err)
+    call MPI_Allreduce(MPI_IN_PLACE_OR(reduce_buffer_dest(1)), reduce_buffer(1), nreduce, &
+      R_MPITYPE, MPI_SUM, mesh%vp%comm, mpi_err)
     SAFE_DEALLOCATE_A(reduce_buffer_dest)
     call profiling_out(reduce_prof)
   end if
@@ -309,7 +310,8 @@ R_TYPE function X(psia_project_psib)(pj, dim, psia, psib, ik) result(apb)
       SAFE_ALLOCATE(uvpsi_tmp(1:pj%reduce_size, 1:2, 0:pj%lmax, -pj%lmax:pj%lmax))
       uvpsi_tmp = R_TOTYPE(M_ZERO)
       size = pj%reduce_size*2*(pj%lmax + 1)*(2*pj%lmax + 1)
-      call MPI_Allreduce(uvpsi(1, 1, 0, -pj%lmax), uvpsi_tmp(1, 1, 0, -pj%lmax), size, R_MPITYPE, MPI_SUM, mesh%vp%comm, mpi_err)
+      call MPI_Allreduce(uvpsi(1, 1, 0, -pj%lmax), uvpsi_tmp(1, 1, 0, -pj%lmax), size, &
+        R_MPITYPE, MPI_SUM, mesh%vp%comm, mpi_err)
       uvpsi = uvpsi_tmp
       SAFE_DEALLOCATE_A(uvpsi_tmp)
     end if
--- src/system/forces_inc.F90
+++ src/system/forces_inc.F90
@@ -205,7 +205,8 @@ subroutine X(forces_from_potential)(gr, geo, ep, st, time, lr, lr2, lr_dir, Born
     !reduce the gradient of the density
     SAFE_ALLOCATE(grad_rho_local(1:np, 1:gr%mesh%sb%dim))
     call lalg_copy(np, gr%mesh%sb%dim, grad_rho, grad_rho_local)
-    call MPI_Allreduce(grad_rho_local(1, 1), grad_rho(1, 1), np*gr%mesh%sb%dim, MPI_CMPLX, MPI_SUM, st%mpi_grp%comm, mpi_err)
+    call MPI_Allreduce(grad_rho_local(1, 1), grad_rho(1, 1), np*gr%mesh%sb%dim, MPI_CMPLX, &
+      MPI_SUM, st%mpi_grp%comm, mpi_err)
     SAFE_DEALLOCATE_A(grad_rho_local)
 
     call profiling_out(prof)
@@ -219,13 +220,15 @@ subroutine X(forces_from_potential)(gr, geo, ep, st, time, lr, lr2, lr_dir, Born
     !reduce the force
     SAFE_ALLOCATE(force_local(1:gr%mesh%sb%dim, 1:geo%natoms))
     force_local = force
-    call MPI_Allreduce(force_local(1, 1), force(1, 1), gr%mesh%sb%dim*geo%natoms, MPI_CMPLX, MPI_SUM, st%d%kpt%mpi_grp%comm, mpi_err)
+    call MPI_Allreduce(force_local(1, 1), force(1, 1), gr%mesh%sb%dim*geo%natoms, MPI_CMPLX, &
+      MPI_SUM, st%d%kpt%mpi_grp%comm, mpi_err)
     SAFE_DEALLOCATE_A(force_local)
     
     !reduce the gradient of the density
     SAFE_ALLOCATE(grad_rho_local(1:np, 1:gr%mesh%sb%dim))
     call lalg_copy(np, gr%mesh%sb%dim, grad_rho, grad_rho_local)
-    call MPI_Allreduce(grad_rho_local(1, 1), grad_rho(1, 1), np*gr%mesh%sb%dim, MPI_CMPLX, MPI_SUM, st%d%kpt%mpi_grp%comm, mpi_err)
+    call MPI_Allreduce(grad_rho_local(1, 1), grad_rho(1, 1), np*gr%mesh%sb%dim, MPI_CMPLX, &
+      MPI_SUM, st%d%kpt%mpi_grp%comm, mpi_err)
     SAFE_DEALLOCATE_A(grad_rho_local)
 
     call profiling_out(prof)
