4092fd6e1f05f2aac0d3ae9a396a9ea32399abd1
Xavier Andrade <xavier@tddft.org>
2009-10-30 15:33:05 +0000

    Commented the code that was causing the periodic system tests to fails.

--- src/hamiltonian/epot.F90
+++ src/hamiltonian/epot.F90
@@ -510,10 +510,12 @@ contains
     if (ep%classical_pot > 0)     ep%vpsl(1:mesh%np) = ep%vpsl(1:mesh%np) + ep%Vclassical(1:mesh%np)
     if (associated(ep%e_field)) ep%vpsl(1:mesh%np) = ep%vpsl(1:mesh%np) + ep%v_static(1:mesh%np)
 
-   if (.not. poisson_solver_has_free_bc()) then
-        ep%eii= M_ZERO
-        call ion_interaction_not_free_bc(gr, geo, ep) 
-   endif
+    !! this is disabled because it doesn't work for periodic systems
+    !    if (.not. poisson_solver_has_free_bc()) then
+    !      ep%eii= M_ZERO
+    !      call ion_interaction_not_free_bc(gr, geo, ep) 
+    !    endif
+
     call pop_sub()
     call profiling_out(epot_generate_prof)
   end subroutine epot_generate
