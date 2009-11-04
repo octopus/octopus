3c583fa7d5ffdf6e17a005e2f7a83f044366b625
Xavier Andrade <xavier@tddft.org>
2009-11-04 11:07:46 +0000

    3.1.x: Declared UPF pseudopotentials as development version.

--- src/species/species.F90
+++ src/species/species.F90
@@ -427,7 +427,10 @@ contains
 
       call loct_parse_block_float (blk, row, 3, s%Z)
 
-      if(s%type == SPEC_PS_UPF) read_data = 4
+      if(s%type == SPEC_PS_UPF) then 
+        read_data = 4
+        call messages_devel_version("UPF pseudopotentials support")
+      end if
 
       if(n>4) then
         call loct_parse_block_int (blk, row, 4, s%lmax)
