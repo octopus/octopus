a3d3fc308d77ae16470054e9549bc3a7b248c9ea
Fulvio Berardi <fulvio.berardi@gmail.com>
2010-06-18 11:34:08 +0000

    Change a wrong minus sign in the calculation of tau

--- src/states/states.F90
+++ src/states/states.F90
@@ -2422,9 +2422,9 @@ return
                 ww*aimag(conjg(wf_psi(1:der%mesh%np, 1))*gwf_psi(1:der%mesh%np, i_dim, 1))
             if(present( tau)) then
               tau (1:der%mesh%np, is)        = tau (1:der%mesh%np, is)        + &
-                ww*abs(gwf_psi(1:der%mesh%np, i_dim, 1))**2 - &
-                ww*abs(st%d%kpoints(i_dim, ik))**2*abs(wf_psi(1:der%mesh%np, 1))**2 + &
-                ww*2*aimag(-CONJG(wf_psi(1:der%mesh%np, 1))*st%d%kpoints(i_dim, ik)*gwf_psi(1:der%mesh%np, i_dim, 1) )
+                ww*abs(gwf_psi(1:der%mesh%np, i_dim, 1))**2  &
+                + ww*abs(st%d%kpoints(i_dim, ik))**2*abs(wf_psi(1:der%mesh%np, 1))**2  &
+                - ww*M_TWO*aimag(conjg(wf_psi(1:der%mesh%np, 1))*st%d%kpoints(i_dim, ik)*gwf_psi(1:der%mesh%np, i_dim, 1) )
             end if
 
             if(st%d%ispin == SPINORS) then
