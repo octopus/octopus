e54bc30f9b49f8a27ff5f79b5a84274bfbf1855a
David Strubbe <dstrubbe@ucmerced.edu>
2011-03-28 23:13:37 +0000

    Actually, we can and do use spinors in dimensions other than 3: the spatial dimensions are different, but the spin vector exists in 3 dimensions regardless. Now the test finite_systems_2d/05-spin_precession.test will proceed again.

--- src/states/states.F90
+++ src/states/states.F90
@@ -249,17 +249,13 @@ contains
     !%Option spinors 3
     !% The spin-orbitals are two-component spinors. This effectively allows the spin-density to
     !% be oriented non-collinearly: <i>i.e.</i> the magnetization vector is allowed to take different
-    !% directions at different points. (Only implemented in 3D.)
+    !% directions at different points. This vector is always in 3D regardless of <tt>Dimensions</tt>.
     !%End
     call parse_integer(datasets_check('SpinComponents'), UNPOLARIZED, st%d%ispin)
     if(.not.varinfo_valid_option('SpinComponents', st%d%ispin)) call input_error('SpinComponents')
     call messages_print_var_option(stdout, 'SpinComponents', st%d%ispin)
     ! Use of spinors requires complex wavefunctions.
     if (st%d%ispin == SPINORS) st%priv%wfs_type = TYPE_CMPLX
-    if (st%d%ispin == SPINORS .and. gr%sb%dim /= 3) then
-      message(1) = "Spinors only implemented for Dimensions = 3."
-      call messages_fatal(1)
-    endif
 
     !%Variable ExcessCharge
     !%Type float
