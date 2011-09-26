5d4958ef2ba25a3eee719e3ee60507668df3ab5c
Xavier Andrade <xavier@tddft.org>
2011-09-26 17:18:15 +0000

    The convergence check in the rmmdiis solver is now done by blocks.

--- src/system/eigen_rmmdiis_inc.F90
+++ src/system/eigen_rmmdiis_inc.F90
@@ -33,9 +33,9 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
 
   R_TYPE, allocatable :: res(:, :, :, :)
   R_TYPE, allocatable :: psi(:, :, :, :)
-  R_TYPE, allocatable :: mm(:, :, :, :), evec(:, :)
+  R_TYPE, allocatable :: mm(:, :, :, :), evec(:, :, :)
   R_TYPE, allocatable :: eigen(:)
-  FLOAT,  allocatable :: eval(:)
+  FLOAT,  allocatable :: eval(:, :)
   FLOAT, allocatable :: lambda(:)
   integer :: ist, minst, idim, ip, ii, iter, nops, maxst, jj, bsize
   R_TYPE :: ca, cb, cc
@@ -50,11 +50,11 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
 
   SAFE_ALLOCATE(psi(1:gr%mesh%np_part, 1:st%d%dim, 1:niter, 1:blocksize))
   SAFE_ALLOCATE(res(1:gr%mesh%np_part, 1:st%d%dim, 1:niter, 1:blocksize))
-  SAFE_ALLOCATE(lambda(1:blocksize))
+  SAFE_ALLOCATE(lambda(1:st%nst))
   SAFE_ALLOCATE(psib(1:niter))
   SAFE_ALLOCATE(resb(1:niter))
-  SAFE_ALLOCATE(  done(1:blocksize))
-  SAFE_ALLOCATE(  last(1:blocksize))
+  SAFE_ALLOCATE(done(1:blocksize))
+  SAFE_ALLOCATE(last(1:blocksize))
   SAFE_ALLOCATE(failed(1:blocksize))
   SAFE_ALLOCATE(fr(1:4, 1:blocksize))
 
@@ -105,31 +105,25 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
 
     ! initialize the batch objects
     do iter = 1, niter
-      call batch_init(psib(iter), st%d%dim, maxst - minst + 1 - sum(done))
-      call batch_init(resb(iter), st%d%dim, maxst - minst + 1 - sum(done))
-
+      call batch_init(psib(iter), st%d%dim, maxst - minst + 1)
+      call batch_init(resb(iter), st%d%dim, maxst - minst + 1)
+      
       do ist = minst, maxst
         ii = ist - minst + 1
-        if(done(ii) /= 0) cycle
-
         call batch_add_state(psib(iter), ist, psi(:, :, iter, ii))
         call batch_add_state(resb(iter), ist, res(:, :, iter, ii))
       end do
     end do
-
+    
     ! get lambda 
     call X(preconditioner_apply_batch)(pre, gr, hm, ik, resb(1), psib(2))
     call X(hamiltonian_apply_batch)(hm, gr%der, psib(2), resb(2), ik)
-    nops = nops + bsize - sum(done)
+    nops = nops + bsize
 
-    do ist = minst, maxst
-      ii = ist - minst + 1
-
-      fr(1, ii) = X(mf_dotp)(gr%mesh, st%d%dim, psi(:, :, 2, ii), psi(:, :, 2, ii), reduce = .false.)
-      fr(2, ii) = X(mf_dotp)(gr%mesh, st%d%dim, psi(:, :, 1, ii), psi(:, :, 2, ii), reduce = .false.)
-      fr(3, ii) = X(mf_dotp)(gr%mesh, st%d%dim, psi(:, :, 2, ii), res(:, :, 2, ii), reduce = .false.)
-      fr(4, ii) = X(mf_dotp)(gr%mesh, st%d%dim, psi(:, :, 1, ii), res(:, :, 2, ii), reduce = .false.)
-    end do
+    call X(mesh_batch_dotp_vector)(gr%mesh, psib(2), psib(2), fr(1, :), reduce = .false.)
+    call X(mesh_batch_dotp_vector)(gr%mesh, psib(1), psib(2), fr(2, :), reduce = .false.)
+    call X(mesh_batch_dotp_vector)(gr%mesh, psib(2), resb(2), fr(3, :), reduce = .false.)
+    call X(mesh_batch_dotp_vector)(gr%mesh, psib(1), resb(2), fr(4, :), reduce = .false.)
 
     if(gr%mesh%parallel_in_domains) call comm_allreduce(gr%mesh%mpi_grp%comm, fr)
 
@@ -140,11 +134,11 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
       cb = fr(3, ii) - st%eigenval(ist, ik)*fr(1, ii)
       cc = st%eigenval(ist, ik)*fr(2, ii) - fr(4, ii)
 
-      lambda(ii) = CNST(2.0)*cc/(cb + sqrt(cb**2 - CNST(4.0)*ca*cc))
+      lambda(ist) = CNST(2.0)*cc/(cb + sqrt(cb**2 - CNST(4.0)*ca*cc))
 
       ! restrict the value of lambda to be between 0.1 and 1.0
-      if(abs(lambda(ii)) > CNST(1.0)) lambda(ii) = lambda(ii)/abs(lambda(ii))
-      if(abs(lambda(ii)) < CNST(0.1)) lambda(ii) = CNST(0.1)*lambda(ii)/abs(lambda(ii))
+      if(abs(lambda(ist)) > CNST(1.0)) lambda(ist) = lambda(ist)/abs(lambda(ist))
+      if(abs(lambda(ist)) < CNST(0.1)) lambda(ist) = CNST(0.1)*lambda(ist)/abs(lambda(ist))
     end do
 
     do iter = 2, niter
@@ -155,31 +149,25 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
       do ist = minst, maxst
         ii = ist - minst + 1
 
-        if(done(ii) /= 0) cycle
-
         ! predict by jacobi
         forall(idim = 1:st%d%dim, ip = 1:gr%mesh%np)
-          psi(ip, idim, iter, ii) = lambda(ii)*psi(ip, idim, iter, ii) + psi(ip, idim, iter - 1, ii)
+          psi(ip, idim, iter, ii) = lambda(ist)*psi(ip, idim, iter, ii) + psi(ip, idim, iter - 1, ii)
         end forall
       end do
 
       ! calculate the residual
       call X(hamiltonian_apply_batch)(hm, gr%der, psib(iter), resb(iter), ik)
-      nops = nops + bsize - sum(done)
+      nops = nops + bsize
 
-      SAFE_ALLOCATE(  mm(1:iter, 1:iter, 1:2, 1:bsize))
-      SAFE_ALLOCATE(evec(1:iter, 1:1))
-      SAFE_ALLOCATE(eval(1:iter))
+      SAFE_ALLOCATE(mm(1:iter, 1:iter, 1:2, 1:bsize))
+      SAFE_ALLOCATE(evec(1:iter, 1:1, 1:blocksize))
+      SAFE_ALLOCATE(eval(1:iter, 1:blocksize))
+
+      call batch_axpy(gr%mesh%np, -st%eigenval(:, ik), psib(iter), resb(iter))
 
       do ist = minst, maxst
         ii = ist - minst + 1
-
-        if(done(ii) /= 0 .and. .not. failed(ii)) cycle
-
-        do idim = 1, st%d%dim
-          call lalg_axpy(gr%mesh%np, -st%eigenval(ist, ik), psi(:, idim, iter, ii), res(:, idim, iter, ii))
-        end do
-
+        
         ! perform the diis correction
 
         !  mm_1(i, j) = <res_i|res_j>
@@ -199,10 +187,8 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
       do ist = minst, maxst
         ii = ist - minst + 1
 
-        if(done(ii) /= 0 .and. .not. failed(ii)) cycle
-
         failed(ii) = .false.
-        call lalg_lowest_geneigensolve(1, iter, mm(:, :, 1, ii), mm(:, :, 2, ii), eval, evec, bof = failed(ii))
+        call lalg_lowest_geneigensolve(1, iter, mm(:, :, 1, ii), mm(:, :, 2, ii), eval(:, ii), evec(:, :, ii), bof = failed(ii))
         if(failed(ii)) then
           last(ii) = iter - 1
           cycle
@@ -212,14 +198,14 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
         ! might be recalculated instead but that seems to be a bit
         ! slower).
         do idim = 1, st%d%dim
-          call lalg_scal(gr%mesh%np, evec(iter, 1), psi(:, idim, iter, ii))
-          call lalg_scal(gr%mesh%np, evec(iter, 1), res(:, idim, iter, ii))
+          call lalg_scal(gr%mesh%np, evec(iter, 1, ii), psi(:, idim, iter, ii))
+          call lalg_scal(gr%mesh%np, evec(iter, 1, ii), res(:, idim, iter, ii))
         end do
 
         do jj = 1, iter - 1
           do idim = 1, st%d%dim
-            call lalg_axpy(gr%mesh%np, evec(jj, 1), psi(:, idim, jj, ii), psi(:, idim, iter, ii))
-            call lalg_axpy(gr%mesh%np, evec(jj, 1), res(:, idim, jj, ii), res(:, idim, iter, ii))
+            call lalg_axpy(gr%mesh%np, evec(jj, 1, ii), psi(:, idim, jj, ii), psi(:, idim, iter, ii))
+            call lalg_axpy(gr%mesh%np, evec(jj, 1, ii), res(:, idim, jj, ii), res(:, idim, iter, ii))
           end do
         end do
 
@@ -239,14 +225,12 @@ subroutine X(eigensolver_rmmdiis) (gr, st, hm, pre, tol, niter, converged, ik, d
     do ist = minst, maxst
       ii = ist - minst + 1
 
-      if(done(ii) /= 0) cycle
-
       if(.not. failed(ii)) then
         ! end with a trial move
         call X(preconditioner_apply)(pre, gr, hm, ik, res(:, :, iter - 1, ii), st%X(psi)(: , :, ist, ik))
 
         forall (idim = 1:st%d%dim, ip = 1:gr%mesh%np)
-          st%X(psi)(ip, idim, ist, ik) = psi(ip, idim, iter - 1, ii) + lambda(ii) * st%X(psi)(ip, idim, ist, ik)
+          st%X(psi)(ip, idim, ist, ik) = psi(ip, idim, iter - 1, ii) + lambda(ist)*st%X(psi)(ip, idim, ist, ik)
         end forall
       else
         do idim = 1, st%d%dim
@@ -384,7 +368,7 @@ subroutine X(eigensolver_rmmdiis_min) (gr, st, hm, pre, tol, niter, converged, i
         cb = me1(2, ii) * me2(3, ii) - me1(1, ii) * me2(1, ii)
         cc = me1(1, ii) * me2(2, ii) - me1(2, ii) * me2(4, ii)
 
-        lambda(ist) = CNST(2.0) * cc/(cb + sqrt(cb**2 - CNST(4.0)*ca*cc))
+        lambda(ist) = CNST(2.0)*cc/(cb + sqrt(cb**2 - CNST(4.0)*ca*cc))
 
       end do
 
