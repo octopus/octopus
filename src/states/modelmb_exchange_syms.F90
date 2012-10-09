bb9772bfee0f74f7310b036beff3cb6a6f32af18
Matthieu Verstraete <matthieu.jean.verstraete@gmail.com>
2012-10-09 17:49:56 +0000

    cleaned up modelmb exchange routines

--- src/states/modelmb_exchange_syms.F90
+++ src/states/modelmb_exchange_syms.F90
@@ -257,24 +257,29 @@ contains
 
       ! generate all Young diagrams, decorated, for this distribution of up
       ! and downs
+      ! we will only use the diagram indexed iyoung_in(itype) - this is necessary for several particle types : 
+      ! we need 1 diagram per type.
       call young_init (young, nspinup, nspindown_in(itype))
 
-      ! loop over all Young diagrams for present distribution of spins up and down
+      ! allocate pointers to pairs of up and down spins in present Young diagram
       SAFE_ALLOCATE(p_of_type_up(nspindown_in(itype)))
       SAFE_ALLOCATE(p_of_type_down(nspindown_in(itype)))
       p_of_type_up   = modelmbparticles%particles_of_type(young%young_up  (1:nspindown_in(itype), iyoung_in(itype)), itype)
       p_of_type_down = modelmbparticles%particles_of_type(young%young_down(1:nspindown_in(itype), iyoung_in(itype)), itype)
 
-      call modelmb_sym_down (ndimmb, npptype, &
-             ofst, young, p_of_type_up, p_of_type_down, gr, normalizer, antisymwf)
+      ! symmetrize pairs of up/down spins in the YD
+      call modelmb_sym_updown (ndimmb, npptype, &
+             ofst, nspindown_in(itype), p_of_type_up, p_of_type_down, gr, normalizer, antisymwf)
 
       SAFE_DEALLOCATE_A(p_of_type_up)
       SAFE_DEALLOCATE_A(p_of_type_down)
 
-      call modelmb_antisym_up (iyoung_in(itype), perms_up, ndimmb, npptype, ofst, &
-             young, gr, normalizer, antisymwf)
-      call modelmb_antisym_down (iyoung_in(itype), perms_down, ndimmb, npptype, ofst, &
-             young, gr, antisymwf)
+      ! antisymmetrize up spins amongst themselves
+      call modelmb_antisym_1spin (nspinup,             perms_up, ndimmb, npptype, ofst, &
+             young%young_up(:,iyoung_in(itype)), gr, normalizer, antisymwf)
+      ! antisymmetrize down spins amongst themselves
+      call modelmb_antisym_1spin (nspindown_in(itype), perms_down, ndimmb, npptype, ofst, &
+             young%young_down(:,iyoung_in(itype)), gr, normalizer, antisymwf)
 
       call permutations_end(perms_up)
       call permutations_end(perms_down)
@@ -307,15 +312,15 @@ contains
 
 !
 !> input 1 wave function, and symetrize wrt spin down labeled particles, according to the given young diagrams
-  subroutine modelmb_sym_down(ndimmb, npptype, &
-             ofst, young, p_of_type_up, p_of_type_down, gr, normalizer, antisymwf)
+  subroutine modelmb_sym_updown(ndimmb, npptype, &
+             ofst, ndown, p_of_type_up, p_of_type_down, gr, normalizer, antisymwf)
     integer, intent(in) :: ndimmb
     integer, intent(in) :: npptype
+    integer, intent(in) :: ndown
     integer, intent(in) :: ofst(1:npptype)
 
-    type(young_t), intent(in) :: young
-    integer, intent(in) :: p_of_type_up(young%ndown)
-    integer, intent(in) :: p_of_type_down(young%ndown)
+    integer, intent(in) :: p_of_type_up(ndown)
+    integer, intent(in) :: p_of_type_down(ndown)
     type(grid_t), intent(in) :: gr
 
     FLOAT, intent(in) :: normalizer
@@ -331,7 +336,7 @@ contains
     type(batch_t) :: antisymwfbatch
     logical :: debug_antisym = .false.
 
-    PUSH_SUB(modelmb_sym_down)
+    PUSH_SUB(modelmb_sym_updown)
 
     SAFE_ALLOCATE(forward_map_exchange(gr%mesh%np_global))
     SAFE_ALLOCATE(ix(1:MAX_DIM))
@@ -340,7 +345,7 @@ contains
 
     ! first symmetrize over pairs of particles associated in the present
     ! Young diagram
-    do idown = 1, young%ndown
+    do idown = 1, ndown
       ipart1 = p_of_type_down(idown)
       ipart2 = p_of_type_up(idown)
   
@@ -388,7 +393,7 @@ contains
         norm = TOFLOAT(sum(conjg(antisymwf(:,1,1))*antisymwf(:,1,1))) * normalizer
       end if
       write (message(1), '(a,I7,a,E20.10)') 'norm of pair-symmetrized-state with ',&
-              young%ndown, ' spins down is ', norm
+              ndown, ' spins down is ', norm
       call messages_info(1)
     end if
     SAFE_DEALLOCATE_A(forward_map_exchange)
@@ -396,24 +401,24 @@ contains
     SAFE_DEALLOCATE_A(ixp)
     SAFE_DEALLOCATE_A(antisymwf_swap)
 
-    POP_SUB(modelmb_sym_down)
-  end subroutine modelmb_sym_down
+    POP_SUB(modelmb_sym_updown)
+  end subroutine modelmb_sym_updown
  
   ! ---------------------------------------------------------
 
-  subroutine modelmb_antisym_up(iyoung_in, perms_up, ndimmb, npptype, ofst, young, gr, normalizer, antisymwf)
-    integer, intent(in) :: iyoung_in
+  subroutine modelmb_antisym_1spin(n1spin, perms_1spin, ndimmb, npptype, ofst, young_1spin, gr, normalizer, antisymwf)
+    integer, intent(in) :: n1spin
     integer, intent(in) :: ndimmb
     integer, intent(in) :: npptype
     integer, intent(in) :: ofst(1:npptype)
-    type(young_t), intent(in) :: young
+    integer, intent(in) :: young_1spin(1:n1spin)
     type(grid_t), intent(in) :: gr
     FLOAT, intent(in) :: normalizer
-    type(permutations_t), intent(in) :: perms_up
+    type(permutations_t), intent(in) :: perms_1spin
     CMPLX, intent(inout) :: antisymwf(1:gr%mesh%np,1,1)
 
 ! local vars
-    integer :: iperm_up, iup
+    integer :: iperm_1spin, i1spin
     integer :: ipart1, ipart2, ip
     CMPLX :: wfdotp(1,1)
     FLOAT :: norm
@@ -424,7 +429,7 @@ contains
     type(batch_t) :: antisymwfbatch
     logical :: debug_antisym = .false.
 
-    PUSH_SUB(modelmb_antisym_up)
+    PUSH_SUB(modelmb_antisym_1spin)
 
     SAFE_ALLOCATE(forward_map_exchange(gr%mesh%np_global))
     SAFE_ALLOCATE(ix(1:MAX_DIM))
@@ -434,7 +439,7 @@ contains
     ! for each permutation of particles of this type
     !  antisymmetrize the up labeled spins, amongst themselves
     antisymwf_acc = M_z0
-    do iperm_up = 1, perms_up%npermutations
+    do iperm_1spin = 1, perms_1spin%npermutations
 
       do ip = 1, gr%mesh%np_global
         ! get present position
@@ -442,10 +447,10 @@ contains
         ! initialize coordinates for all particles
         ixp = ix
         ! permute the particles labeled spin up 
-        do iup = 1, young%nup
+        do i1spin = 1, n1spin
           ! get image of ipart1 under permutation iperm1
-          ipart1 = young%young_up(iup,iyoung_in)
-          ipart2 = young%young_up(perms_up%allpermutations(iup,iperm_up),iyoung_in)
+          ipart1 = young_1spin(i1spin)
+          ipart2 = young_1spin(perms_1spin%allpermutations(i1spin,iperm_1spin))
           ixp (ofst(ipart1)+1:ofst(ipart1)+ndimmb) = ix (ofst(ipart2)+1:ofst(ipart2)+ndimmb) ! part1 to 2
         end do
         ! get position of exchanged point
@@ -462,11 +467,11 @@ contains
         antisymwf_swap(:,1,1) = antisymwf(forward_map_exchange(:),1,1)
       end if
 
-      antisymwf_acc = antisymwf_acc + perms_up%permsign(iperm_up)*antisymwf_swap
+      antisymwf_acc = antisymwf_acc + perms_1spin%permsign(iperm_1spin)*antisymwf_swap
 
-    end do ! iperm_up
+    end do ! iperm_1spin
 
-    antisymwf = antisymwf_acc / TOFLOAT(perms_up%npermutations)
+    antisymwf = antisymwf_acc / TOFLOAT(perms_1spin%npermutations)
 
     ! the following could be removed for production
     if (debug_antisym) then 
@@ -478,8 +483,8 @@ contains
       else
         norm = TOFLOAT(sum(conjg(antisymwf(:,1,1))*antisymwf(:,1,1))) * normalizer
       end if
-      write (message(1), '(a,I7,a,E20.10)') 'norm of up-antisym+pairsym-state with ',&
-              young%ndown, ' spins down is ', norm
+      write (message(1), '(a,I7,a,E20.10)') 'norm of 1spin-antisym+pairsym-state with ',&
+              n1spin, ' spins of present type is ', norm
       call messages_info(1)
     end if
     SAFE_DEALLOCATE_A(forward_map_exchange)
@@ -488,85 +493,9 @@ contains
     SAFE_DEALLOCATE_A(ix)
     SAFE_DEALLOCATE_A(ixp)
 
-    POP_SUB(modelmb_antisym_up)
+    POP_SUB(modelmb_antisym_1spin)
 
-  end subroutine modelmb_antisym_up
-
-  ! ---------------------------------------------------------
-
-  subroutine modelmb_antisym_down (iyoung_in, perms_down, ndimmb, npptype, ofst, young, gr, antisymwf)
-    integer, intent(in) :: iyoung_in
-    integer, intent(in) :: ndimmb
-    integer, intent(in) :: npptype
-    integer, intent(in) :: ofst(1:npptype)
-    type(permutations_t), intent(in) :: perms_down
-    type(young_t), intent(in) :: young
-    type(grid_t), intent(in) :: gr
-    CMPLX, intent(inout) :: antisymwf(1:gr%mesh%np,1,1)
-
-! local vars
-    integer :: iperm_down, idown
-    integer :: ipart1, ipart2, ip
-
-    integer, allocatable :: forward_map_exchange(:)
-    integer, allocatable :: ix(:), ixp(:)
-    CMPLX, allocatable  :: antisymwf_swap(:,:,:) !< single wf term, with correct permutation of particles
-    CMPLX, allocatable  :: antisymwf_acc(:,:,:)
-    type(batch_t) :: antisymwfbatch
-
-    PUSH_SUB(modelmb_antisym_down)
-
-    SAFE_ALLOCATE(forward_map_exchange(gr%mesh%np_global))
-    SAFE_ALLOCATE(ix(1:MAX_DIM))
-    SAFE_ALLOCATE(ixp(1:MAX_DIM))
-    SAFE_ALLOCATE(antisymwf_swap(1:gr%mesh%np, 1, 1))
-    SAFE_ALLOCATE(antisymwf_acc(1:gr%mesh%np, 1, 1))
-
-
-    ! for each permutation of particles of this type
-    !  antisymmetrize the down labeled spins, amongst themselves
-    antisymwf_acc = M_z0
-    do iperm_down = 1, perms_down%npermutations
-      do ip = 1, gr%mesh%np_global
-        ! get present position
-        call index_to_coords(gr%mesh%idx, gr%sb%dim, ip, ix)
-        ! initialize coordinates for all particles
-        ixp = ix
-        ! permute the particles labeled spin down
-        do idown = 1, young%ndown
-          ! get image of ipart1 under permutation iperm1
-          ipart1 = young%young_down(idown,iyoung_in)
-          ipart2 = young%young_down(perms_down%allpermutations(idown,iperm_down),iyoung_in)
-          ixp (ofst(ipart1)+1:ofst(ipart1)+ndimmb) = ix (ofst(ipart2)+1:ofst(ipart2)+ndimmb) ! part1 to 2
-        end do
-        ! get position of exchanged point
-        forward_map_exchange(ip) = index_from_coords(gr%mesh%idx, gr%sb%dim, ixp)
-      end do ! ip
-
-      if (gr%mesh%parallel_in_domains) then
-        antisymwf_swap = antisymwf
-        ! set up batch type for global exchange operation: 1 state, the loop over MB states is outside this routine
-        call batch_init (antisymwfbatch, 1, 1, 1, antisymwf_swap)
-        call zmesh_batch_exchange_points (gr%mesh, antisymwfbatch, forward_map=forward_map_exchange)
-        call batch_end(antisymwfbatch)
-      else
-        antisymwf_swap(:,1,1) = antisymwf(forward_map_exchange(:),1,1)
-      end if
-
-      antisymwf_acc = antisymwf_acc + perms_down%permsign(iperm_down)*antisymwf_swap
-
-    end do ! iperm_down
-
-    antisymwf = antisymwf_acc / TOFLOAT(perms_down%npermutations)
-
-    SAFE_DEALLOCATE_A(forward_map_exchange)
-    SAFE_DEALLOCATE_A(antisymwf_swap)
-    SAFE_DEALLOCATE_A(ix)
-    SAFE_DEALLOCATE_A(ixp)
-
-    POP_SUB(modelmb_antisym_down)
-
-  end subroutine modelmb_antisym_down
+  end subroutine modelmb_antisym_1spin
 
   ! ---------------------------------------------------------
 
