65097c4128efb2b3dbf5245958c930a7ad27f862
Xavier Andrade <xavier@tddft.org>
2013-02-09 13:25:10 +0000

    The direct 3D test started failing in parallel. It seems to me that there was an obvious error in the code, but I don't understand why the test was working before.

--- src/poisson/solver_3d_inc.F90
+++ src/poisson/solver_3d_inc.F90
@@ -227,7 +227,7 @@ subroutine poisson_solve_direct(this, pot, rho)
         enddo
       endif
       tmp = dmf_integrate(this%der%mesh, pvec)
-      if (this%der%mesh%vp%part(ip) /= this%der%mesh%vp%partno) then
+      if (this%der%mesh%vp%part(ip) == this%der%mesh%vp%partno) then
         pot(vec_global2local(this%der%mesh%vp, ip, this%der%mesh%vp%partno)) = tmp
       end if
     end do
