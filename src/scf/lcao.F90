b7597fe7792d13690c58e944e410f812f34219de
David Strubbe <dstrubbe@ucmerced.edu>
2013-02-22 00:18:49 +0000

    Backport of 10056 to branches/4.1.x: ------------------------------------------------------------------------ r10056 | dstrubbe | 2013-02-21 17:32:32 -0500 (Thu, 21 Feb 2013) | 2 lines
    Documentation on atomic guess densities.
    
    ------------------------------------------------------------------------

--- src/scf/lcao.F90
+++ src/scf/lcao.F90
@@ -174,8 +174,13 @@ contains
     !% of initial wavefunctions and with a new guess for the density.
     !% (Up to the current version, only a minimal basis set is used.)
     !% The default is <tt>lcao_full</tt> if at least one species representing an atom is present.
-    !% The default is <tt>lcao_none</tt> if all species are <tt>spec_user_def</tt>,
+    !% The default is <tt>lcao_none</tt> if all species are <tt>spec_user_defined</tt>,
     !% <tt>spec_charge_density</tt>, <tt>species_from_file</tt>, or <tt>spec_jelli_slab</tt>.
+    !% The initial guess densitie for LCAO are from the pseudopotential for PSF, HGH, UPF, PSPIO species;
+    !% from the natural charge density for <tt>spec_charge_density</tt>, <tt>spec_point</tt>,
+    !% <tt>spec_jelli</tt>, and <tt>spec_jelli_slab</tt>;
+    !% or uniform for CPI and FHI pseudopotentials, <tt>spec_full_delta</tt>, <tt>spec_full_gaussian</tt>,
+    !% <tt>spec_user_defined</tt>, or <tt>species_from_file</tt>.
     !% (Non-pseudopotential species use Hermite polynomials as orbitals.)
     !%Option lcao_none 0
     !% Do not perform a LCAO calculation before the SCF cycle. Instead use random wavefunctions.
