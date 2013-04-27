6d154d25da6c036832b776b62427b0e89c0bd8ed
David Strubbe <dstrubbe@ucmerced.edu>
2013-04-27 19:40:35 +0000

    Backport of 10444 to branches/4.1.x: ------------------------------------------------------------------------ r10444 | dstrubbe | 2013-04-27 01:49:32 -0400 (Sat, 27 Apr 2013) | 2 lines
    Labeling the initialization output of the Poisson solver on the fine mesh.
    
    ------------------------------------------------------------------------

--- src/system/v_ks.F90
+++ src/system/v_ks.F90
@@ -310,7 +310,7 @@ contains
       if(gr%have_fine_mesh) then
         ks%new_hartree = .true.
         SAFE_ALLOCATE(ks%hartree_solver)
-        call poisson_init(ks%hartree_solver, gr%fine%der, geo, mc%master_comm)
+        call poisson_init(ks%hartree_solver, gr%fine%der, geo, mc%master_comm, label = " (fine mesh)")
       else
         ks%hartree_solver => psolver
       end if
