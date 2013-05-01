cf62b63db3da525c20a26de4d696ff0e6c1dc1b9
David Strubbe <dstrubbe@ucmerced.edu>
2013-05-01 02:16:15 +0000

    Doxygen comments.

--- src/math/sparskit.F90
+++ src/math/sparskit.F90
@@ -31,16 +31,16 @@ module sparskit_m
   private
 
   integer, public, parameter ::  &
-    SK_CG      =  1,             &  ! Conjugate Gradient Method
-    SK_CGNR    =  2,             &  ! Conjugate Gradient Method (Normal Residual equation)
-    SK_BCG     =  3,             &  ! Bi-Conjugate Gradient Method
-    SK_DBCG    =  4,             &  ! BCG with partial pivoting
-    SK_BCGSTAB =  5,             &  ! BCG stabilized
-    SK_TFQMR   =  6,             &  ! Transpose-Free Quasi-Minimum Residual method
-    SK_FOM     =  7,             &  ! Full Orthogonalization Method
-    SK_GMRES   =  8,             &  ! Generalized Minimum Residual method
-    SK_FGMRES  =  9,             &  ! Flexible version of Generalized Minimum Residual method
-    SK_DQGMRES = 10,             &  ! Direct versions of Quasi Generalized Minimum Residual method
+    SK_CG      =  1,             &  !< Conjugate Gradient Method
+    SK_CGNR    =  2,             &  !< Conjugate Gradient Method (Normal Residual equation)
+    SK_BCG     =  3,             &  !< Bi-Conjugate Gradient Method
+    SK_DBCG    =  4,             &  !< BCG with partial pivoting
+    SK_BCGSTAB =  5,             &  !< BCG stabilized
+    SK_TFQMR   =  6,             &  !< Transpose-Free Quasi-Minimum Residual method
+    SK_FOM     =  7,             &  !< Full Orthogonalization Method
+    SK_GMRES   =  8,             &  !< Generalized Minimum Residual method
+    SK_FGMRES  =  9,             &  !< Flexible version of Generalized Minimum Residual method
+    SK_DQGMRES = 10,             &  !< Direct versions of Quasi Generalized Minimum Residual method
     SK_MINVAL  = SK_CG,          &
     SK_MAXVAL  = SK_DQGMRES
 
@@ -58,20 +58,20 @@ module sparskit_m
     zsparskit_solver_end
 
   type sparskit_solver_t
-    integer :: size                 ! size of the linear system
-    integer :: solver_type          ! which solver to use
-    integer :: krylov_size          ! size of the Krylov subspace (used for some solvers)
-    integer :: preconditioning      ! what kind of preconditioning to use
-    integer :: maxiter              ! maximum number of iterations
-    integer :: used_iter            ! number of performed iterations
-    integer :: iter_out             ! determines how often status info of the solver is printed
-    FLOAT   :: residual_norm        ! used store current error norm
-    FLOAT   :: rel_tolerance        ! relative tolerance
-    FLOAT   :: abs_tolerance        ! absolute tolerance
+    integer :: size                 !< size of the linear system
+    integer :: solver_type          !< which solver to use
+    integer :: krylov_size          !< size of the Krylov subspace (used for some solvers)
+    integer :: preconditioning      !< what kind of preconditioning to use
+    integer :: maxiter              !< maximum number of iterations
+    integer :: used_iter            !< number of performed iterations
+    integer :: iter_out             !< determines how often status info of the solver is printed
+    FLOAT   :: residual_norm        !< used store current error norm
+    FLOAT   :: rel_tolerance        !< relative tolerance
+    FLOAT   :: abs_tolerance        !< absolute tolerance
 
-    integer :: ipar(16)             ! integer parameter array for the reverse communication protocol
-    FLOAT   :: fpar(16)             ! floating-point parameter array for the reverse communication protocol
-    logical :: verbose              ! if .true. then the solver will write more details
+    integer :: ipar(16)             !< integer parameter array for the reverse communication protocol
+    FLOAT   :: fpar(16)             !< floating-point parameter array for the reverse communication protocol
+    logical :: verbose              !< if .true. then the solver will write more details
   end type sparskit_solver_t
 
 
