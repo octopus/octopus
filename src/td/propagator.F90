23c309a6bfabecbbec2b253d96051ee568f88410
David Strubbe <dstrubbe@ucmerced.edu>
2013-05-03 18:41:08 +0000

    Backport of 10494 to branches/4.1.x: ------------------------------------------------------------------------ r10494 | dstrubbe | 2013-05-02 15:25:53 -0400 (Thu, 02 May 2013) | 2 lines
    The SPARSKIT propagator does not work parallel in domains. (I believe it is for the reason as discussed in r10486, i.e. the dot product and norm being done by SPARSKIT is not collective.)
    
    ------------------------------------------------------------------------

--- src/td/propagator.F90
+++ src/td/propagator.F90
@@ -258,6 +258,7 @@ contains
     !%Option crank_nicholson_sparskit 6
     !%Option crank_nicolson_sparskit 6
     !% Classical Crank-Nicolson propagator. Requires the SPARSKIT library.
+    !% (Domain parallelization not implemented.)
     !%
     !% <MATH>
     !%  (1 + i\delta t/2 H_{n+1/2}) \psi_{n+1} = (1 - i\delta t/2 H_{n+1/2}) \psi_{n}  
@@ -303,6 +304,8 @@ contains
     case(PROP_CRANK_NICOLSON)
     case(PROP_CRANK_NICOLSON_SPARSKIT)
 #ifdef HAVE_SPARSKIT
+      if(gr%mesh%parallel_in_domains) &
+        call messages_not_implemented("crank_nicolson_sparskit propagator parallel in domains")
       SAFE_ALLOCATE(tdsk)
       call zsparskit_solver_init(gr%mesh%np, tdsk)
       SAFE_ALLOCATE(zpsi_tmp(1:gr%mesh%np_part, 1:st%d%dim, 1:st%nst, st%d%kpt%start:st%d%kpt%end))
