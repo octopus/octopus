c508d6be36fae5f1544e35c40d55abba1d4fb89e
Xavier Andrade <xavier@tddft.org>
2013-08-30 19:05:09 +0000

    Bugfix: Theta should not be used if complex scaling is not enabled.

--- src/system/system.F90
+++ src/system/system.F90
@@ -108,10 +108,19 @@ contains
     call states_lead_densities_init(sys%st, sys%gr)
     call elf_init()
 
-    call poisson_init(psolver, sys%gr%der, sys%geo, sys%mc%master_comm, theta = sys%st%cmplxscl%theta)
+    if(sys%st%cmplxscl%space) then
+      call poisson_init(psolver, sys%gr%der, sys%geo, sys%mc%master_comm, theta = sys%st%cmplxscl%theta)
+    else
+      call poisson_init(psolver, sys%gr%der, sys%geo, sys%mc%master_comm)
+    end if
+
     if(poisson_is_multigrid(psolver)) call grid_create_multigrid(sys%gr, sys%geo)
 
-    call v_ks_init(sys%ks, sys%gr, sys%st%d, sys%geo, sys%mc, sys%st%qtot, theta = sys%st%cmplxscl%theta)
+    if(sys%st%cmplxscl%space) then
+      call v_ks_init(sys%ks, sys%gr, sys%st%d, sys%geo, sys%mc, sys%st%qtot, theta = sys%st%cmplxscl%theta)
+    else
+      call v_ks_init(sys%ks, sys%gr, sys%st%d, sys%geo, sys%mc, sys%st%qtot)
+    end if
 
     !print the mesh information if it is required
     call print_r()
