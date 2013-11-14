eef5aa263cdd7fc8610446707271dfd99cc62ea2
David Strubbe <dstrubbe@ucmerced.edu>
2013-11-14 17:58:24 +0000

    Correcting misleading output from full OEP: it always said "converged" even when it was not.

--- src/system/xc_oep_inc.F90
+++ src/system/xc_oep_inc.F90
@@ -202,7 +202,12 @@ subroutine X(xc_oep_solve) (gr, hm, st, is, vxc, oep)
     if(ff < oep%scftol%conv_abs_dens) exit
   end do
 
-  write(message(1), '(a,i4,a,es14.6)') "Info: After ", iter, " iterations, the OEP converged to ", ff
+  if(ff > oep%scftol%conv_abs_dens) then
+    write(message(1), '(a)') "OEP did not converge."
+    call messages_warning(1)
+  endif
+
+  write(message(1), '(a,i4,a,es14.6)') "Info: After ", iter, " iterations, the OEP residual = ", ff
   message(2) = ''
   call messages_info(2)
 
