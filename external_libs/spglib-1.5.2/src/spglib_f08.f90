24c631d76bf6f5b0aec4bfbf78ebdad7eae32f01
David Strubbe <dstrubbe@ucmerced.edu>
2014-01-30 21:44:52 +0000

    The troublesome parts of the spglib Fortran interface for the PGI 7 compiler are not being used by Octopus anyway, so I am just commenting them out rather than trying to figure out how to make them work.

--- external_libs/spglib-1.5.2/src/spglib_f08.f90
+++ external_libs/spglib-1.5.2/src/spglib_f08.f90
@@ -1,26 +1,26 @@
-   module SpglibDataset_c_m
-
-   use iso_c_binding, only:  c_char, c_int, c_double, c_ptr, c_f_pointer
-      
-      type, bind(c) :: SpglibDataset_c
-         integer(c_int) :: spacegroup_number
-         integer(c_int) :: hall_number
-         character(kind=c_char) :: international_symbol(11)
-         character(kind=c_char) :: hall_symbol(17)
-         real(c_double)      :: transformation_matrix(3,3)
-         real(c_double) :: origin_shift(3)
-         integer(c_int) :: n_operations
-   !       integer(c_int), pointer :: rotations(3,3,:)
-         type(c_ptr) :: rotations
-   !       real(c_double), pointer :: translations(3,:)
-         type(c_ptr) :: translations
-         integer(c_int) :: n_atoms
-   !       integer(c_int), pointer :: wyckoffs(:)
-         type(c_ptr) :: wyckoffs
-   !       integer(c_int), pointer :: equivalent_atoms(:)
-         type(c_ptr) :: equivalent_atoms
-      end type 
-   end module SpglibDataset_c_m
+!   module SpglibDataset_c_m
+!
+!   use iso_c_binding, only:  c_char, c_int, c_double, c_ptr, c_f_pointer
+!      
+!      type, bind(c) :: SpglibDataset_c
+!         integer(c_int) :: spacegroup_number
+!         integer(c_int) :: hall_number
+!         character(kind=c_char) :: international_symbol(11)
+!         character(kind=c_char) :: hall_symbol(17)
+!         real(c_double)      :: transformation_matrix(3,3)
+!         real(c_double) :: origin_shift(3)
+!         integer(c_int) :: n_operations
+!   !       integer(c_int), pointer :: rotations(3,3,:)
+!         type(c_ptr) :: rotations
+!   !       real(c_double), pointer :: translations(3,:)
+!         type(c_ptr) :: translations
+!         integer(c_int) :: n_atoms
+!   !       integer(c_int), pointer :: wyckoffs(:)
+!         type(c_ptr) :: wyckoffs
+!   !       integer(c_int), pointer :: equivalent_atoms(:)
+!         type(c_ptr) :: equivalent_atoms
+!      end type 
+!   end module SpglibDataset_c_m
 
    module spglib_f08
       
@@ -30,20 +30,20 @@
    
    private
 
-   type :: SpglibDataset
-      integer :: spacegroup_number
-      integer :: hall_number
-      character(len=11) :: international_symbol
-      character(len=17) :: hall_symbol
-      real(c_double)  :: transformation_matrix(3,3)
-      real(c_double)  :: origin_shift(3)
-      integer :: n_operations
-      integer, allocatable :: rotations(:,:,:)
-      real(c_double), allocatable :: translations(:,:)
-      integer :: n_atoms
-      integer, allocatable :: wyckoffs(:)
-      integer, allocatable :: equivalent_atoms(:) !Beware mapping refers to positions starting at 0
-   end type 
+!   type :: SpglibDataset
+!      integer :: spacegroup_number
+!      integer :: hall_number
+!      character(len=11) :: international_symbol
+!      character(len=17) :: hall_symbol
+!      real(c_double)  :: transformation_matrix(3,3)
+!      real(c_double)  :: origin_shift(3)
+!      integer :: n_operations
+!      integer, allocatable :: rotations(:,:,:)
+!      real(c_double), allocatable :: translations(:,:)
+!      integer :: n_atoms
+!      integer, allocatable :: wyckoffs(:)
+!      integer, allocatable :: equivalent_atoms(:) !Beware mapping refers to positions starting at 0
+!   end type 
   
    
    
@@ -319,8 +319,8 @@
    end interface 
    
       
-   
-   public :: SpglibDataset, spg_get_dataset, &
+!    SpglibDataset, spg_get_dataset, &
+   public :: &
       & spg_get_symmetry, spgat_get_symmetry, &
       & spg_get_symmetry_with_collinear_spin, spgat_get_symmetry_with_collinear_spin, &
       & spg_get_multiplicity, spgat_get_multiplicity, spg_get_smallest_lattice, &
@@ -337,78 +337,78 @@
 
 
    
-   function spg_get_dataset(lattice, position, types, num_atom, symprec) result(dset)
-      use SpglibDataset_c_m      
-      real(c_double), intent(in) :: lattice(3,3)
-      real(c_double), intent(in) :: position(3,*)
-      integer, intent(in) :: types(*)
-      integer, intent(in), value :: num_atom
-      real(c_double), intent(in), value :: symprec
-      type(SpglibDataset) :: dset
-      
-      
-      
-      interface     
-         function spg_get_dataset_c(lattice, position, types, num_atom, symprec) bind(c, name='spg_get_dataset')
-            use iso_c_binding, only: c_int, c_double, c_ptr
-            real(c_double), intent(in) :: lattice(3,3)
-            real(c_double), intent(in) :: position(3,*)
-            integer(c_int), intent(in) :: types(*)
-            integer(c_int), intent(in), value :: num_atom
-            real(c_double), intent(in), value :: symprec
-            type(c_ptr) :: spg_get_dataset_c
-         end function spg_get_dataset_c      
-         
-         subroutine spg_free_dataset_c(dataset) bind(c, name = 'spg_free_dataset')
-            use SpglibDataset_c_m
-            type(SpglibDataset_c), intent(inout) :: dataset
-         end subroutine spg_free_dataset_c
-      end interface
-   
-      type(SpglibDataset_c), pointer :: dset_c
-      integer :: n_operations, n_atoms, i
-      
-      real(c_double), pointer :: translations(:,:)
-      integer(c_int), pointer :: rotations(:,:,:), wyckoffs(:), equivalent_atoms(:)
-      
-      call c_f_pointer( spg_get_dataset_c(lattice, position, types, num_atom, symprec), dset_c)
-      
-      dset % spacegroup_number     = dset_c % spacegroup_number
-      dset % hall_number           = dset_c % hall_number
-      dset % transformation_matrix = dset_c % transformation_matrix
-      dset % origin_shift          = dset_c % origin_shift         
-      dset % n_operations          = dset_c % n_operations         
-      dset % n_atoms               = dset_c % n_atoms              
-      
-      do i = 1, size(dset_c % international_symbol)
-         dset % international_symbol(i:i) = dset_c % international_symbol(i)
-      end do
-      
-      do i = 1, size(dset_c % hall_symbol)
-         dset % hall_symbol(i:i) = dset_c % hall_symbol(i)
-      end do
-      
-      n_operations = dset_c % n_operations
-      n_atoms      = dset_c % n_atoms
-      
-      call c_f_pointer (dset_c % rotations       , rotations       , (/3, 3, n_operations/))
-      call c_f_pointer (dset_c % translations    , translations    , (/3,    n_operations/))
-      call c_f_pointer (dset_c % wyckoffs        , wyckoffs        , (/n_atoms/))
-      call c_f_pointer (dset_c % equivalent_atoms, equivalent_atoms, (/n_atoms/))
-      
-      allocate( dset % rotations       (3, 3, n_operations))
-      allocate( dset % translations    (3,    n_operations))
-      allocate( dset % wyckoffs        (n_atoms))
-      allocate( dset % equivalent_atoms(n_atoms))
-      
-      dset % rotations        = rotations       
-      dset % translations     = translations    
-      dset % wyckoffs         = wyckoffs        
-      dset % equivalent_atoms = equivalent_atoms
-      
-      call spg_free_dataset_c(dset_c)
-      
-   end function spg_get_dataset
+!   function spg_get_dataset(lattice, position, types, num_atom, symprec) result(dset)
+!      use SpglibDataset_c_m      
+!      real(c_double), intent(in) :: lattice(3,3)
+!      real(c_double), intent(in) :: position(3,*)
+!      integer, intent(in) :: types(*)
+!      integer, intent(in), value :: num_atom
+!      real(c_double), intent(in), value :: symprec
+!      type(SpglibDataset) :: dset
+!      
+!      
+!      
+!      interface     
+!         function spg_get_dataset_c(lattice, position, types, num_atom, symprec) bind(c, name='spg_get_dataset')
+!            use iso_c_binding, only: c_int, c_double, c_ptr
+!            real(c_double), intent(in) :: lattice(3,3)
+!            real(c_double), intent(in) :: position(3,*)
+!            integer(c_int), intent(in) :: types(*)
+!            integer(c_int), intent(in), value :: num_atom
+!            real(c_double), intent(in), value :: symprec
+!            type(c_ptr) :: spg_get_dataset_c
+!         end function spg_get_dataset_c      
+!         
+!         subroutine spg_free_dataset_c(dataset) bind(c, name = 'spg_free_dataset')
+!            use SpglibDataset_c_m
+!            type(SpglibDataset_c), intent(inout) :: dataset
+!         end subroutine spg_free_dataset_c
+!      end interface
+!   
+!      type(SpglibDataset_c), pointer :: dset_c
+!      integer :: n_operations, n_atoms, i
+!      
+!      real(c_double), pointer :: translations(:,:)
+!      integer(c_int), pointer :: rotations(:,:,:), wyckoffs(:), equivalent_atoms(:)
+!      
+!      call c_f_pointer( spg_get_dataset_c(lattice, position, types, num_atom, symprec), dset_c)
+!      
+!      dset % spacegroup_number     = dset_c % spacegroup_number
+!      dset % hall_number           = dset_c % hall_number
+!      dset % transformation_matrix = dset_c % transformation_matrix
+!      dset % origin_shift          = dset_c % origin_shift         
+!      dset % n_operations          = dset_c % n_operations         
+!      dset % n_atoms               = dset_c % n_atoms              
+!      
+!      do i = 1, size(dset_c % international_symbol)
+!         dset % international_symbol(i:i) = dset_c % international_symbol(i)
+!      end do
+!      
+!      do i = 1, size(dset_c % hall_symbol)
+!         dset % hall_symbol(i:i) = dset_c % hall_symbol(i)
+!      end do
+!      
+!      n_operations = dset_c % n_operations
+!      n_atoms      = dset_c % n_atoms
+!      
+!      call c_f_pointer (dset_c % rotations       , rotations       , (/3, 3, n_operations/))
+!      call c_f_pointer (dset_c % translations    , translations    , (/3,    n_operations/))
+!      call c_f_pointer (dset_c % wyckoffs        , wyckoffs        , (/n_atoms/))
+!      call c_f_pointer (dset_c % equivalent_atoms, equivalent_atoms, (/n_atoms/))
+!      
+!      allocate( dset % rotations       (3, 3, n_operations))
+!      allocate( dset % translations    (3,    n_operations))
+!      allocate( dset % wyckoffs        (n_atoms))
+!      allocate( dset % equivalent_atoms(n_atoms))
+!      
+!      dset % rotations        = rotations       
+!      dset % translations     = translations    
+!      dset % wyckoffs         = wyckoffs        
+!      dset % equivalent_atoms = equivalent_atoms
+!      
+!      call spg_free_dataset_c(dset_c)
+!      
+!   end function spg_get_dataset
 
    
    end module spglib_f08   
