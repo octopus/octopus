c8c4fca6894b1c198406440f71d558980dced086
David Strubbe <dstrubbe@ucmerced.edu>
2014-02-10 19:19:26 +0000

    Bugfix: for a spin-polarized system, the output of states_write_eigenvalues always showed the error from the first spin for both spins. (The change to the spinor case is purely to look consistent, but actually behaves the same since is == 0.) This bug goes back pretty close to the beginning of time: it was introduced in r393 as part of a bigger bug, and only partially corrected in r434.

--- src/states/states_io.F90
+++ src/states/states_io.F90
@@ -151,17 +151,17 @@ contains
             if(st%d%ispin == SPINORS) then
               write(tmp_str(2), '(1x,f12.6,3x,4f5.2)') &
                 units_from_atomic(units_out%energy, st%eigenval(ist, ik)), occ, st%spin(1:3, ist, ik)
-              if(present(error)) write(tmp_str(3), '(a7,es8.1,a1)')'      (', error(ist, ik+is), ')'
+              if(present(error)) write(tmp_str(3), '(a7,es8.1,a1)')'      (', error(ist, ik), ')'
             else
               write(tmp_str(2), '(1x,f12.6,3x,f12.6)') &
                 units_from_atomic(units_out%energy, st%eigenval(ist, ik+is)), occ
-              if(present(error)) write(tmp_str(3), '(a7,es8.1,a1)')'      (', error(ist, ik), ')'
+              if(present(error)) write(tmp_str(3), '(a7,es8.1,a1)')'      (', error(ist, ik+is), ')'
             end if
           else
             if(st%d%ispin == SPINORS) then
               write(tmp_str(2), '(1x,f12.6,5x,f5.2,3x,3f8.4)') &
                 units_from_atomic(units_out%energy, st%eigenval(ist, ik)), occ, st%spin(1:3, ist, ik)
-              if(present(error)) write(tmp_str(3), '(a3,es8.1,a1)')'  (', error(ist, ik+is), ')'
+              if(present(error)) write(tmp_str(3), '(a3,es8.1,a1)')'  (', error(ist, ik), ')'
             else
               if(st%cmplxscl%space) then !cmplxscl
                 write(tmp_str(2), '(1x,f12.6,3x,f12.6,3x,f12.6)') &
@@ -171,7 +171,7 @@ contains
                 write(tmp_str(2), '(1x,f12.6,3x,f12.6)') &
                   units_from_atomic(units_out%energy, st%eigenval(ist, ik+is)), occ
               end if
-              if(present(error)) write(tmp_str(3), '(a7,es8.1,a1)')'      (', error(ist, ik), ')'
+              if(present(error)) write(tmp_str(3), '(a7,es8.1,a1)')'      (', error(ist, ik+is), ')'
             end if
           end if
           if(present(error)) then
