bf569bb97b721f818d3cd1765e54ea4e7b2fa493
David Strubbe <dstrubbe@ucmerced.edu>
2014-02-12 20:40:53 +0000

    * Bugfix to r9383, restoring ability to run with SCFinLCAO. In that commit, lcao_init_orbitals was refactored out of lcao_wf, requiring an extra call to that routine as added in r9787. However, since the states are used to store the orbitals, and are then overwritten by the states from diagonalization, we must repeat lcao_init_orbitals for every iteration. Moreover, since some arrays are allocated in lcao_init_orbitals, and only deallocated in lcao_end (which also deallocates other things), we must completely initialize and destroy the LCAO object for each iteration. Obviously this is quite inefficient and can be improved, but at least it works again. * Adding a test to ensure SCFinLCAO continues to work.

--- src/scf/scf.F90
+++ src/scf/scf.F90
@@ -479,6 +479,7 @@ contains
         call messages_fatal(1)
       end if
       call lcao_init_orbitals(lcao, st, gr, geo)
+      call lcao_end(lcao)
     end if
 
     nspin = st%d%nspin
@@ -562,7 +563,10 @@ contains
       scf%eigens%converged = 0
 
       if(scf%lcao_restricted) then
+        call lcao_init(lcao, gr, geo, st)
+        call lcao_init_orbitals(lcao, st, gr, geo)
         call lcao_wf(lcao, st, gr, geo, hm)
+        call lcao_end(lcao)
       else
         ! FIXME: Currently, only the eigensolver or the
         ! Lippmann-Schwinger approach can be used (exclusively),
@@ -743,7 +747,7 @@ contains
           write(message(2), '(a)')        '' 
           call messages_info(2)
         end if
-        if(scf%lcao_restricted) call lcao_end(lcao)
+!        if(scf%lcao_restricted) call lcao_end(lcao)
         call profiling_out(prof)
         exit
       end if
