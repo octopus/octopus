90e414a7790ed99ca30e2d5f880293c5ec24a33d
David Strubbe <dstrubbe@ucmerced.edu>
2014-04-20 02:53:14 +0000

    true_compiler.sh: * Adding understanding of CC (C++) compiler wrapper. * Organizing a bit more. * Returns "unknown" if it cannot figure it out. This is relevant for the compiler wrappers on the newest Crays which do not support "-show" unfortunately.

--- build/true_compiler.sh
+++ build/true_compiler.sh
@@ -5,14 +5,18 @@
 # If the argument is mpicc or mpif90, prints the true compiler that is
 # being called. For the moment it only works if the -show argument is
 # accepted by the wrapper (mpich, openmpi and derivatives do).
+# It also works for the Cray compiler wrappers cc and ftn.
 
-if echo $1 | grep mpi > /dev/null
-then
-echo -n "("`$1 -show | cut -f 1 -d" "`")"
-fi
+RESULT="(unknown)"
 
+# MPI
+if echo $1 | grep mpi > /dev/null; then
+  RESULT="("`$1 -show | cut -f 1 -d" "`")"
 # Cray compiler wrappers
-if [ x$1 == xcc -o x$1 == xftn ]
-then
-echo -n "("`$1 -show | grep DRIVERNAME | cut -f 2 -d"="`")"
+elif [ x$1 == xcc -o x$1 == xftn -o x$1 == xCC ]; then
+  if $1 -show &> /dev/null; then
+      RESULT="("`$1 -show | grep DRIVERNAME | cut -f 2 -d"="`")"
+  fi
 fi
+
+echo -n "$RESULT"
