e157f8f8f20ae5b43b1b34c1ace8b4e03d46629a
David Strubbe <dstrubbe@ucmerced.edu>
2015-02-09 17:29:53 +0000

    Backport of 12916 to branches/5.0.x: ------------------------------------------------------------------------ r12916 | dstrubbe | 2015-02-09 12:17:08 -0500 (Mon, 09 Feb 2015) | 2 lines
    Bugfix: Printing the partition (as from DebugLevel >=1) failed if compiled with --max-dim=4 or larger due to nadequate format statements.
    
    ------------------------------------------------------------------------

--- src/grid/mesh_partition.F90
+++ src/grid/mesh_partition.F90
@@ -802,7 +802,7 @@ contains
       action='write')
     do ii = 1, mesh%np
       jj = mesh%vp%local(mesh%vp%xlocal + ii - 1)
-      write(iunit, '(i8,3f18.8)') jj, mesh_x_global(mesh, jj)
+      write(iunit, '(i8,99f18.8)') jj, mesh_x_global(mesh, jj)
     end do
     call io_close(iunit)
 
@@ -810,11 +810,11 @@ contains
       action='write')
     do ii = 1, mesh%np
       jj = mesh%vp%local(mesh%vp%xlocal + ii - 1)
-      write(iunit, '(i8,3f18.8)') jj, mesh_x_global(mesh, jj)
+      write(iunit, '(i8,99f18.8)') jj, mesh_x_global(mesh, jj)
     end do
     do ii = 1, mesh%vp%np_bndry
       jj = mesh%vp%bndry(mesh%vp%xbndry + ii - 1)
-      write(iunit, '(i8,3f18.8)') jj, mesh_x_global(mesh, jj)
+      write(iunit, '(i8,99f18.8)') jj, mesh_x_global(mesh, jj)
     end do
     call io_close(iunit)
 
@@ -824,7 +824,7 @@ contains
       iunit = io_open('debug/mesh_partition/mesh_partition.'//filenum, &
         action='write')
       do ii = mesh%np_global+1, mesh%np_part_global
-        write(iunit, '(i8,3f18.8)') ii, mesh_x_global(mesh, ii)
+        write(iunit, '(i8,99f18.8)') ii, mesh_x_global(mesh, ii)
       end do
       call io_close(iunit)
     end if
