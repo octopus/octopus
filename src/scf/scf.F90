f1e153a3f47738c2ee81903509240af40180dc4e
David Strubbe <dstrubbe@ucmerced.edu>
2015-02-22 20:37:28 +0000

    Backport of 13121 to branches/5.0.x: ------------------------------------------------------------------------ r13121 | dstrubbe | 2015-02-19 17:17:13 -0500 (Thu, 19 Feb 2015) | 2 lines
    Minor bugfix: If SCFinLCAO was stopped before convergence, lcao_end was not called.
    
    ------------------------------------------------------------------------

--- src/scf/scf.F90
+++ src/scf/scf.F90
@@ -828,7 +828,6 @@ contains
           write(message(2), '(a)')        '' 
           call messages_info(2)
         end if
-        if(scf%lcao_restricted) call lcao_end(lcao)
         call profiling_out(prof)
         exit
       end if
@@ -867,6 +866,8 @@ contains
       call profiling_out(prof)
     end do !iter
     
+    if(scf%lcao_restricted) call lcao_end(lcao)
+
     if(scf%max_iter > 0 .and. scf%mix_field == MIXPOT) then
       call v_ks_calc(ks, hm, st, geo)
     endif
