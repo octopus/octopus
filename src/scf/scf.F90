5302e0d7290493215e2a8dd82caa4e766d216136
David Strubbe <dstrubbe@ucmerced.edu>
2015-02-23 22:45:50 +0000

    A warning if the density becomes negative in SCF.

--- src/scf/scf.F90
+++ src/scf/scf.F90
@@ -743,6 +743,10 @@ contains
         ! mix input and output densities and compute new potential
         if(.not. cmplxscl) then
           call dmixing(scf%smix, rhoin, rhoout, rhonew, dmf_dotp_aux)
+          if(minval(rhonew(1:gr%fine%mesh%np, 1, 1:nspin)) < -M_EPSILON) then
+            write(message(1),*) 'Negative density after mixing. Minimum value = ', minval(rhonew(1:gr%fine%mesh%np, 1, 1:nspin))
+            call messages_warning(1)
+          endif
           st%rho(1:gr%fine%mesh%np, 1:nspin) = rhonew(1:gr%fine%mesh%np, 1, 1:nspin)
         else
           call zmixing(scf%smix, zrhoin, zrhoout, zrhonew, zmf_dotp_aux)
