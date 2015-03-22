7746423ec3ae9d3528e8cec4f65688868aec1e9f
David Strubbe <dstrubbe@ucmerced.edu>
2015-03-22 20:52:16 +0000

    Backport of 13459 to branches/5.0.x: ------------------------------------------------------------------------ r13459 | dstrubbe | 2015-03-20 17:19:08 -0400 (Fri, 20 Mar 2015) | 2 lines
    Some use of only_root_writes, to help ensure that messages actually get written to the console.
    
    ------------------------------------------------------------------------

--- src/system/subspace.F90
+++ src/system/subspace.F90
@@ -117,7 +117,7 @@ contains
         default = SD_SCALAPACK
 #else
         message(1) = 'Parallelization in states of the ground state requires scalapack.'
-        call messages_fatal(1)
+        call messages_fatal(1, only_root_writes = .true.)
 #endif
       end if
 
@@ -132,11 +132,11 @@ contains
     if(this%method == SD_SCALAPACK) then
 #ifndef HAVE_MPI
       message(1) = 'The scalapack subspace diagonalization can only be used in parallel.'
-      call messages_fatal(1)
+      call messages_fatal(1, only_root_writes = .true.)
 #else
 #ifndef HAVE_SCALAPACK
       message(1) = 'The scalapack subspace diagonalization requires scalapack.'
-      call messages_fatal(1)
+      call messages_fatal(1, only_root_writes = .true.)
 #endif
       if(st%dom_st_mpi_grp%size == 1) then
         message(1) = 'The scalapack subspace diagonalization is designed to be used with domain or state parallelization.'
@@ -153,7 +153,7 @@ contains
 #ifdef HAVE_MPI
     if(this%method == SD_STANDARD .and. st%parallel_in_states) then
       message(1) = 'The standard subspace diagonalization cannot work with state parallelization.'
-      call messages_fatal(1)
+      call messages_fatal(1, only_root_writes = .true.)
     end if
 #endif
 
