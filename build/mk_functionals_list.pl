1a4966ac310f0867edebb0725d1c1f5e1d79e9f2
David Strubbe <dstrubbe@ucmerced.edu>
2015-03-25 00:07:27 +0000

    svn:keywords

--- build/mk_functionals_list.pl
+++ build/mk_functionals_list.pl
@@ -17,7 +17,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 #
-# $Id: mk_varinfo.pl 5657 2009-06-29 15:02:07Z marques $
+# $Id$
 
 
 use Getopt::Std;
--- build/true_compiler.sh
+++ build/true_compiler.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # 
-# $Id: true_compiler.sh 4655 2008-10-13 17:31:10Z acastro $
+# $Id$
 
 # If the argument is mpicc or mpif90, prints the true compiler that is
 # being called. For the moment it only works if the -show argument is
