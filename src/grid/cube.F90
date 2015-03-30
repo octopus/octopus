c11ce7497153cd335f1c0df79de1a82b2b9a0044
David Strubbe <dstrubbe@ucmerced.edu>
2015-03-30 14:45:48 +0000

    Backport of 13657 to branches/5.0.x: ------------------------------------------------------------------------ r13657 | dstrubbe | 2015-03-29 16:51:18 -0400 (Sun, 29 Mar 2015) | 2 lines
    Fixing incorrect types listed for two input variables.
    
    ------------------------------------------------------------------------

--- src/grid/cube.F90
+++ src/grid/cube.F90
@@ -150,7 +150,7 @@ contains
         fft_library_ = fft_library
       else
         !%Variable FFTLibrary
-        !%Type logical
+        !%Type integer
         !%Section Mesh::FFTs
         !%Default fftw 
         !%Description
