122a0d058cab4fda7103cd33c7144cc089307f16
David Strubbe <dstrubbe@ucmerced.edu>
2015-05-27 20:44:27 +0000

    Explicit allocation instead of the uninstrumented loct_pointer_copy removes a false positive of non-deallocated memory from profiling.

--- src/states/states_dim.F90
+++ src/states/states_dim.F90
@@ -124,7 +124,8 @@ contains
     dout%pack_states    = din%pack_states
     dout%cl_states_mem  = din%cl_states_mem
 
-    call loct_pointer_copy(dout%kweights, din%kweights)
+    SAFE_ALLOCATE(dout%kweights(1:din%nik))
+    dout%kweights(:) = din%kweights(:)
 
     call distributed_copy(din%kpt, dout%kpt)
 
