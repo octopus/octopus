9a9c27ee0b09424f4da4c8d3ebda10113e41c4e8
Xavier Andrade <xavier@tddft.org>
2015-05-29 22:39:22 +0000

    Added Biswajit Santra (coauthor of the original quantum espresso implementation) to the copyright note for Tkatchenko-Scheffler.

--- src/hamiltonian/vdw_ts.F90
+++ src/hamiltonian/vdw_ts.F90
@@ -1,4 +1,4 @@
-!! Copyright (C) 2015 X. Andrade, R. A. DiStasio Jr.
+!! Copyright (C) 2015 X. Andrade, R. A. DiStasio Jr., B. Santra
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
