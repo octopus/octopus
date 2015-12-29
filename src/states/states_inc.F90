ebb4fe88c204790ebad8372cf3e84afb92d9a2a0
Xavier Andrade <xavier@tddft.org>
2015-12-29 17:51:08 +0000

    3-index version of states_get_state.

--- src/states/states_inc.F90
+++ src/states/states_inc.F90
@@ -83,6 +83,26 @@ end subroutine X(states_get_state4)
 
 ! ------------------------------------------------------------
 
+subroutine X(states_get_state3)(st, mesh, iqn, psi, left)
+  type(states_t),    intent(in)    :: st
+  type(mesh_t),      intent(in)    :: mesh
+  integer,           intent(in)    :: iqn
+  R_TYPE,            intent(inout) :: psi(:, :, st%st_start:)
+  logical, optional, intent(in)    :: left
+  
+  integer :: ist
+  
+  PUSH_SUB(X(states_get_state3))
+
+  do ist = st%st_start, st%st_end
+    call states_get_state(st, mesh, ist, iqn, psi(:, :, ist), left)
+  end do
+  
+  POP_SUB(X(states_get_state3))
+end subroutine X(states_get_state3)
+
+! ------------------------------------------------------------
+
 subroutine X(states_set_state2)(st, mesh, ist, iqn, psi, left)
   type(states_t),    intent(inout) :: st
   type(mesh_t),      intent(in)    :: mesh
@@ -124,6 +144,26 @@ subroutine X(states_set_state1)(st, mesh, idim, ist, iqn, psi, left)
   
   POP_SUB(X(states_set_state1))
 end subroutine X(states_set_state1)
+
+! ------------------------------------------------------------
+
+subroutine X(states_set_state3)(st, mesh, iqn, psi, left)
+  type(states_t),    intent(inout) :: st
+  type(mesh_t),      intent(in)    :: mesh
+  integer,           intent(in)    :: iqn
+  R_TYPE,            intent(in)    :: psi(:, :, st%st_start:)
+  logical, optional, intent(in)    :: left
+  
+  integer :: ist
+  
+  PUSH_SUB(X(states_set_state3))
+
+  do ist = st%st_start, st%st_end
+    call states_set_state(st, mesh, ist, iqn, psi(:, :, ist), left)
+  end do
+  
+  POP_SUB(X(states_set_state3))
+end subroutine X(states_set_state3)
 
 ! ------------------------------------------------------------
 
