8ae1df90e92133106f52ac007feca12d4336141f
Umberto De Giovannini <umberto.degiovannini@gmail.com>
2016-02-24 23:20:07 +0000

    Calculate the pcm_potential as a solution of the Poisson equation for a gaussian smeared polarizaion charge density. This is much faster than the previously implemente direct sum but the results are slightly different. These discrepancies need to be carefully checked.

--- src/system/v_ks.F90
+++ src/system/v_ks.F90
@@ -1215,10 +1215,8 @@ contains
     !> PCM reaction field due to the electronic density
     if (hm%pcm%run_pcm .and. pcm_update(hm%pcm,hm%current_time)) then
     !> Generates the real-space PCM potential due to electrons during the SCF calculation.
-        call pcm_v_electrons_cav_li(hm%pcm%v_e, pot, hm%pcm, ks%gr%mesh)
-        call pcm_charges(hm%pcm%q_e, hm%pcm%qtot_e, hm%pcm%v_e, hm%pcm%matrix, hm%pcm%n_tesserae) 
-        call pcm_pot_rs( hm%pcm%v_e_rs, hm%pcm%q_e, hm%pcm%tess, hm%pcm%n_tesserae, ks%gr%mesh, hm%pcm%gaussian_width )
-
+        call pcm_calc_pot_rs(hm%pcm, ks%gr%mesh, v_h = pot)
+        
         ! Calculating the PCM term renormalizing the sum of the single-particle energies
         hm%energy%pcm_corr = dmf_dotp( ks%gr%fine%mesh, ks%calc%total_density, hm%pcm%v_e_rs + hm%pcm%v_n_rs )
     end if
