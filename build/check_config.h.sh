9e83cfa29fba7a0e16c84fe04a782a8c8aa5e509
David Strubbe <dstrubbe@ucmerced.edu>
2016-02-25 20:47:23 +0000

    Add bash script for buildbot to use in checking config.h against reference.

---
+++ build/check_config.h.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2016 D. Strubbe
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+# $Id$
+
+# Used by buildbot to determine if result of configure, encoded in config.h,
+# has changed from the reference result.
+
+BUILDER="$1"
+BRANCH="$2"
+
+if [ "x$BRANCH" == "x" ]; then
+    BRANCH=trunk
+fi
+    
+# show commands in terminal output
+set -x
+
+# download ref_file with svn
+svn export --force http://www.tddft.org/svn/octopus/buildbot/config.h/$BRANCH/$BUILDER
+
+# these two fields will generally be different, and that is fine
+diff -I '^#define BUILD_TIME' -I '^#define LATEST_SVN' config.h $BUILDER
+
+# this is to make this script succeed regardless, for temporary testing purposes.
+echo "Done with diff"
