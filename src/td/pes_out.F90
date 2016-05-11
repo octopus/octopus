0f907bd1ab591ca5f934bfaf875fbf53182d8fb4
Umberto De Giovannini <umberto.degiovannini@gmail.com>
2016-05-11 09:13:57 +0000

    Moving general purpose PES output routines into their own module

---
+++ src/td/pes_out.F90
@@ -0,0 +1,587 @@
+!! Copyright (C) 2016 U. De Giovannini
+!!
+!! This program is free software; you can redistribute it and/or modify
+!! it under the terms of the GNU General Public License as published by
+!! the Free Software Foundation; either version 2, or (at your option)
+!! any later version.
+!!
+!! This program is distributed in the hope that it will be useful,
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!! GNU General Public License for more details.
+!!
+!! You should have received a copy of the GNU General Public License
+!! along with this program; if not, write to the Free Software
+!! Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+!! 02110-1301, USA.
+!!
+!! $Id: pes.F90 15203 2016-03-19 13:15:05Z xavier $
+
+#include "global.h"
+
+module pes_out_oct_m
+  use cube_function_oct_m
+  use cube_oct_m
+  use geometry_oct_m
+  use global_oct_m
+  use grid_oct_m
+  use io_oct_m
+  use loct_oct_m
+  use math_oct_m
+  use mesh_oct_m
+  use messages_oct_m
+  use mpi_oct_m
+  use output_oct_m
+  use parser_oct_m
+  use profiling_oct_m
+  use qshep_oct_m
+  use restart_oct_m
+  use simul_box_oct_m
+  use sort_oct_m
+  use states_oct_m
+  use string_oct_m
+  use unit_oct_m
+  use unit_system_oct_m
+  use varinfo_oct_m
+  use vtk_oct_m
+    
+  implicit none
+
+  private
+
+  public ::                             &
+    pes_out_velocity_map,               &
+    pes_out_velocity_map_cut,           &
+    pes_out_arpes_cut
+
+  integer, parameter ::   &
+    INTEGRATE_NONE    = -1,       &
+    INTEGRATE_PHI     =  1,       &
+    INTEGRATE_THETA   =  2,       &
+    INTEGRATE_R       =  3,       &
+    INTEGRATE_KX      =  4,       &
+    INTEGRATE_KY      =  5,       &
+    INTEGRATE_KZ      =  6
+
+
+
+contains
+
+  ! ---------------------------------------------------------
+  subroutine pes_out_velocity_map(pesK, file, Lk, ll, how, sb, pmesh)
+    FLOAT,             intent(in) :: pesK(:,:,:)
+    character(len=*),  intent(in) :: file
+    FLOAT,             intent(in) :: Lk(:,:)
+    integer,           intent(in) :: ll(:)  
+    integer,           intent(in) :: how
+    type(simul_box_t), intent(in) :: sb 
+    FLOAT, optional,   intent(in) :: pmesh(:,:,:,:)  
+  
+    integer :: iunit
+    integer :: ierr
+    character(len=512) :: filename
+    type(cube_t) :: cube
+    type(cube_function_t) :: cf
+    integer :: ii
+    FLOAT :: dk(3)  
+
+    PUSH_SUB(pes_out_velocity_map)
+
+    call cube_init(cube, ll, sb)
+    call cube_function_null(cf)
+    call dcube_function_alloc_RS(cube, cf, force_alloc = .true.)
+    cf%dRS = pesK
+  
+  
+    if (.not. present(pmesh) ) then
+      ! Ignore Lk and use pmesh
+      dk(:) = M_ZERO
+      dk(1:sb%dim) = abs(Lk(2,1:sb%dim)-Lk(1,1:sb%dim))
+      do ii = 1, sb%dim
+        dk(ii) = units_from_atomic(sqrt(units_out%energy), dk(ii))
+      end do
+    end if
+  
+  #if defined(HAVE_NETCDF)  
+  
+    if(iand(how, OPTION__OUTPUTFORMAT__NETCDF) /= 0) then
+      filename = trim(file)//".ncdf"
+      write(message(1), '(a)') 'Writing netcdf format file: '
+      call messages_info(1)
+  
+      call dout_cf_netcdf(filename, ierr, cf, cube, sb%dim, dk(:) , & 
+            .false., sqrt(units_out%energy)**sb%dim)
+
+    end if
+
+  #endif
+  
+    if(iand(how, OPTION__OUTPUTFORMAT__VTK) /= 0)  then
+      filename = trim(file)//".vtk"
+      write(message(1), '(a)') 'Writing vtk format file: '
+      call messages_info(1)
+    
+      if (present(pmesh)) then          
+        call dvtk_out_cf_structured(filename, ierr, cf, cube,& 
+          sqrt(units_out%energy)**sb%dim, pmesh, ascii = .false.)
+      else 
+        call dvtk_out_cf(filename, ierr, cf, cube, dk(:),& 
+          sqrt(units_out%energy)**sb%dim)
+      end if        
+    end if
+  
+    call cube_end(cube)
+    call dcube_function_free_RS(cube, cf)
+
+    POP_SUB(pes_out_velocity_map)
+    
+  end subroutine pes_out_velocity_map
+
+
+  ! ---------------------------------------------------------
+  subroutine pes_out_arpes_cut(arpes, file, ll, pmesh, Ekin)
+    FLOAT,             intent(in) :: arpes(:,:,:)
+    character(len=*),  intent(in) :: file
+    integer,           intent(in) :: ll(:)
+    FLOAT,             intent(in) :: pmesh(:,:,:,:)
+    FLOAT,             intent(in) :: Ekin(:,:,:)
+    
+    integer :: iunit, ip, ie
+    FLOAT   :: dp, length, pp(1:2), pdiff(1:2)
+    
+    PUSH_SUB(pes_out_arpes_cut)
+    
+    iunit = io_open(file, action='write')
+    write(iunit, '(a)') '##################################################'
+    write(iunit, '(a1,a18,2x,a18,2x,a18,2x,a18,2x, a18,2x,a18)') '#', &
+                                      str_center("Ppath", 18), &
+                                      str_center("px", 18), str_center("py", 18), &
+                                      str_center("E", 18),  str_center("P[Ppath,E]", 18)
+
+    write(iunit, '(a1,a18,2x,a18,2x,a18,2x,a18,2x, a18,2x,a18)') '#', &
+                                      str_center('[hbar/'//trim(units_abbrev(units_out%length)) // ']', 18), &
+                                      str_center('[hbar/'//trim(units_abbrev(units_out%length)) // ']', 18), &
+                                      str_center('[hbar/'//trim(units_abbrev(units_out%length)) // ']', 18), &    
+                                      str_center('['//trim(units_abbrev(units_out%energy)) // ']', 18), &
+                                      str_center('[1/' //trim(units_abbrev(units_out%energy))//']', 18)
+    write(iunit, '(a)') '##################################################'
+    
+    
+    length = M_ZERO
+    pdiff(:) = M_ZERO
+    
+    do ip = 1, ll(1)
+      pp(1:2) = pmesh(ip,1,1,1:2)
+      if (ip > 1) pdiff = pp(1:2) - pmesh(ip-1,1,1,1:2)
+      dp = sqrt(sum(pdiff(1:2)**2))
+      length = length + dp 
+      
+      do ie = 1, ll(3) 
+        write(iunit, '(es19.12,2x,es19.12,2x,es19.12,2x,es19.12,2x,es19.12,2x,es19.12)')   &
+                                        units_from_atomic(unit_one/units_out%length, length),&
+                                        units_from_atomic(unit_one/units_out%length, pp(1)), &
+                                        units_from_atomic(unit_one/units_out%length, pp(2)), &
+                                        units_from_atomic(units_out%energy, Ekin(ip,1,ie)), &
+                                        arpes(ip,1,ie)
+      end do      
+      write(iunit, *)
+      
+       
+    end do
+    
+    
+    call io_close(iunit)
+    
+    POP_SUB(pes_out_arpes_cut)    
+  end subroutine pes_out_arpes_cut
+
+
+  ! ---------------------------------------------------------
+  subroutine pes_out_velocity_map_cut(pesK, file, ll, dim, pol, dir, integrate, pos, Lk, pmesh)
+    FLOAT,             intent(in) :: pesK(:,:,:)
+    character(len=*),  intent(in) :: file
+    integer,           intent(in) :: ll(:)
+    integer,           intent(in) :: dim
+    FLOAT,             intent(in) :: pol(3)
+    integer,           intent(in) :: dir
+    integer,           intent(in) :: integrate
+    integer, optional, intent(in) :: pos(3)
+    FLOAT, optional,   intent(in) :: Lk(:,:)
+    FLOAT, optional,   intent(in) :: pmesh(:,:,:,:)
+
+    integer              :: ii, ix, iy, iunit,ldir(2), icut(3)
+    FLOAT                :: KK(3),temp
+    integer, allocatable :: idx(:,:)
+    FLOAT, allocatable   :: Lk_(:,:)
+    FLOAT                :: rotation(1:dim,1:dim)
+    logical              :: aligned_axis
+  ! integration
+    FLOAT                :: K, KKK(3), theta, phi, Dphi, Dk(3)
+    integer              :: iph, Nphi
+  ! progress
+    integer              :: idone, ntodo
+
+    FLOAT, pointer :: cube_f(:)
+    type(qshep_t) :: interp
+
+    PUSH_SUB(pes_out_velocity_map_cut)
+
+    iunit = io_open(file, action='write')
+
+
+
+    ASSERT(size(pesK, 1) == ll(1))
+
+    if (.not. present(pmesh)) then
+      ASSERT(present(Lk))
+
+      SAFE_ALLOCATE(idx(1:maxval(ll(:)), 1:3))
+      SAFE_ALLOCATE(Lk_(1:maxval(ll(:)), 1:3))
+
+      Dk(:) = M_ZERO
+      Dk(1:dim) = abs(Lk(2,1:dim)-Lk(1,1:dim))
+
+      do ii = 1, 3
+        Lk_(:,ii) = Lk(:,ii)
+        call sort(Lk_(1:ll(ii), ii), idx(1:ll(ii), ii)) !We need to sort the k-vectors in order to dump in gnuplot format
+      end do
+    end if
+
+    aligned_axis = sum((pol-(/0 ,0 ,1/))**2)  <= M_EPSILON  .or. &
+                   sum((pol-(/0 ,1 ,0/))**2)  <= M_EPSILON  .or. &
+                   sum((pol-(/1 ,0 ,0/))**2)  <= M_EPSILON
+
+    if (present(pos)) then
+      icut(1:3) = pos(1:3)
+    else
+      icut(1:3) = ll(1:3)/2 + 1
+    end if
+
+    if (aligned_axis .and. integrate == INTEGRATE_NONE) then !no need to rotate and interpolate
+
+      select case (dir)
+        case (1)
+          ldir(:) =(/2,3/)
+        case (2)
+          ldir(:) =(/1,3/)
+        case (3)
+          ldir(:) =(/1,2/)
+
+      end select
+
+
+      if (present(pmesh)) then
+        do ix = 1, ll(ldir(1))
+          do iy = 1, ll(ldir(2))
+
+            select case (dir)
+              case (1)
+                temp = pesK(icut(dir), ix, iy)
+                KK(1) = pmesh(icut(dir), ix, iy, ldir(1))
+                KK(2) = pmesh(icut(dir), ix, iy, ldir(2))
+              case (2)
+                temp = pesK(ix, icut(dir), iy)
+                KK(1) = pmesh(ix, icut(dir), iy, ldir(1))
+                KK(2) = pmesh(ix, icut(dir), iy, ldir(2))
+              case (3)
+                temp = pesK(ix, iy, icut(dir))
+                KK(1) = pmesh(ix, iy, icut(dir), ldir(1))
+                KK(2) = pmesh(ix, iy, icut(dir), ldir(2))
+            end select
+
+            write(iunit, '(es19.12,2x,es19.12,2x,es19.12)') &
+                    units_from_atomic(sqrt(units_out%energy), KK(1)),&
+                    units_from_atomic(sqrt(units_out%energy), KK(2)),&
+                    temp
+          end do
+          write(iunit, *)
+        end do
+
+      else
+        do ix = 1, ll(ldir(1))
+          KK(1) = Lk_(ix, ldir(1))
+          do iy = 1, ll(ldir(2))
+            KK(2) = Lk_(iy, ldir(2))
+
+            select case (dir)
+              case (1)
+                temp = pesK(idx(icut(dir), 1), idx(ix, 2), idx(iy, 3))
+              case (2)
+                temp = pesK(idx(ix, 1), idx(icut(dir), 2), idx(iy, 3))
+              case (3)
+                temp = pesK(idx(ix, 1), idx(iy, 2), idx(icut(dir), 3))
+            end select
+
+            write(iunit, '(es19.12,2x,es19.12,2x,es19.12)') &
+                    units_from_atomic(sqrt(units_out%energy), KK(1)),&
+                    units_from_atomic(sqrt(units_out%energy), KK(2)),&
+                    temp
+          end do
+          write(iunit, *)
+        end do
+      end if
+
+    else
+      ! We set the z-axis along the pol vector
+      call generate_rotation_matrix(rotation, (/M_ZERO, M_ZERO, M_ONE/), pol )
+
+      if(debug%info) then
+        print *,"Rotate z-axis over the zenith axis"
+        print *,rotation(1,:)
+        print *,rotation(2,:)
+        print *,rotation(3,:)
+      end if
+
+      call pes_out_interpolator_init(pesK, Lk, ll, dim, cube_f, interp, pmesh)
+
+      ntodo = product(ll(1:2))
+      idone = 0
+      call loct_progress_bar(-1, ntodo)
+
+      do ix = 1, ll(1)
+       do iy = 1, ll(2)
+
+         !cut
+         select case (dir)
+           case (1)
+             KK(1) = M_ZERO
+             if (present(pmesh)) then
+               KK(2) = pmesh(ix, 1, 1, 1)
+               KK(3) = pmesh(1, iy, 1, 2)
+             else
+               KK(2) = Lk_(ix, 1)
+               KK(3) = Lk_(iy, 2)
+             end if
+           case (2)
+             KK(2) = M_ZERO
+             if(present(pmesh)) then
+               KK(1) = pmesh(ix,  1, 1, 1)
+               KK(3) = pmesh(1,  iy, 1, 2)
+             else
+               KK(1) = Lk_(ix, 1)
+               KK(3) = Lk_(iy, 2)
+             end if
+
+           case (3)
+             KK(3) = M_ZERO
+             if(present(pmesh)) then
+               KK(1) = pmesh(ix,  1, 1, 1)
+               KK(2) = pmesh(1,  iy, 1, 2)
+             else
+               KK(1) = Lk_(ix, 1)
+               KK(2) = Lk_(iy, 2)
+             end if
+
+         end select
+
+         temp = qshep_interpolate(interp, cube_f, matmul(rotation,KK(1:3)) )
+
+         select case (integrate)
+           case (INTEGRATE_PHI)
+             temp = M_ZERO
+             K = sqrt(KK(1)**2 + KK(2)**2 + KK(3)**2)
+
+             Nphi = 360
+             Dphi = M_TWO * M_PI/Nphi
+
+             do iph = 0, Nphi
+               phi = iph * Dphi
+               theta = atan2(sqrt(KK(1)**2+KK(2)**2),KK(3))
+
+               KKK(1) = K *sin(theta)*cos(phi)
+               KKK(2) = K *sin(theta)*sin(phi)
+               KKK(3) = K *cos(theta)
+
+               temp = temp + &
+                             abs(qshep_interpolate(interp, cube_f, matmul(rotation,KKK(1:3)) ))
+             end do
+             temp = temp * Dphi
+
+           case (INTEGRATE_KX)
+             temp = M_ZERO
+             do ii =1, ll(1)
+                KKK(:) = KK(:) + (/Lk(ii,1), M_ZERO, M_ZERO/)
+                temp = temp + &
+                              abs(qshep_interpolate(interp, cube_f, matmul(rotation,KKK(1:3)) ))
+             end do
+             temp = temp * Dk(1)
+
+           case (INTEGRATE_KY)
+             temp = M_ZERO
+             do ii =1, ll(2)
+                KKK(:) = KK(:) + (/M_ZERO, Lk(ii,2), M_ZERO/)
+                temp = temp + &
+                              abs(qshep_interpolate(interp, cube_f, matmul(rotation,KKK(1:3)) ))
+             end do
+             temp = temp * Dk(2)
+
+           case (INTEGRATE_KZ)
+             temp = M_ZERO
+             do ii =1, ll(3)
+                KKK(:) = KK(:) + (/M_ZERO, M_ZERO, Lk(ii,3)/)
+                temp = temp + &
+                              abs(qshep_interpolate(interp, cube_f, matmul(rotation,KKK(1:3)) ))
+             end do
+             temp = temp * Dk(3)
+
+         end select
+
+         write(iunit, '(es19.12,2x,es19.12,2x,es19.12)') &
+                 units_from_atomic(sqrt(units_out%energy), KK(1)),&
+                 units_from_atomic(sqrt(units_out%energy), KK( 2)),&
+                 temp
+
+
+         idone = idone +1
+         call loct_progress_bar(idone, ntodo)
+
+       end do
+       write(iunit, *)
+      end do
+
+      write(stdout, '(1x)')
+
+
+    end if
+
+    call io_close(iunit)
+
+    SAFE_DEALLOCATE_A(idx)
+    SAFE_DEALLOCATE_A(Lk_)
+
+    POP_SUB(pes_out_velocity_map_cut)
+  end subroutine pes_out_velocity_map_cut
+
+  ! --------------------------------------------------------
+  !
+  !>  Qshep interpolation helper function initialization.
+  !!  Generates the linearized version of pesK (cube_f) and the associated
+  !!  qshep interpolator opbject (interp).
+  !
+  ! ---------------------------------------------------------
+  subroutine pes_out_interpolator_init(pesK, Lk, ll, dim, cube_f, interp, pmesh)
+    FLOAT,           intent(in)    :: pesK(:,:,:)
+    FLOAT,           intent(in)    :: Lk(:,:)
+    integer,         intent(in)    :: ll(:)
+    integer,         intent(in)    :: dim
+    FLOAT, pointer,  intent(inout) :: cube_f(:)
+    type(qshep_t),   intent(out)   :: interp
+    FLOAT, optional, intent(in)   :: pmesh(:,:,:,:)
+
+    integer :: np, ii, ix, iy, iz
+    FLOAT   :: KK(3)
+    FLOAT, allocatable ::  kx(:),ky(:),kz(:)
+
+    PUSH_SUB(pes_out_interpolator_init)
+
+
+    call messages_write("Initializing Qshep interpolator. Be patient it may take a while... ")
+    call messages_info()
+
+    np = ll(1)*ll(2)*ll(3)
+
+    !check dim
+    if (dim  <  2 .or. dim > 3) then
+      message(1) = "This interpolator works only for 2 <= dim <= 3."
+      call messages_fatal(1)
+    end if
+
+    SAFE_ALLOCATE(cube_f(1:np))
+
+    SAFE_ALLOCATE(kx(1:np))
+    SAFE_ALLOCATE(ky(1:np))
+    SAFE_ALLOCATE(kz(1:np))
+
+    cube_f = M_ZERO
+    kx = M_ZERO
+    ky = M_ZERO
+    kz = M_ZERO
+
+
+    ii=1
+    if (present(pmesh)) then
+      do ix = 1, ll(1)
+        do iy = 1, ll(2)
+          do iz = 1, ll(3)
+
+            cube_f(ii) =  pesK(ix,iy,iz)
+
+            kx(ii) = pmesh(ix,iy,iz,1)
+            ky(ii) = pmesh(ix,iy,iz,2)
+            kz(ii) = pmesh(ix,iy,iz,3)
+
+            ii = ii +1
+          end do
+        end do
+      end do
+
+    else
+
+      do ix = 1, ll(1)
+        KK(1) = Lk(ix, 1)
+        do iy = 1, ll(2)
+          KK(2) = Lk(iy, 2)
+          do iz = 1, ll(3)
+            KK(3) = Lk(iz, 3)
+
+            cube_f(ii) =  pesK(ix,iy,iz)
+
+            kx(ii) = KK(1)
+            ky(ii) = KK(2)
+            kz(ii) = KK(3)
+
+            ii = ii +1
+          end do
+        end do
+      end do
+    end if
+
+
+    select case(dim)
+      case (2)
+        call qshep_init(interp, np, cube_f, kx, ky)
+      case (3)
+        call qshep_init(interp, np, cube_f, kx, ky, kz)
+    end select
+
+    SAFE_DEALLOCATE_A(kx)
+    SAFE_DEALLOCATE_A(ky)
+    SAFE_DEALLOCATE_A(kz)
+
+    call messages_write("done")
+    call messages_new_line()
+    call messages_info()
+
+
+    POP_SUB(pes_out_interpolator_init)
+  end subroutine pes_out_interpolator_init
+
+  ! ---------------------------------------------------------
+  !>  Destroy the interpolation objects
+  ! ---------------------------------------------------------
+  subroutine pes_out_interpolator_end(cube_f, interp)
+    FLOAT, pointer, intent(inout) :: cube_f(:)
+    type(qshep_t),  intent(inout) :: interp
+
+    PUSH_SUB(pes_out_interpolator_end)
+
+    call qshep_end(interp)
+
+    SAFE_DEALLOCATE_P(cube_f)
+
+    POP_SUB(pes_out_interpolator_end)
+  end subroutine pes_out_interpolator_end
+
+
+
+
+
+
+
+end module pes_out_oct_m
+
+
+!! Local Variables:
+!! mode: f90
+!! coding: utf-8
+!! End:
--- src/utils/photoelectron_spectrum.F90
+++ src/utils/photoelectron_spectrum.F90
@@ -33,7 +33,8 @@ program photoelectron_spectrum
   use parser_oct_m
   use pes_oct_m  
   use pes_mask_oct_m  
-  use pes_flux_oct_m  
+  use pes_flux_oct_m
+  use pes_out_oct_m  
   use profiling_oct_m
   use restart_oct_m
   use simul_box_oct_m
@@ -56,7 +57,7 @@ program photoelectron_spectrum
   end type pesoutput_t  
 
   integer              :: ierr, integrate
-  integer              :: dim, dir, how, idim
+  integer              :: dim, dir, how, idim, pdim, ngpt
   integer              :: llp(3), llg(3)   !< The size of the g-point and p-point cubic grids 
   FLOAT                :: Emax, Emin, Estep, uEstep,uEspan(2), pol(3)
   FLOAT                :: uThstep, uThspan(2), uPhstep, uPhspan(2), pvec(3)
@@ -85,6 +86,7 @@ program photoelectron_spectrum
   integer              :: ist, ispin  
   FLOAT, pointer       :: pesP_out(:,:,:)    
   FLOAT, allocatable, target :: pesP(:,:,:,:)     !< The momentum-resolved photoelectron spectrum
+  FLOAT, allocatable   :: Ekin(:,:,:)    
   
   type(pes_flux_t)     :: pflux
   integer              :: pes_method, option 
@@ -116,9 +118,11 @@ program photoelectron_spectrum
   !Initialize variables
   llp(:) = 1 
   llg(:) = 1
+  ngpt = 1
 
   need_pmesh = .false. 
-  dim = sb%dim   ! The dimensionality dim = [1,2,3]
+  dim    = sb%dim   ! The dimensionality dim = [1,2,3]
+  pdim   = sb%periodic_dim
 
   call messages_print_stress(stdout,"Postprocessing")  
   
@@ -155,6 +159,7 @@ program photoelectron_spectrum
     call pes_flux_reciprocal_mesh_gen(pflux, sb, st, 0, post = .true.)
     
     llg(1:dim) = pflux%ll(1:dim)
+    ngpt = pflux%ngpt
     need_pmesh = .true.
     
   
@@ -218,7 +223,7 @@ program photoelectron_spectrum
       pol = (/0,0,1/) 
       pvec = (/0,1,0/)
 
-      pesout%what = OPTION__PHOTOELECTRONSPECTRUMOUTPUT__VELOCITY_MAP_CUT
+      pesout%what = OPTION__PHOTOELECTRONSPECTRUMOUTPUT__ARPES_CUT
       pesout%pol = (/0,0,1/) 
       pesout%pvec = (/0,1,0/)
     end if 
@@ -266,13 +271,17 @@ program photoelectron_spectrum
   krng(2) =  kpoints_number(sb%kpoints)
   
   if (have_zweight_path) then 
+    
+    
     krng(1) = kpoints_number(sb%kpoints) - sb%kpoints%nik_skip  + 1
     
     call messages_print_stress(stdout, "Kpoint selection")
     write(message(1), '(a)') 'Will use a zero-weight path in reciprocal space with the following points'
     call messages_info(1)
     ! Figure out the direction of the path - it must be along kx or ky only
-    call get_kpath_direction(sb%kpoints, krng, kpth_dir, pvec)
+!     call get_kpath_direction(sb%kpoints, krng, kpth_dir, pvec)
+    kpth_dir = 1
+    pvec = (/0,1,0/)
     
     call write_kpoints_info(sb%kpoints, krng(1), krng(2))    
     call messages_print_stress(stdout)
@@ -289,6 +298,7 @@ program photoelectron_spectrum
   else
     llp(1:dim) = llg(1:dim) * sb%kpoints%nik_axis(1:dim)    
   endif  
+  llp(1:pdim) = llp(1:pdim) * ngpt
   
   if (debug%info) then
     print *, "llp(:)= ", llp(:) 
@@ -306,7 +316,9 @@ program photoelectron_spectrum
   case (OPTION__PHOTOELECTRONSPECTRUM__PES_FLUX)
     ! Lp is allocated inside pes_flux_pmesh to comply with the 
     ! declinations of the different surfaces
-    call pes_flux_pmesh(pflux, dim, sb%kpoints, llg, Lg, pmesh, idxZero, krng, Lp)    
+    SAFE_ALLOCATE(Ekin(1:llp(1),1:llp(2),1:llp(3)))
+    Ekin = M_ZERO
+    call pes_flux_pmesh(pflux, dim, sb%kpoints, llg, Lg, pmesh, idxZero, krng, Lp, Ekin)    
   end select
    
 
@@ -355,6 +367,8 @@ program photoelectron_spectrum
   !% The output format can be controlled with OutputHow and can be vtk or ncdf.  
   !%Option arpes bit(7)
   !% Full ARPES for semi-periodic systems (vtk).
+  !%Option arpes_cut bit(8)
+  !% ARPES cut on a plane following a zero-weight path in reciprocal space.
   !%End
   call parse_variable('PhotoelectronSpectrumOutput', pesout%what, pesout%what)
   
@@ -439,6 +453,7 @@ program photoelectron_spectrum
   SAFE_DEALLOCATE_A(pesP)    
   SAFE_DEALLOCATE_A(pmesh)
   SAFE_DEALLOCATE_P(Lp)
+  SAFE_DEALLOCATE_A(Ekin)
   if (.not. need_pmesh .or. pes_method == OPTION__PHOTOELECTRONSPECTRUM__PES_MASK) then
     SAFE_DEALLOCATE_P(Lg)
   end if
@@ -550,7 +565,6 @@ program photoelectron_spectrum
         if (have_zweight_path) then
           filename = outfile('PES_velocity_map',ist,ispin,'path')
         else
-!           filename = "PES_velocity_map.p"//index2axis(dir)//"=0"
           filename = outfile('PES_velocity_map',ist,ispin,'p'//index2axis(dir)//'=0')
         end if
 
@@ -566,15 +580,18 @@ program photoelectron_spectrum
           write(message(1), '(a)') 'Integrate on: '//index2var(integrate)
           call messages_info(1)
           filename = trim(filename)//'.i_'//trim(index2var(integrate))
-!           filename = "PES_velocity.map.i_"//".p"//index2axis(dir)//"=0"
         end if
 
         if (need_pmesh) then
-          call pes_mask_output_full_mapM_cut(pesP_out, filename, llp, dim, pol, dir, integrate, &
+          call pes_out_velocity_map_cut(pesP_out, filename, llp, dim, pol, dir, integrate, &
                                              pos = idxZero, pmesh = pmesh)
+!           call pes_mask_output_full_mapM_cut(pesP_out, filename, llp, dim, pol, dir, integrate, &
+!                                              pos = idxZero, pmesh = pmesh)
         else
-          call pes_mask_output_full_mapM_cut(pesP_out, filename, llp, dim, pol, dir, integrate, &
+          call pes_out_velocity_map_cut(pesP_out, filename, llp, dim, pol, dir, integrate, &
                                              pos = idxZero, Lk = Lg)
+!           call pes_mask_output_full_mapM_cut(pesP_out, filename, llp, dim, pol, dir, integrate, &
+!                                              pos = idxZero, Lk = Lg)
         end if
       end if
 
@@ -630,9 +647,12 @@ program photoelectron_spectrum
         if (need_pmesh) then
           !force vtk output
           how = io_function_fill_how("VTK")
-          call pes_mask_output_full_mapM(pesP_out, filename, Lg, llp, how, sb, pmesh)
+           
+!           call pes_mask_output_full_mapM(pesP_out, filename, Lg, llp, how, sb, pmesh)
+          call pes_out_velocity_map(pesP_out, filename, Lg, llp, how, sb, pmesh)
         else
-          call pes_mask_output_full_mapM(pesP_out, filename, Lg, llp, how, sb)
+!           call pes_mask_output_full_mapM(pesP_out, filename, Lg, llp, how, sb)
+          call pes_out_velocity_map(pesP_out, filename, Lg, llp, how, sb)
         end if
         
       end if
@@ -647,10 +667,20 @@ program photoelectron_spectrum
 
         how = io_function_fill_how("VTK")
 
-        call pes_mask_output_full_mapM(pesP_out, outfile('./PES_ARPES', ist, ispin), &
+!         call pes_mask_output_full_mapM(pesP_out, outfile('./PES_ARPES', ist, ispin), &
+!                                        Lg, llp, how, sb, pmesh)
+        call pes_out_velocity_map(pesP_out, outfile('./PES_ARPES', ist, ispin), &
                                        Lg, llp, how, sb, pmesh)
       end if
       
+      
+      if(iand(pesout%what, OPTION__PHOTOELECTRONSPECTRUMOUTPUT__ARPES_CUT) /= 0) then
+        call messages_print_stress(stdout, "ARPES cut on reciprocal space path")
+        
+        filename = outfile('./PES_ARPES', ist, ispin, "path")
+        call pes_out_arpes_cut(pesP_out, filename, llp, pmesh, Ekin)
+        
+      end if
       
       
     end subroutine output_pes
