c5267f8b73944e32f7db744ac1b1d5f811113c34
Hannes Huebener <hannes.huebener@gmail.com>
2016-05-21 12:42:39 +0000

    change in argument list that I overlooked in my previous commit

--- src/opt_control/propagation.F90
+++ src/opt_control/propagation.F90
@@ -236,7 +236,7 @@ contains
 
       ! only write in final run
       if(write_iter_) then
-        call td_write_iter(write_handler, gr, psi, hm, sys%geo, hm%ep%kick, td%dt, istep)
+        call td_write_iter(write_handler, gr, psi, hm, sys%geo, hm%ep%kick, td%dt, sys%ks, istep)
         ii = ii + 1 
         if(ii == sys%outp%output_interval+1 .or. istep == td%max_iter) then ! output
           if(istep == td%max_iter) sys%outp%output_interval = ii - 1
