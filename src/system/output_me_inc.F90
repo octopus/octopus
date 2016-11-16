0ee6fe3bd45c7709888cfcde38f4b020cb4d5379
Nicolas Tancogne-Dejean <tnico@eos01.bc.rzg.mpg.de>
2016-11-16 19:09:11 +0100

    Adding a check for MGGAs.

--- src/system/output_me_inc.F90
+++ src/system/output_me_inc.F90
@@ -264,6 +264,8 @@ end subroutine X(output_me_ks_multipoles1d)
 
 ! ---------------------------------------------------------
 subroutine X(one_body) (dir, gr, geo, st, hm)
+  use XC_F90(lib_m)
+
   character(len=*),    intent(in)    :: dir
   type(grid_t),        intent(inout) :: gr
   type(geometry_t),    intent(in)    :: geo
@@ -284,6 +286,7 @@ subroutine X(one_body) (dir, gr, geo, st, hm)
 
   ASSERT(.not. st%parallel_in_states)
   if(gr%mesh%sb%kpoints%full%npoints > 1) call messages_not_implemented("OutputMatrixElements=one_body with k-points")
+  if(iand(hm%xc_family, XC_FAMILY_MGGA + XC_FAMILY_HYB_MGGA) /= 0) call messages_not_implemented("OutputMatrixElements=one_body with MGGA")
 
   ! how to do this properly? states_matrix
   iunit = io_open(trim(dir)//'/output_me_one_body', action='write')
