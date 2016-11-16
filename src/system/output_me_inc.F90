4f13c37983e2297c29487c060a9a3b3873ca2211
Nicolas Tancogne-Dejean <tnico@eos01.bc.rzg.mpg.de>
2016-11-16 22:18:06 +0100

    This sould solve the problem with gfortran.

--- src/system/output_me_inc.F90
+++ src/system/output_me_inc.F90
@@ -265,6 +265,7 @@ end subroutine X(output_me_ks_multipoles1d)
 ! ---------------------------------------------------------
 subroutine X(one_body) (dir, gr, geo, st, hm)
   use XC_F90(lib_m)
+  use xc_functl_oct_m
 
   character(len=*),    intent(in)    :: dir
   type(grid_t),        intent(inout) :: gr
