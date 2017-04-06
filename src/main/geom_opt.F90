3ade6226b1540d934810b5cfb7c794baa81f08ec
Nicolas Tancogne-Dejean <tnico@eos01.bc.rzg.mpg.de>
2017-04-06 18:18:10 +0200

    Check if the file last.xyz exists before trying to open it.

--- src/main/geom_opt.F90
+++ src/main/geom_opt.F90
@@ -422,6 +422,12 @@ contains
       call loct_rm("./work-geom.xyz")
 
       if(.not. fromScratch) then
+        inquire(file = './last.xyz', exist = does_exist)
+        if(.not. does_exist) then
+          write(message(1), '(a)') "The file last.xyz does not exist. Octopus cannot restart the GO calculation."
+          write(message(2), '(a)') "Please use FromScratch=yes to start the calculation from scratch."
+          call messages_fatal(2)          
+        end if
         call geometry_read_xyz(g_opt%geo, './last')
       end if
 
