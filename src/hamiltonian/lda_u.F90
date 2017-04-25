b399432683af7d26486d9f56bd5ef57ee71e6d13
Nicolas Tancogne-Dejean <tnico@draco02.mpcdf.mpg.de>
2017-04-25 14:48:24 +0200

    The Coulomb orbitals are not recomputed for the moment, assuming that the basis only moves with the atoms. This is not valid in the bounding-antibounding case.

--- src/hamiltonian/lda_u.F90
+++ src/hamiltonian/lda_u.F90
@@ -431,15 +431,15 @@ contains
     call zconstruct_orbital_basis(this, geo, gr%mesh, st)
   end if 
 
-  if(this%useACBN0) then
-    write(message(1),'(a)')    'Computing the Coulomb integrals localized orbital basis.'
-    call messages_info(1)
-    if (states_are_real(st)) then
-      call dcompute_coulomb_integrals(this, gr%mesh, gr%der, st)
-    else
-      call zcompute_coulomb_integrals(this, gr%mesh, gr%der, st)
-    end if
-  end if
+  !if(this%useACBN0) then
+  !  write(message(1),'(a)')    'Computing the Coulomb integrals localized orbital basis.'
+  !  call messages_info(1)
+  !  if (states_are_real(st)) then
+  !    call dcompute_coulomb_integrals(this, gr%mesh, gr%der, st)
+  !  else
+  !    call zcompute_coulomb_integrals(this, gr%mesh, gr%der, st)
+  !  end if
+  !end if
 
 
   call messages_print_stress(stdout)
