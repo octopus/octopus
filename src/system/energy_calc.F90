af5188aa95c6d25e5f527d32e605f7f78d8dcbe5
Nicolas Tancogne-Dejean <tnico@draco02.mpcdf.mpg.de>
2017-06-07 06:18:54 +0200

    Add a missing blank space.

--- src/system/energy_calc.F90
+++ src/system/energy_calc.F90
@@ -263,7 +263,7 @@ contains
       end if  
       if(hm%lda_u%apply) then
         write(message(1), '(6x,a, f18.8)')'Hubbard     = ', units_from_atomic(units_out%energy, hm%energy%dft_u)
-        write(message(2), '(6x,a, f18.8)')'Int[n*v_U] = ', units_from_atomic(units_out%energy, hm%energy%int_dft_u)
+        write(message(2), '(6x,a, f18.8)')'Int[n*v_U]  = ', units_from_atomic(units_out%energy, hm%energy%int_dft_u)
         if(cmplxscl) write(message(2), '(a, es18.6)') trim(message(2)),&
                      units_from_atomic(units_out%energy, hm%energy%Imint_dft_u)
         call messages_info(2, iunit)
