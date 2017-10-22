5c7f9e8c142f93a35d7115086b0179415efcb42c
Nicolas Tancogne-Dejean <tnico@eos01.bc.rzg.mpg.de>
2017-10-22 18:13:44 +0200

    The use of kpoint symmetries is now marked as experimental for all the calculation mode except the GS and UNOCC.

--- src/main/run.F90
+++ src/main/run.F90
@@ -224,8 +224,12 @@ contains
       case(CM_UNOCC)
         call unocc_run(sys, hm, fromScratch)
       case(CM_TD)
+        if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = td")
         call td_run(sys, hm, fromScratch)
       case(CM_LR_POL)
+        if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = em_resp")
         select case(get_resp_method())
         case(FD)
           call static_pol_run(sys, hm, fromScratch)
@@ -233,10 +237,16 @@ contains
           call em_resp_run(sys, hm, fromScratch)
         end select
       case(CM_VDW)
+         if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = vdw")
         call vdW_run(sys, hm, fromScratch)
       case(CM_GEOM_OPT)
+        if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = go")
         call geom_opt_run(sys, hm, fromScratch)
       case(CM_PHONONS_LR)
+        if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = vib_modes")
         select case(get_resp_method())
         case(FD)
           call phonons_run(sys, hm)
@@ -244,16 +254,24 @@ contains
           call phonons_lr_run(sys, hm, fromscratch)
         end select
       case(CM_OPT_CONTROL)
+        if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = opt_control")
         call opt_control_run(sys, hm)
       case(CM_CASIDA)
+        if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = casida")
         call casida_run(sys, hm, fromScratch)
       case(CM_ONE_SHOT)
         message(1) = "CalculationMode = one_shot is obsolete. Please use gs with MaximumIter = 0."
         call messages_fatal(1)
       case(CM_KDOTP)
+        if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = kdotp")
         call kdotp_lr_run(sys, hm, fromScratch)
       case(CM_DUMMY)
       case(CM_INVERTKDS)
+        if(gr%sb%kpoints%use_symmetries) &
+          call messages_experimental("KPoints symmetries with CalculationMode = invert_ks")
         call invert_ks_run(sys, hm)
       case(CM_PULPO_A_FEIRA)
         ASSERT(.false.) !this is handled before, if we get here, it is an error
