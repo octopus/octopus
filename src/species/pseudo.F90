bde2f4e4e69e51629b3305c7bb6e5decdadf6b1a
Xavier Andrade <xavier@tddft.org>
2018-03-07 10:57:13 -0800

    Copyright notices for the new files.

--- src/species/pseudo.F90
+++ src/species/pseudo.F90
@@ -1,8 +1,8 @@
 !! Copyright (C) 2018 X. Andrade
 !!
 !! This program is free software; you can redistribute it and/or modify
-!! it under the terms of the GNU General Public License as published by
-!! the Free Software Foundation; either version 2, or (at your option)
+!! it under the terms of the Lesser GNU General Public License as published by
+!! the Free Software Foundation; either version 3, or (at your option)
 !! any later version.
 !!
 !! This program is distributed in the hope that it will be useful,
