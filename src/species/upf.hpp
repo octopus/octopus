b40076618401ff276c7d0e0d4f000979da178dc5
Xavier Andrade <xavier@tddft.org>
2018-03-15 22:47:10 -0700

    Parse some functionals for UPF files.

--- src/species/upf.hpp
+++ src/species/upf.hpp
@@ -142,6 +142,23 @@ namespace pseudopotential {
       return std::max(-1, ll); 
     }
 
+    pseudopotential::exchange exchange() const {
+      std::string functional = root_node_->first_node("PP_HEADER")->first_attribute("functional")->value();
+      if(functional == "PBE") return pseudopotential::exchange::PBE;
+      if(functional == "PBESOL") return pseudopotential::exchange::PBE;
+      if(functional == "SLA  PW   NOGX NOGC") return pseudopotential::exchange::LDA;
+      return pseudopotential::exchange::UNKNOWN;
+    }
+
+    pseudopotential::correlation correlation() const {
+      std::string functional = root_node_->first_node("PP_HEADER")->first_attribute("functional")->value();
+      if(functional == "PBE") return pseudopotential::correlation::PBE;
+      if(functional == "PBESOL") return pseudopotential::correlation::PBE;
+      if(functional == "SLA  PW   NOGX NOGC") return pseudopotential::correlation::LDA_PW;
+      return pseudopotential::correlation::UNKNOWN;
+    }
+
+    
     int nquad() const {
       return 0;
     }
