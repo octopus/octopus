12d89ac610cb3c8ccfcbb997178363a450cd49b7
NicolasTD <nicolas.tancogne-dejean@mpsd.mpg.de>
2018-04-01 23:03:56 +0200

    Unused variable.

--- src/hamiltonian/projector_inc.F90
+++ src/hamiltonian/projector_inc.F90
@@ -64,7 +64,7 @@ subroutine X(project_psi_batch)(mesh, pj, npj, dim, psib, ppsib, ik)
   type(batch_t),     intent(inout) :: ppsib
   integer,           intent(in)    :: ik
 
-  integer :: ipj, nreduce, ii, jj, ns, idim, ll, mm, is, ist, bind
+  integer :: ipj, nreduce, ii, ns, idim, ll, mm, is, ist, bind
   R_TYPE, allocatable :: reduce_buffer(:,:), lpsi(:, :), uvpsi(:,:,:)
   integer, allocatable :: ireduce(:, :, :, :)
   type(profile_t), save :: prof
