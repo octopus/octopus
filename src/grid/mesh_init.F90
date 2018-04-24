f60b4aba2d037c20e1c72f20fd55db54d6e7c8d7
Nicolas Tancogne-Dejean <tnico@draco01.mpcdf.mpg.de>
2018-04-24 13:34:22 +0200

    The routine curvilinear_det_Jac already takes care of giving the correct volume element.

--- src/grid/mesh_init.F90
+++ src/grid/mesh_init.F90
@@ -1008,11 +1008,7 @@ contains
 
     SAFE_ALLOCATE(mesh%vol_pp(1:np))
 
-    if(.not.sb%nonorthogonal) then
-      forall(ip = 1:np) mesh%vol_pp(ip) = product(mesh%spacing(1:sb%dim))
-    else
-      forall(ip = 1:np) mesh%vol_pp(ip) = sb%volume_element
-    end if
+    forall(ip = 1:np) mesh%vol_pp(ip) = product(mesh%spacing(1:sb%dim))
 
     jj(sb%dim + 1:MAX_DIM) = 0
 
@@ -1073,11 +1069,7 @@ contains
         ! volumes are initialized even for the intermediate points
         nr(:,:) = mesh%idx%nr(:,:)
         SAFE_ALLOCATE(vol_tmp(nr(1,1):nr(2,1),nr(1,2):nr(2,2),nr(1,3):nr(2,3)))
-        if(.not.sb%nonorthogonal) then
-          vol_tmp(:,:,:) = product(mesh%spacing(1:sb%dim))
-        else
-          vol_tmp(:,:,:) = sb%volume_element
-        end if
+        vol_tmp(:,:,:) = product(mesh%spacing(1:sb%dim))
 
         ! The idea is that in the first i_lev loop we find intermediate
         ! points that are odd, i.e. at least one of their indices cannot
