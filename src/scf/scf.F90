078bd25daa9ee265c10d907779ff47debe3df3e1
Nicolas Tancogne-Dejean <tnico@draco02.mpcdf.mpg.de>
2018-04-24 09:33:34 +0200

    Removing the file band-gp.dat and bands-e_fermi.dat which were not used, and became useless with the possibility of properly computing bandstructures.

--- src/scf/scf.F90
+++ src/scf/scf.F90
@@ -1010,11 +1010,10 @@ contains
       call output_all(outp, gr, geo, st, hm, ks, STATIC_DIR)
     end if
 
-    if(simul_box_is_periodic(gr%sb) .and. st%d%nik > st%d%nspin) &
-      call states_write_bands(STATIC_DIR, st%nst, st, gr%sb)
+    if(simul_box_is_periodic(gr%sb) .and. st%d%nik > st%d%nspin) then
       if(iand(gr%sb%kpoints%method, KPOINTS_PATH) /= 0) &
         call states_write_bandstructure(STATIC_DIR, st%nst, st, gr%sb)
-      
+    end if
 
     POP_SUB(scf_run)
 
