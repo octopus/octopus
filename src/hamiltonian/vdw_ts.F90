dbb59e32dcc60399c422700c79e797e67102cb24
Le Breton Guillaume <guillaume.le-breton@ens-lyon.fr>
2018-05-14 12:50:35 +0200

    force try 1

--- src/hamiltonian/vdw_ts.F90
+++ src/hamiltonian/vdw_ts.F90
@@ -295,12 +295,23 @@ contains
     end if
       
     ! Add the extra term for the force 
+!    do iatom = 1, geo%natoms
+!      do jatom = 1, geo%natoms
+!        call hirshfeld_position_derivative(hirshfeld, der, iatom, jatom, density, dvadrr)
+!        force(1:sb%dim, iatom) = force(1:sb%dim, iatom) - derivative_coeff(iatom)*dvadrr(1:sb%dim)
+!      end do
+!    end do
+
+
     do iatom = 1, geo%natoms
-      do jatom = 1, geo%natoms
+      do jatom = 1, iatom
         call hirshfeld_position_derivative(hirshfeld, der, iatom, jatom, density, dvadrr)
         force(1:sb%dim, iatom) = force(1:sb%dim, iatom) - derivative_coeff(iatom)*dvadrr(1:sb%dim)
+        force(1:sb%dim, jatom) = force(1:sb%dim, jatom) + derivative_coeff(iatom)*dvadrr(1:sb%dim)
       end do
     end do
+
+
 
     ! Calculate the potential
     potential = M_ZERO
