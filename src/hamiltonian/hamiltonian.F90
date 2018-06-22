a44e5b80dc376069baf22f512dc6ce07c6080346
Xavier Iago Andrade-valencia <andradevalen1@llnl.gov>
2018-06-22 16:24:13 -0700

    Continuation lines in an if are ugly.

--- src/hamiltonian/hamiltonian.F90
+++ src/hamiltonian/hamiltonian.F90
@@ -485,8 +485,7 @@ contains
     kick_present = hm%ep%kick%delta_strength /= M_ZERO
 
     call pcm_init(hm%pcm, geo, gr, st%qtot, st%val_charge, external_potentials_present, kick_present )  !< initializes PCM  
-    if(hm%pcm%run_pcm .and. hm%theory_level /= KOHN_SHAM_DFT) &
-      call messages_not_implemented("PCM for TheoryLevel /= DFT")
+    if(hm%pcm%run_pcm .and. hm%theory_level /= KOHN_SHAM_DFT) call messages_not_implemented("PCM for TheoryLevel /= DFT")
     
     !%Variable SCDM_EXX
     !%Type logical
