1ed7b6d964d4cf70ac27c2fb527565320af49a58
Xavier Andrade <xavier@tddft.org>
2018-07-06 15:03:39 -0700

    Code cleaning.

--- src/grid/restart.F90
+++ src/grid/restart.F90
@@ -622,8 +622,7 @@ contains
     ! as there might be some subsequent calls to this function where that information will
     ! be written/read to/from the same directory.
 #if defined(HAVE_MPI)
-    if(restart%mpi_grp%size > 1) &
-      call MPI_Barrier(restart%mpi_grp%comm, mpi_err)
+    if(restart%mpi_grp%size > 1) call MPI_Barrier(restart%mpi_grp%comm, mpi_err)
 #endif
 
     POP_SUB(restart_init)
