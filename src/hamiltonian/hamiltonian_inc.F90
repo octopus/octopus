84778c171febf31862fbff2ad2169d59d36726de
Nicolas Tancogne-Dejean <tnico@draco02.mpcdf.mpg.de>
2018-08-14 14:21:36 +0200

    There is no need to call unpack_copy here, as we do not use the phase anymore.

--- src/hamiltonian/hamiltonian_inc.F90
+++ src/hamiltonian/hamiltonian_inc.F90
@@ -170,7 +170,7 @@ subroutine X(hamiltonian_apply_batch) (hm, der, psib, hpsib, ik, terms, set_bc, 
 
   if(apply_phase .and. set_phase_) then
     call X(hamiltonian_base_phase)(hm%hm_base, der, der%mesh%np, ik, .true., hpsib)
-    call batch_end(epsib)
+    call batch_end(epsib, copy = .false.)
     SAFE_DEALLOCATE_P(epsib)
   end if
 
