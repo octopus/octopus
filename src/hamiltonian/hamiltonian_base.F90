ac9ef1915e084c110545fc4a25585f8c94e49a8b
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2018-08-17 17:30:31 +0200

    remove definitions of phase correction functions

--- src/hamiltonian/hamiltonian_base.F90
+++ src/hamiltonian/hamiltonian_base.F90
@@ -83,8 +83,6 @@ module hamiltonian_base_oct_m
     hamiltonian_base_update,                   &
     dhamiltonian_base_phase,                   &
     zhamiltonian_base_phase,                   &
-    dhamiltonian_base_phase_correction,        &
-    zhamiltonian_base_phase_correction,        &
     dhamiltonian_base_nlocal_force,            &
     zhamiltonian_base_nlocal_force,            &
     projection_t
