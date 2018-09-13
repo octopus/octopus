b32b0d996f6e79acace004749de4e436bd912dad
Xavier Andrade <xavier@tddft.org>
2018-09-12 22:56:29 -0700

    Initial implementation of the double sum.

--- src/sternheimer/kubo_greenwood.F90
+++ src/sternheimer/kubo_greenwood.F90
@@ -19,9 +19,13 @@
 #include "global.h"
 
 module kubo_greenwood_oct_m
+  use boundaries_oct_m
+  use derivatives_oct_m
   use global_oct_m
   use hamiltonian_oct_m
   use messages_oct_m
+  use mesh_oct_m
+  use mesh_function_oct_m
   use restart_oct_m
   use states_oct_m
   use states_restart_oct_m
@@ -42,6 +46,11 @@ contains
 
     type(restart_t) :: gs_restart
     integer :: ierr
+    integer :: ist, jst, iqn, idim, idir, jdir
+    CMPLX, allocatable :: psii(:, :), psij(:, :), gpsii(:, :, :), gpsij(:, :, :)
+    CMPLX, allocatable :: tensor(:, :)
+    CMPLX :: prod
+    type(mesh_t), pointer :: mesh
     
     PUSH_SUB(kubo_greewood_run)
 
@@ -58,6 +67,69 @@ contains
       call messages_write("Cannot find occupied and unoccupied states, a previous ground state calculation is required.")
       call messages_fatal()
     end if
+
+    ASSERT(.not. sys%st%parallel_in_states)    
+
+    mesh => sys%gr%mesh
+    
+    SAFE_ALLOCATE(psii(1:mesh%np_part, 1:sys%st%d%dim))
+    SAFE_ALLOCATE(psij(1:mesh%np_part, 1:sys%st%d%dim))
+    SAFE_ALLOCATE(gpsii(1:mesh%np, 1:sys%gr%sb%dim, 1:sys%st%d%dim))
+    SAFE_ALLOCATE(gpsij(1:mesh%np, 1:sys%gr%sb%dim, 1:sys%st%d%dim))
+
+    SAFE_ALLOCATE(tensor(1:mesh%sb%dim, 1:mesh%sb%dim))
+
+    tensor = CNST(0.0)
+    
+    do iqn = sys%st%d%kpt%start, sys%st%d%kpt%end
+    
+      do ist = 1, sys%st%nst
+        ! get the state i and calculate the gradient
+        call states_get_state(sys%st, mesh, ist, iqn, psii)
+        do idim = 1, sys%st%d%dim
+          call boundaries_set(sys%gr%der%boundaries, psii(:, idim))
+        end do
+        if(associated(hm%hm_base%phase)) then 
+          call states_set_phase(sys%st%d, psii, hm%hm_base%phase(:, iqn), mesh%np_part, .false.)
+        end if
+        do idim = 1, sys%st%d%dim
+          call zderivatives_grad(sys%gr%der, psii(:, idim), gpsii(:, :, idim), set_bc = .false.)
+        end do
+        
+        do jst = 1, sys%st%nst
+          ! get the state j and calculate the gradient
+          call states_get_state(sys%st, mesh, ist, iqn, psij)
+          do idim = 1, sys%st%d%dim
+            call boundaries_set(sys%gr%der%boundaries, psij(:, idim))
+          end do
+          if(associated(hm%hm_base%phase)) then 
+            call states_set_phase(sys%st%d, psij, hm%hm_base%phase(:, iqn), mesh%np_part, .false.)
+          end if
+          do idim = 1, sys%st%d%dim
+            call zderivatives_grad(sys%gr%der, psij(:, idim), gpsij(:, :, idim), set_bc = .false.)
+          end do
+
+          do idir = 1, mesh%sb%dim
+            do jdir = 1, mesh%sb%dim
+
+              prod = zmf_dotp(mesh, sys%st%d%dim, psii, gpsij(:, idir, :))*zmf_dotp(mesh, sys%st%d%dim, psij, gpsii(:, jdir, :))
+              
+              tensor(idir, jdir) = tensor(idir, jdir) + CNST(2.0)*M_ZI/mesh%sb%rcell_volume* &
+                (sys%st%occ(ist, iqn) - sys%st%occ(jst, iqn))*prod
+
+            end do
+          end do
+          
+        end do
+      end do
+      
+    end do
+
+    SAFE_DEALLOCATE_A(tensor)
+    SAFE_DEALLOCATE_A(psii)
+    SAFE_DEALLOCATE_A(psij)
+    SAFE_DEALLOCATE_A(gpsii)
+    SAFE_DEALLOCATE_A(gpsij)    
     
     POP_SUB(kubo_greewood_run)
   end subroutine kubo_greenwood_run
