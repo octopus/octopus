17649b2121edbfae0566dc88d9760b50890dad66
Nicolas Tancogne-Dejean <tnico@draco02.mpcdf.mpg.de>
2018-09-20 08:17:20 +0200

    Using M_z0 instead of cmplx(M_ZER0,M_ZER0), to improve code readability.

--- src/hamiltonian/dft_u_noncollinear_inc.F90
+++ src/hamiltonian/dft_u_noncollinear_inc.F90
@@ -147,10 +147,10 @@ subroutine compute_ACBNO_U_noncollinear(this, ios)
   PUSH_SUB(compute_ACBNO_U_noncollinear)
 
   norbs = this%orbsets(ios)%norbs
-  numU = cmplx(M_ZERO,M_ZERO)
-  numJ = cmplx(M_ZERO,M_ZERO)
-  denomU = cmplx(M_ZERO,M_ZERO)
-  denomJ = cmplx(M_ZERO,M_ZERO)
+  numU = M_z0
+  numJ = M_z0
+  denomU = M_z0
+  denomJ = M_z0
 
   if(norbs > 1) then
 
@@ -161,8 +161,8 @@ subroutine compute_ACBNO_U_noncollinear(this, ios)
         ! We first compute the terms
         ! sum_{alpha,beta} P^alpha_{mmp}P^beta_{mpp,mppp}  
         ! sum_{alpha} P^alpha_{mmp}P^alpha_{mpp,mppp}
-        tmpU = cmplx(M_ZERO,M_ZERO)
-        tmpJ = cmplx(M_ZERO,M_ZERO)
+        tmpU = M_z0
+        tmpJ = M_z0
 
         do ispin1 = 1, this%spin_channels
           do ispin2 = 1, this%spin_channels
@@ -184,8 +184,8 @@ subroutine compute_ACBNO_U_noncollinear(this, ios)
 
       ! We compute the term
       ! sum_{alpha} sum_{m,mp/=m} N^alpha_{m}N^alpha_{mp}
-      tmpJ = cmplx(M_ZERO,M_ZERO)
-      tmpU = cmplx(M_ZERO,M_ZERO)
+      tmpJ = M_z0
+      tmpU = M_z0
       if(imp/=im) then
         do ispin1 = 1, this%spin_channels
           tmpJ = tmpJ + this%zn(im,im,ispin1,ios)*this%zn(imp,imp,ispin1,ios)
@@ -220,8 +220,8 @@ subroutine compute_ACBNO_U_noncollinear(this, ios)
   else !In the case of s orbitals, the expression is different
     ! sum_{alpha/=beta} P^alpha_{mmp}P^beta_{mpp,mppp}  
     ! sum_{alpha,beta} sum_{m,mp} N^alpha_{m}N^beta_{mp}
-    numU = cmplx(M_ZERO,M_ZERO)
-    denomU = cmplx(M_ZERO,M_ZERO)
+    numU = M_z0
+    denomU = M_z0
     do ispin1 = 1, this%spin_channels
       do ispin2 = 1, this%spin_channels
         if(ispin1 /= ispin2) then
