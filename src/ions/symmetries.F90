9d520a20006e577c4ee4313f133a5dae3df23821
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2019-03-19 15:00:52 +1100

    Removed unused symmetries_nullify subroutine.

--- src/ions/symmetries.F90
+++ src/ions/symmetries.F90
@@ -38,7 +38,6 @@ module symmetries_oct_m
   
   public ::                        &
     symmetries_t,                  &
-    symmetries_nullify,            &
     symmetries_init,               &
     symmetries_copy,               &
     symmetries_end,                &
@@ -89,15 +88,6 @@ module symmetries_oct_m
   end interface
 
 contains
-  
-  elemental subroutine symmetries_nullify(this)
-    type(symmetries_t), intent(out) :: this
-
-    this%nops = 0
-    this%breakdir = M_ZERO
-    this%space_group = 0
-
-  end subroutine symmetries_nullify
 
   subroutine symmetries_init(this, geo, dim, periodic_dim, rlattice, klattice)
     type(symmetries_t),  intent(out) :: this
