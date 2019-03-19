7ecd20ddb1e0e706155eb8dcff47a24c9b676479
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2019-03-20 09:39:17 +1100

    Removed unused states_orthogonalize_cproduct and reorder_states_by_args subroutines.

--- src/states/states_calc.F90
+++ src/states/states_calc.F90
@@ -90,7 +90,6 @@ module states_calc_oct_m
     zstates_matrix,                 &
     dstates_calc_overlap,           &
     zstates_calc_overlap,           &
-    states_orthogonalize_cproduct,  &
     dstates_calc_projections,       &
     zstates_calc_projections,       &
     dstates_me_one_body,            &
@@ -124,97 +123,6 @@ contains
 
     POP_SUB(states_orthogonalize)
   end subroutine states_orthogonalize
-
-  ! ---------------------------------------------------------
-
-  subroutine states_orthogonalize_cproduct(st, mesh)
-    type(states_t),    intent(inout) :: st
-    type(mesh_t),      intent(in)    :: mesh
-
-    integer            :: ik,ist
-    CMPLX              :: cnorm
-    CMPLX, allocatable :: psi(:,:)
-
-    PUSH_SUB(states_orthogonalize_cproduct)
-    ASSERT(st%d%dim == 1)
-    SAFE_ALLOCATE(psi(1:mesh%np_part, 1))
-   
-    do ik = st%d%kpt%start, st%d%kpt%end
-      do ist = 1, st%nst
-        call states_get_state(st, mesh, ist, ik, psi)
-
-!         ! Orthogonalize eigenstates according to cproduct - this implies st%cmplxscl = .true. 
-!         if(ist > 1) then
-!            call zstates_orthogonalize_single(st, mesh, ist - 1, ik, psi, normalize = .true.,  norm = cnorm)
-!         else
-!         ! Normalize the first eigenstate  
-        cnorm = sqrt(zmf_dotp(mesh, 1, psi, psi, dotu = .true.))
-!           cnorm = sqrt(zmf_integrate(mesh, psi(:,1)**2))
-!         end if    
-
-        psi = psi /cnorm
-        call states_set_state(st, mesh, ist, ik, psi)
-        
-      end do
-    end do
-    SAFE_DEALLOCATE_A(psi)
-
-    POP_SUB(states_orthogonalize_cproduct)
-  end subroutine states_orthogonalize_cproduct
-
-  ! ---------------------------------------------------------
-  !> Reorder the states in st so that the order corresponds to
-  !! the indices given in args (args could come from an argsort)
-  subroutine reorder_states_by_args(st, mesh, args, ik)
-
-    type(states_t), intent(inout) :: st
-    type(mesh_t),   intent(in)    :: mesh
-    integer,        intent(in)    :: args(:)
-    integer,        intent(in)    :: ik
-
-    integer :: ist, jst, kst
-    CMPLX,   allocatable :: buf(:,:),buf1(:,:)
-    logical, allocatable :: ok(:)
-    integer, allocatable :: rank(:)
-    
-    PUSH_SUB(reorder_states_by_args)
-
-    SAFE_ALLOCATE(ok(1:st%nst))
-    SAFE_ALLOCATE(rank(1:st%nst))
-    SAFE_ALLOCATE(buf(1:mesh%np_part,1:st%d%dim))
-    SAFE_ALLOCATE(buf1(1:mesh%np_part,1:st%d%dim))
-
-    do ist = 1, st%nst
-      ok(ist) = .false.
-      rank(args(ist)) = ist
-    end do
-
-    do ist = 1, st%nst
-      if ((args(ist) /= ist).and.(.not.(ok(ist)))) then
-        call states_get_state(st, mesh, ist, ik, buf)
-        kst = ist
-        do
-          jst = args(kst)
-          if (jst == ist) then
-            call states_set_state(st, mesh, rank(jst), ik, buf)
-            ok(rank(jst)) = .true.
-            exit
-          end if
-          call states_get_state(st, mesh, jst, ik, buf1)
-          call states_set_state(st, mesh, kst, ik, buf1)             
-          ok(kst) = .true.
-          kst = jst
-        end do
-      end if
-    end do
-
-    SAFE_DEALLOCATE_A(ok)
-    SAFE_DEALLOCATE_A(rank)
-    SAFE_DEALLOCATE_A(buf)
-    SAFE_DEALLOCATE_A(buf1)
-    
-    POP_SUB(reorder_states_by_args)
-  end subroutine reorder_states_by_args
 
 
   ! -------------------------------------------------------
