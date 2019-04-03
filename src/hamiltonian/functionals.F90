1a74da39fe159b7735d2d83af05f450e0c3a22b4
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-04-03 12:22:00 +0200

    disable experimental status for libvdwxc

--- src/hamiltonian/functionals.F90
+++ src/hamiltonian/functionals.F90
@@ -148,7 +148,6 @@ contains
           call messages_experimental("complex-scaled LB94 exchange and correlation")
           functl%family = XC_FAMILY_GGA
         else if(functl%id == XC_VDW_C_VDWDF .or. functl%id == XC_VDW_C_VDWDF2 .or. functl%id == XC_VDW_C_VDWDFCX) then
-          call messages_experimental("van der Waals functionals from libvdwxc")
           functl%family = XC_FAMILY_LIBVDWXC
           !functl%flags = functl%flags + XC_FLAGS_HAVE_VXC + XC_FLAGS_HAVE_EXC
         else if (functl%id == XC_RDMFT_XC_M) then
