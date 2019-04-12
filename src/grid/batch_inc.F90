11d1b04c973620dfe08e92170466d082c57e549e
Xavier Andrade <xavier@tddft.org>
2019-04-12 01:18:57 -0700

    Fill_zeros is now .false. by default.

--- src/grid/batch_inc.F90
+++ src/grid/batch_inc.F90
@@ -99,15 +99,15 @@ subroutine X(batch_allocate)(this, st_start, st_end, np, fill_zeros, mirror)
   integer,        intent(in)    :: st_start
   integer,        intent(in)    :: st_end
   integer,        intent(in)    :: np
-  logical, optional, intent(in) :: fill_zeros
-  logical, optional, intent(in) :: mirror
+  logical, optional, intent(in) :: fill_zeros !< If .true., the batch is set to zero. Default: .false.
+  logical, optional, intent(in) :: mirror     !< If .true., this batch will keep a copy when packed. Default: .false.
 
   integer :: ist
 
   PUSH_SUB(X(batch_allocate))
 
   SAFE_ALLOCATE(this%X(psicont)(1:np, 1:this%dim, 1:st_end - st_start + 1))
-  if (optional_default(fill_zeros, .true.)) this%X(psicont) = R_TOTYPE(M_ZERO)
+  if (optional_default(fill_zeros, .false.)) this%X(psicont) = R_TOTYPE(M_ZERO)
 
   this%is_allocated = .true.
   this%mirror = optional_default(mirror, .false.)  
