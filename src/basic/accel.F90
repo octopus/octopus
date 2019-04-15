164844cc6f2acb980c63bb4bde9fc8bfe7d67b9e
Xavier Andrade <xavier@tddft.org>
2019-04-14 21:03:46 -0700

    Improved the cache information output.

--- src/basic/accel.F90
+++ src/basic/accel.F90
@@ -777,19 +777,24 @@ contains
 
     if(accel_is_enabled()) then
       call alloc_cache_end(memcache, hits, misses, volume_hits, volume_misses)
-      
-      call messages_write('Acceleration device allocation cache:', new_line = .true.)
-      call messages_write('   Number of allocations    =')
+
+      call messages_print_stress(stdout, "Acceleration-device allocation cache")
+
+      call messages_new_line()
+      call messages_write('    Number of allocations    =')
       call messages_write(hits + misses, new_line = .true.)
-      call messages_write('   Volume of allocations    =')
+      call messages_write('    Volume of allocations    =')
       call messages_write(volume_hits + volume_misses, fmt = 'f18.1', units = unit_gigabytes, align_left = .true., new_line = .true.)
-      call messages_write('   Hit ratio                =')
+      call messages_write('    Hit ratio                =')
       call messages_write(hits/dble(hits + misses)*100, fmt='(f5.1)')
       call messages_write('%', new_line = .true.)
-      call messages_write('   Volume hit ratio         =')
+      call messages_write('    Volume hit ratio         =')
       call messages_write(volume_hits/(volume_hits + volume_misses)*100, fmt='(f5.1)')
       call messages_write('%')
+      call messages_new_line()
       call messages_info()
+
+      call messages_print_stress(stdout)
     end if
     
     call accel_kernel_global_end()
