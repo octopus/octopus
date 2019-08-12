e25f237082e48937d62a578f6126e7c251e5af5e
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2019-08-12 09:13:36 +0200

    Make methods of basis_set base class public
    Otherwise they are in principle not allowed to be public in derived
    classes. The newest Intel 19 compiler also complains about this and
    won't compile it otherwise.

--- src/grid/basis_set_abst.F90
+++ src/grid/basis_set_abst.F90
@@ -39,13 +39,13 @@ module basis_set_abst_oct_m
     logical :: time_dependent   !< flag for time-dependent basis sets
   contains
     private
-    procedure(init),          deferred :: init
-    procedure(end),           deferred :: end
-    procedure(write_info),    deferred :: write_info
-    procedure(dump),          deferred :: dump
-    procedure(load),          deferred :: load
-    procedure, non_overridable, public :: is_time_dependent
-    procedure, non_overridable, public :: set_time_dependent
+    procedure(init),       public, deferred        :: init
+    procedure(end),        public, deferred        :: end
+    procedure(write_info), public, deferred        :: write_info
+    procedure(dump),       public, deferred        :: dump
+    procedure(load),       public, deferred        :: load
+    procedure,             public, non_overridable :: is_time_dependent
+    procedure,             public, non_overridable :: set_time_dependent
   end type basis_set_abst_t
 
   abstract interface
