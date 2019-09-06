e2e1279d9fc38c1a4bf06cc5136a8a859093c130
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2019-09-06 15:31:59 +0200

    Move accel_init out of system to run, before fft_init
    This is needed to make the GPU FFTs work - the accel part needs to be
    initialized before the FFT part. Moreover, accel is a global module and
    does not depend on system, so probably it's best to be initialized
    globally.
    
    This also fixes the multisystem test on the GPU.

--- src/system/system.F90
+++ src/system/system.F90
@@ -85,8 +85,6 @@ contains
 
     sys%namespace = namespace
 
-    call accel_init(mpi_world, sys%namespace)
-
     call messages_obsolete_variable(sys%namespace, 'SystemName')
 
     call space_init(sys%space, sys%namespace)
@@ -171,8 +169,6 @@ contains
     call grid_end(sys%gr)
 
     call space_end(sys%space)
-
-    call accel_end()
 
     SAFE_DEALLOCATE_P(sys%gr)
 
