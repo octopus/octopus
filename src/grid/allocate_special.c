5f4cf9cdeebd3cbe29d1849ba8d55e62e655aa2d
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2019-10-10 16:53:27 +0200

    Fix missing brace in debug code

--- src/grid/allocate_special.c
+++ src/grid/allocate_special.c
@@ -30,6 +30,7 @@ cudaError_t checkCuda(cudaError_t result)
   if (result != cudaSuccess) {
     fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
     assert(result == cudaSuccess);
+  }
 #endif
   return result;
 }
