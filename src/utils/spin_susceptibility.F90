0065d12e2a6c61759cb93b45a767969188fce343
nicolastd <nicolas.tancogne-dejean@mpsd.mpg.de>
2019-11-06 14:22:08 +0100

    The copyright was not correct.

--- src/utils/spin_susceptibility.F90
+++ src/utils/spin_susceptibility.F90
@@ -1,4 +1,4 @@
-!! Copyright (C) 2008 X. Andrade
+!! Copyright (C) 2018 N. Tancogne-Dejean
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
