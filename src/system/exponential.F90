751ff100c36e90b732fd06e52ffd3b550134fd91
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2019-11-11 17:13:43 +1100

    Update copyright.

--- src/system/exponential.F90
+++ src/system/exponential.F90
@@ -1,4 +1,5 @@
 !! Copyright (C) 2002-2006 M. Marques, A. Castro, A. Rubio, G. Bertsch
+!! Copyright (C) 2019 M. Oliveira
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
