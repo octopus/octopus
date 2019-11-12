aa133b62559b9bff46e4466b1e54b27b606dbcc4
Jannis Krumland <krumlanj@physik.hu-berlin.de>
2019-11-12 14:47:19 +0100

    Fake change (added a space).

--- src/hamiltonian/pcm.F90
+++ src/hamiltonian/pcm.F90
@@ -1,5 +1,5 @@
 !! Copyright (C) 2014 Alain Delgado Gran, Carlo Andrea Rozzi, Stefano Corni, Gabriel Gil
-!!
+!! 
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
 !! the Free Software Foundation; either version 2, or (at your option)
