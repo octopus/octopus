3f63847e5ffe1fc4411ca8db177308a711855275
Franco Bonafe <franco.bonafe@mpsd.mpg.de>
2019-11-18 17:57:46 +0100

    Fixed bug and now it compiles

--- src/system/current.F90
+++ src/system/current.F90
@@ -1,4 +1,4 @@
-7!! Copyright (C) 2008 X. Andrade
+!! Copyright (C) 2008 X. Andrade
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
@@ -52,7 +52,8 @@ module current_oct_m
   use states_mxll_oct_m
   use unit_oct_m
   use unit_system_oct_m  
-
+  use string_oct_m
+  
   implicit none
 
   private
