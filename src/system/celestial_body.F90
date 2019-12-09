98b2b4e3403e043c1fb6ee1fb44a39a858cd2599
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2019-12-09 16:12:49 +0100

    Celestial body's properties are now parsed from the input file.

--- src/system/celestial_body.F90
+++ src/system/celestial_body.F90
@@ -21,8 +21,11 @@
 module celestial_body_oct_m
   use global_oct_m
   use messages_oct_m
+  use namespace_oct_m
+  use parser_oct_m
   use profiling_oct_m
   use propagator_abst_oct_m
+  use space_oct_m
   use system_abst_oct_m
 
   implicit none
@@ -34,14 +37,14 @@ module celestial_body_oct_m
   type, extends(system_abst_t) :: celestial_body_t
     private
 
-    FLOAT, public :: mass
+    FLOAT :: mass
     FLOAT, public :: pos(1:MAX_DIM)
     FLOAT, public :: vel(1:MAX_DIM)
     FLOAT, public :: acc(1:MAX_DIM)
     FLOAT, public :: tot_force(1:MAX_DIM)
 
     FLOAT, allocatable :: forces(:,:)
-    integer :: ndim
+    type(space_t) :: space
 
     class(propagator_abst_t), pointer :: prop
 
@@ -56,15 +59,62 @@ module celestial_body_oct_m
 
 contains
 
-  subroutine celestial_body_init(sys)
+  subroutine celestial_body_init(sys, namespace)
     class(celestial_body_t),  intent(inout) :: sys
+    type(namespace_t),        intent(in)    :: namespace
+
+    integer :: n_rows, idir
+    type(block_t) :: blk
 
     PUSH_SUB(celestial_body_init)
 
-    sys%ndim = 2
+    call space_init(sys%space, namespace)
+
+    !%Variable CelestialBodyMass
+    !%Type float
+    !%Section CelestialDynamics
+    !%Description
+    !% Mass of celestial body.
+    !%End
+    call parse_variable(namespace, 'CelestialBodyMass', M_ONE, sys%mass)
+
+    !%Variable CelestialBodyInitialPosition
+    !%Type block
+    !%Section CelestialDynamics
+    !%Description
+    !% Initial position of celestial body.
+    !%End
+    sys%pos = M_ZERO
+    if (parse_block(namespace, 'CelestialBodyInitialPosition', blk) == 0) then
+      n_rows = parse_block_n(blk)
+      if (n_rows > 1) call  messages_input_error('CelestialBodyInitialPosition')
+
+      do idir = 1, sys%space%dim
+        call parse_block_float(blk, 0, idir - 1, sys%pos(idir))
+      end do
+      call parse_block_end(blk)
+    end if
+
+    !%Variable CelestialBodyInitialVelocity
+    !%Type block
+    !%Section CelestialDynamics
+    !%Description
+    !% Initial velocity of celestial body.
+    !%End
+    sys%vel = M_ZERO
+    if (parse_block(namespace, 'CelestialBodyInitialVelocity', blk) == 0) then
+      n_rows = parse_block_n(blk)
+      if (n_rows > 1) call  messages_input_error('CelestialBodyInitialVelocity')
+      do idir = 1, sys%space%dim
+        call parse_block_float(blk, 0, idir - 1, sys%vel(idir))
+      end do
+      call parse_block_end(blk)
+    end if
+
+    sys%acc = M_ZERO
+    sys%tot_force = M_ZERO
 
     POP_SUB(celestial_body_init)
-
   end subroutine celestial_body_init
 
   ! ---------------------------------------------------------
@@ -76,7 +126,6 @@ contains
     celestial_body_needed_quantity = FORCE
 
     POP_SUB(celestial_body_needed_quantity)
-
   end function celestial_body_needed_quantity
 
   ! ---------------------------------------------------------
@@ -89,7 +138,6 @@ contains
     sys%prop => prop
 
     POP_SUB(celestial_body_set_prop)
-
   end subroutine celestial_body_set_prop
 
   ! ---------------------------------------------------------
@@ -98,10 +146,9 @@ contains
 
     PUSH_SUB(celestial_body_alloc_receiver)
 
-    SAFE_ALLOCATE(this%forces(1:this%ndim, 1:this%nb_partners)) 
+    SAFE_ALLOCATE(this%forces(1:this%space%dim, 1:this%nb_partners))
 
     POP_SUB(celestial_body_alloc_receiver)
-
   end subroutine celestial_body_alloc_receiver
 
 
@@ -122,25 +169,25 @@ contains
 
     case(VERLET_UPDATE_POS)
 
-      this%acc(1:this%ndim) = this%tot_force(1:this%ndim)
-      this%pos(1:this%ndim) = this%pos(1:this%ndim) + this%prop%dt * this%vel(1:this%ndim) &
-                                   + M_HALF * this%prop%dt**2 * this%tot_force(1:this%ndim)
+      this%acc(1:this%space%dim) = this%tot_force(1:this%space%dim)
+      this%pos(1:this%space%dim) = this%pos(1:this%space%dim) + this%prop%dt * this%vel(1:this%space%dim) &
+                                   + M_HALF * this%prop%dt**2 * this%tot_force(1:this%space%dim)
       call this%prop%list%next()
 
     case(VERLET_COMPUTE_ACC)
 
       !We sum the forces from the different partners
-      this%tot_force(1:this%ndim) = M_ZERO
+      this%tot_force(1:this%space%dim) = M_ZERO
       do ip = 1, this%nb_partners
-        this%tot_force(1:this%ndim) = this%tot_force(1:this%ndim) + this%forces(1:this%ndim, ip)
+        this%tot_force(1:this%space%dim) = this%tot_force(1:this%space%dim) + this%forces(1:this%space%dim, ip)
       end do
-      this%tot_force(1:this%ndim) = this%tot_force(1:this%ndim) / this%mass
+      this%tot_force(1:this%space%dim) = this%tot_force(1:this%space%dim) / this%mass
       call this%prop%list%next()
 
     case(VERLET_COMPUTE_VEL)
 
-      this%vel(1:this%ndim) = this%vel(1:this%ndim) + &
-         M_HALF * this%prop%dt * (this%acc(1:this%ndim) + this%tot_force(1:this%ndim))
+      this%vel(1:this%space%dim) = this%vel(1:this%space%dim) + &
+         M_HALF * this%prop%dt * (this%acc(1:this%space%dim) + this%tot_force(1:this%space%dim))
       call this%prop%list%next()
 
     case default
@@ -172,8 +219,8 @@ contains
       select type(remote)
       class is(celestial_body_t)
 
-        dist = sqrt(sum((remote%pos(1:sys%ndim)-sys%pos(1:sys%ndim))**2))
-        sys%forces(1:sys%ndim, partner_index) = (remote%pos(1:sys%ndim)-sys%pos(1:sys%ndim)) &
+        dist = sqrt(sum((remote%pos(1:sys%space%dim)-sys%pos(1:sys%space%dim))**2))
+        sys%forces(1:sys%space%dim, partner_index) = (remote%pos(1:sys%space%dim)-sys%pos(1:sys%space%dim)) &
                                                   * GG * sys%mass * remote%mass / dist**3
 
       class default
@@ -186,10 +233,8 @@ contains
       call messages_fatal(1)
     end select
 
-   POP_SUB(celestial_body_pull)
-
+    POP_SUB(celestial_body_pull)
   end subroutine celestial_body_pull
-
 
 end module celestial_body_oct_m
 
