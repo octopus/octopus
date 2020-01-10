113b1209b44ea6c241e7639ba3ea4140158d16f0
Nicolas Tancogne-Dejean <nicolas.tancogne-dejean@mpsd.mpg.de>
2020-01-10 10:21:22 +0100

    Fix the copyright.

--- src/hamiltonian/exchange_operator_inc.F90
+++ src/hamiltonian/exchange_operator_inc.F90
@@ -1,4 +1,4 @@
-!! Copyright (C) !! Copyright (C) 2002-2006 M. Marques, A. Castro, A. Rubio, G. Bertsch
+!! Copyright (C) 2002-2018 M. Marques, A. Castro, A. Rubio, G. Bertsch, N. Tancogne-Dejean
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
