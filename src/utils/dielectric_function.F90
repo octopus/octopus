6d271acfe1cf97e9f4715089974d689340269dca
Nicolas Tancogne-Dejean <nicolas.tancogne-dejean@mpsd.mpg.de>
2020-01-20 18:53:13 +0100

    Make the logic simpler.

--- src/utils/dielectric_function.F90
+++ src/utils/dielectric_function.F90
@@ -247,96 +247,43 @@ program dielectric_function
   select case(space%dim)
   case(1)
     write(header, '(7a15)') '#        energy', 'Re x', 'Im x'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(3e15.6)') ww,                                         &
-         real(invdielectric(1, kk), REAL_PRECISION), aimag(invdielectric(1, kk))
-    end do
   case(2)
     write(header, '(7a15)') '#        energy', 'Re x', 'Im x', 'Re y', 'Im y'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(5e15.6)') ww,                                         &
-         real(invdielectric(1, kk), REAL_PRECISION), aimag(invdielectric(1, kk)), &
-         real(invdielectric(2, kk), REAL_PRECISION), aimag(invdielectric(2, kk))
-    end do
   case(3)
     write(header, '(7a15)') '#        energy', 'Re x', 'Im x', 'Re y', 'Im y', 'Re z', 'Im z'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(7e15.6)') ww,                                         &
-         real(invdielectric(1, kk), REAL_PRECISION), aimag(invdielectric(1, kk)), &
-         real(invdielectric(2, kk), REAL_PRECISION), aimag(invdielectric(2, kk)), &
-         real(invdielectric(3, kk), REAL_PRECISION), aimag(invdielectric(3, kk))
+  end select
+
+
+  write(out_file,'(a)') trim(header)
+  do kk = 0, energy_steps
+    write(out_file, '(e15.6)', advance='no') kk*spectrum%energy_step
+    do idir = 1, space%dim-1
+      write(out_file, '(2e15.6)', advance='no') real(invdielectric(idir, kk), REAL_PRECISION), aimag(invdielectric(idir, kk))
     end do
-  end select 
+    write(out_file, '(2e15.6)', advance='no') real(invdielectric(space%dim, kk), REAL_PRECISION), aimag(invdielectric(space%dim, kk))
+  end do
   call io_close(out_file)
  
   out_file = io_open('td.general/dielectric_function', action='write')
-  select case(space%dim)
-  case(1)
-    write(header, '(7a15)') '#        energy', 'Re x', 'Im x'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(3e15.6)') ww,                                         &
-         real(dielectric(1, kk), REAL_PRECISION), aimag(dielectric(1, kk))
+  write(out_file,'(a)') trim(header)
+  do kk = 0, energy_steps
+    write(out_file, '(e15.6)', advance='no') kk*spectrum%energy_step
+    do idir = 1, space%dim-1
+      write(out_file, '(2e15.6)', advance='no') real(dielectric(idir, kk), REAL_PRECISION), aimag(dielectric(idir, kk))
     end do
-  case(2)
-    write(header, '(7a15)') '#        energy', 'Re x', 'Im x', 'Re y', 'Im y'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(5e15.6)') ww,                                         &
-         real(dielectric(1, kk), REAL_PRECISION), aimag(dielectric(1, kk)), &
-         real(dielectric(2, kk), REAL_PRECISION), aimag(dielectric(2, kk))
-    end do
-  case(3)
-    write(header, '(7a15)') '#        energy', 'Re x', 'Im x', 'Re y', 'Im y', 'Re z', 'Im z'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(7e15.6)') ww,                                         &
-         real(dielectric(1, kk), REAL_PRECISION), aimag(dielectric(1, kk)), &
-         real(dielectric(2, kk), REAL_PRECISION), aimag(dielectric(2, kk)), &
-         real(dielectric(3, kk), REAL_PRECISION), aimag(dielectric(3, kk))
-    end do
-  end select 
+    write(out_file, '(2e15.6)', advance='no') real(dielectric(space%dim, kk), REAL_PRECISION), aimag(dielectric(space%dim, kk))
+  end do
   call io_close(out_file)
 
   out_file = io_open('td.general/chi', action='write')
-  select case(space%dim)
-  case(1)
-    write(header, '(7a15)') '#        energy', 'Re x', 'Im x'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(3e15.6)') ww,                                         &
-         real(chi(1, kk), REAL_PRECISION), aimag(chi(1, kk))
+  write(out_file,'(a)') trim(header)
+  do kk = 0, energy_steps
+    write(out_file, '(e15.6)', advance='no') kk*spectrum%energy_step
+    do idir = 1, space%dim-1
+      write(out_file, '(2e15.6)', advance='no') real(chi(idir, kk), REAL_PRECISION), aimag(chi(idir, kk))
     end do
-  case(2)
-    write(header, '(7a15)') '#        energy', 'Re x', 'Im x', 'Re y', 'Im y'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(5e15.6)') ww,                                         &
-         real(chi(1, kk), REAL_PRECISION), aimag(chi(1, kk)), &
-         real(chi(2, kk), REAL_PRECISION), aimag(chi(2, kk))
-    end do
-  case(3)
-    write(header, '(7a15)') '#        energy', 'Re x', 'Im x', 'Re y', 'Im y', 'Re z', 'Im z'
-    write(out_file,'(a)') trim(header)
-    do kk = 0, energy_steps
-      ww = kk*spectrum%energy_step
-      write(out_file, '(7e15.6)') ww,                                         &
-         real(chi(1, kk), REAL_PRECISION), aimag(chi(1, kk)), &
-         real(chi(2, kk), REAL_PRECISION), aimag(chi(2, kk)), &
-         real(chi(3, kk), REAL_PRECISION), aimag(chi(3, kk))
-    end do
-  end select 
+    write(out_file, '(2e15.6)', advance='no') real(chi(space%dim, kk), REAL_PRECISION), aimag(chi(space%dim, kk))
+  end do
   call io_close(out_file)
 
   SAFE_DEALLOCATE_A(dielectric)
