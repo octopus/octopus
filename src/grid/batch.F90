6cd7fe1e63ba846275ad33502fc13a4e6d683a9f
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2020-02-05 09:02:23 +0100

    Update copyright notice in batch.F90

--- src/grid/batch.F90
+++ src/grid/batch.F90
@@ -1,4 +1,4 @@
-!! Copyright (C) 2008 X. Andrade
+!! Copyright (C) 2008 X. Andrade, 2020 S. Ohlmann
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
