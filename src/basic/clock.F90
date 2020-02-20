73f839aeb1d963a28ff4dcb79b4cdffe200293e1
Heiko Appel <heiko.appel@mpsd.mpg.de>
2020-02-20 13:51:38 +0100

    Add documentation for the clock

--- src/basic/clock.F90
+++ src/basic/clock.F90
@@ -29,7 +29,7 @@ module clock_oct_m
 
   private
   public ::                &
-    clock_t,    &
+    clock_t,               &
     operator(.eq.),        &
     operator(.lt.),        &
     operator(.gt.),        &
@@ -38,20 +38,21 @@ module clock_oct_m
 
   type clock_t
     private
-    integer :: clock_tick
-    integer :: granularity
-    FLOAT   :: time_step
-    type(namespace_t) :: namespace
+    integer :: clock_tick  !< internal clock counter which is incremented by one when the clock is advanced
+    integer :: granularity !< one clock tick corresponds to an advancement of 'granularity' steps on the finest scale in the system
+    FLOAT   :: time_step   !< physical simulation time increment which corresponds to a single clock tick
+    type(namespace_t) :: namespace !< namespace for labeling the clock
 
   contains
-    procedure :: print => clock_print
-    procedure :: set => clock_set
-    procedure :: get_tick => clock_get_tick
-    procedure :: get_sim_time => clock_get_sim_time
-    procedure :: increment => clock_increment
-    procedure :: decrement => clock_decrement
-    procedure :: reset => clock_reset
-    procedure :: is_later_with_step => clock_is_later_with_step
+    procedure :: print => clock_print                 !< print internal state of the clock
+    procedure :: set => clock_set                     !< set the clock to the state of a given input clock
+    procedure :: get_tick => clock_get_tick           !< get value of internal clock counter
+    procedure :: get_sim_time => clock_get_sim_time   !< get the current physical simulation time of the clock
+    procedure :: increment => clock_increment         !< increment the internal clock counter by one or several steps
+    procedure :: decrement => clock_decrement         !< decrement the internal clock counter by one or several steps
+    procedure :: reset => clock_reset                 !< set the internal clock counter back to zero
+    procedure :: is_later_with_step => clock_is_later_with_step !< compare two clocks and indicate if the first clock plus
+                                                                !! one step is later than the second clock
   end type clock_t
 
   interface clock_t
@@ -86,9 +87,20 @@ module clock_oct_m
 contains
 
   ! ---------------------------------------------------------
-  type(clock_t) function clock_init(namespace, time_step, smallest_algo_dt, initial_tick) result(this)
+  !> Initialize the clock with the given namespace and associated physical time step.
+  !! The internal clock counter starts at zero or if the optional argument initial_tick is given
+  !! at the value of initial_tick.
+  !! When several clocks are used simultaneously, then smallest_dt is the smallest time step of
+  !! all the clocks, or equivalently the time step of the fastest ticking clock. If only a single
+  !! clock is used or all clocks run with the same time step, then smallest_dt is identical to
+  !! time_step. 
+  !! All clocks are compared on the finest scale. The granularity tells how many steps on the finest 
+  !! scale an increase of the clock tick corresponds to. In other words, it tells in which 'units'
+  !! an increment of the internal clock counter is counting the time on the finest scale.
+  !! So far the implementation assumes that all clocks in the system have commensurable time steps.
+  type(clock_t) function clock_init(namespace, time_step, smallest_dt, initial_tick) result(this)
     type(namespace_t), intent(in) :: namespace
-    FLOAT,             intent(in) :: time_step, smallest_algo_dt
+    FLOAT,             intent(in) :: time_step, smallest_dt
     integer, optional             :: initial_tick
 
     PUSH_SUB(clock_init)
@@ -97,10 +109,10 @@ contains
     this%namespace = namespace
     this%clock_tick = optional_default(initial_tick, 0)
 
-    if (ceiling(time_step/smallest_algo_dt) == floor(time_step/smallest_algo_dt)) then
-      this%granularity = ceiling(time_step/smallest_algo_dt)
+    if (ceiling(time_step/smallest_dt) == floor(time_step/smallest_dt)) then
+      this%granularity = ceiling(time_step/smallest_dt)
     else
-      message(1) = 'Timesteps of the clocks are not comensurate.'
+      message(1) = 'Timesteps of the clocks are not commensurable.'
       message(2) = 'Please adapt the time steps of your subsystems to make them compatible.'
       call messages_fatal(2)
     endif
@@ -135,7 +147,7 @@ contains
 
   ! ---------------------------------------------------------
   subroutine clock_set(this, clock_in)
-    class(clock_t), intent(in) :: clock_in
+    class(clock_t), intent(in)    :: clock_in
     class(clock_t), intent(inout) :: this
     
     PUSH_SUB(clock_set)
@@ -172,7 +184,7 @@ contains
   ! ---------------------------------------------------------
   subroutine clock_increment(this, steps)
     class(clock_t), intent(inout) :: this
-    integer, optional,         intent(in)    :: steps
+    integer, optional, intent(in) :: steps
 
     PUSH_SUB(clock_increment)
 
@@ -184,7 +196,7 @@ contains
   ! ---------------------------------------------------------
   subroutine clock_decrement(this, steps)
     class(clock_t), intent(inout) :: this
-    integer, optional,         intent(in)    :: steps
+    integer, optional, intent(in) :: steps
 
     PUSH_SUB(clock_decrement)
 
@@ -260,6 +272,8 @@ contains
   end function clock_is_equal
 
   ! ---------------------------------------------------------
+  !> This function returns true if taking clock_a and adding one
+  !! time step is later in time than the current time of clock_b
   logical function clock_is_later_with_step(clock_a, clock_b) result(is_later_with_step)
     class(clock_t), intent(in) :: clock_a, clock_b
 
