85fd99f50dd415202e8e1406bab2eaa107259d9b
Heiko Appel <heiko.appel@mpsd.mpg.de>
2020-04-21 12:20:41 +0200

    Replace usage of abs for better performance.

--- src/system/xc_oep_sic_inc.F90
+++ src/system/xc_oep_sic_inc.F90
@@ -56,7 +56,7 @@ subroutine X(oep_sic) (xcs, gr, psolver, namespace, st, is, oep, ex, ec, exxop)
       call states_elec_get_state(st, gr%mesh, ist, is, psi)
 
       ! get orbital density
-      rho(1:gr%mesh%np, 1) = oep%socc*st%occ(ist, is)*abs(psi(1:gr%mesh%np, 1))**2
+      rho(1:gr%mesh%np, 1) = oep%socc*st%occ(ist, is)*psi(1:gr%mesh%np, 1)*R_CONJ(psi(1:gr%mesh%np, 1))
 
       ! initialize before calling get_vxc
       vxc = M_ZERO
@@ -77,7 +77,7 @@ subroutine X(oep_sic) (xcs, gr, psolver, namespace, st, is, oep, ex, ec, exxop)
 
       ! The exchange energy.
       ex_ = ex_ - M_HALF*oep%sfact*oep%socc*st%occ(ist, is)* &
-        dmf_dotp(gr%mesh, vxc(1:gr%mesh%np, 1), abs(psi(1:gr%mesh%np, 1))**2)
+        dmf_dotp(gr%mesh, vxc(1:gr%mesh%np, 1), psi(1:gr%mesh%np, 1)*R_CONJ(psi(1:gr%mesh%np, 1)))
 
       oep%X(lxc)(1:gr%mesh%np, ist, is) = oep%X(lxc)(1:gr%mesh%np, ist, is) - &
         vxc(1:gr%mesh%np, 1)*R_CONJ(psi(1:gr%mesh%np, 1))
