a21b61b547920bf13e2889e27001c68b4ab88c5a
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2020-10-14 17:07:00 +0200

    Include config.h and undef CC in cgal file

--- src/basic/cgal_polyhedra_low.cc
+++ src/basic/cgal_polyhedra_low.cc
@@ -17,6 +17,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301, USA.
 */
 
+#include <config.h>
+// undef CC here because that is used in CGAL...
+#undef CC
 
 #ifdef HAVE_CGAL
 #include <iostream>
