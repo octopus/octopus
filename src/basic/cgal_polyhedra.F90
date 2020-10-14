727f3179fd55f51d069421801aeae90fb96449b5
Heiko Appel <heiko.appel@mpsd.mpg.de>
2020-10-14 10:58:16 +0200

    Remove not needed functions from interface to CGAL library.

--- src/basic/cgal_polyhedra.F90
+++ src/basic/cgal_polyhedra.F90
@@ -8,9 +8,7 @@ module cgal_polyhedra_oct_m
   private
 
   public cgal_polyhedron_read, &
-         cgal_polyhedron_closest, &
          cgal_polyhedron_point_inside, &
-         cgal_polyhedron_bbox, &
          cgal_polyhedron_finalize
 
   type, bind(C) :: d3
@@ -21,56 +19,24 @@ module cgal_polyhedra_oct_m
 
     subroutine polyhedron_from_file(ptree, fname, verbose, ierr) bind(C,name="polyhedron_from_file")
       import
-      type(c_ptr),intent(out) :: ptree
-      character(kind=c_char),intent(in) :: fname(*)
-      integer(c_int),value :: verbose !avoid bool in C++, not equal to c_bool
-      integer(c_int),intent(out) :: ierr
-    end subroutine
-
-    subroutine polyhedron_closest(ptree, query, near) bind(C,name="polyhedron_closest")
-      import
-      type(c_ptr),value :: ptree
-      type(d3),intent(in) :: query
-      type(d3),intent(out) :: near
+      type(c_ptr), intent(out) :: ptree
+      character(kind=c_char), intent(in) :: fname(*)
+      integer(c_int), value :: verbose                 ! avoid bool in C++, not equal to c_bool
+      integer(c_int), intent(out) :: ierr
     end subroutine
 
     function polyhedron_point_inside(poly, vec) result(res) bind(C,name="polyhedron_point_inside")
       import
       logical(c_bool) :: res
-      type(c_ptr),value :: poly
+      type(c_ptr), value :: poly
       type(d3),intent(in) :: vec
     end function
 
-!    function polyhedron_inside(ptree, query, outside_ref) result(res) bind(C,name="polyhedron_inside")
-!      import
-!      logical(c_bool) :: res
-!      type(c_ptr),value :: ptree
-!      type(d3),intent(in) :: query, outside_ref
-!    end function
-
-!    function polyhedron_intersects_ray(ptree, origin, vec) result(res) bind(C,name="polyhedron_intersects_ray")
-!      import
-!      logical(c_bool) :: res
-!      type(c_ptr),value :: ptree
-!      type(d3),intent(in) :: origin, vec
-!    end function
-
-    subroutine polyhedron_bbox(ptree, min, max) bind(C,name="polyhedron_bbox")
-      import
-      type(c_ptr),value :: ptree
-      type(d3),intent(out) :: min, max
-    end subroutine
-
     subroutine polyhedron_finalize(ptree) bind(C,name="polyhedron_finalize")
       import
-      type(c_ptr),intent(inout) :: ptree
+      type(c_ptr), intent(inout) :: ptree
     end subroutine
 
-  end interface
-
-  interface cgal_polyhedron_closest
-    module procedure cgal_polyhedron_closest_s
-    module procedure cgal_polyhedron_closest_d
   end interface
 
   interface cgal_polyhedron_point_inside
@@ -78,22 +44,13 @@ module cgal_polyhedra_oct_m
     module procedure cgal_polyhedron_point_inside_d
   end interface
 
-!  interface cgal_polyhedron_intersects_ray
-!    module procedure cgal_polyhedron_intersects_ray_s
-!    module procedure cgal_polyhedron_intersects_ray_d
-!  end interface
-
-  interface cgal_polyhedron_bbox
-    module procedure cgal_polyhedron_bbox_s
-    module procedure cgal_polyhedron_bbox_d
-  end interface
 
 contains
 
-
+  ! ---------------------------------------------------------
   subroutine cgal_polyhedron_read(ptree, fname)
-    type(c_ptr),intent(out) :: ptree
-    character(*),intent(in) :: fname
+    type(c_ptr), intent(out) :: ptree
+    character(*), intent(in) :: fname
     integer(c_int) :: ierr
 
     call polyhedron_from_file(ptree, fname//c_null_char, 1, ierr)
@@ -107,109 +64,33 @@ contains
     end if
   end subroutine
 
-  subroutine cgal_polyhedron_closest_s(ptree, xq,yq,zq, xn,yn,zn)
-    type(c_ptr),intent(in) :: ptree
-    real(c_float),intent(in)  :: xq,yq,zq
-    real(c_float),intent(out) :: xn,yn,zn
-    type(d3) :: query, near
-
-    query = d3(real(xq,c_double),real(yq,c_double),real(zq,c_double))
-    call polyhedron_closest(ptree, query, near) 
-    xn = real(near%x,c_float)
-    yn = real(near%y,c_float)
-    zn = real(near%z,c_float)
-  end subroutine
-
-  subroutine cgal_polyhedron_closest_d(ptree, xq,yq,zq, xn,yn,zn)
-    type(c_ptr),intent(in) :: ptree
-    real(c_double),intent(in)  :: xq,yq,zq
-    real(c_double),intent(out) :: xn,yn,zn
-    type(d3) :: query, near
-
-    query = d3(xq,yq,zq)
-    call polyhedron_closest(ptree, query, near) 
-    xn = near%x
-    yn = near%y
-    zn = near%z
-  end subroutine
-
-  subroutine cgal_polyhedron_bbox_s(ptree, xmin,ymin,zmin, xmax,ymax,zmax)
-    type(c_ptr),intent(in) :: ptree
-    real(c_float),intent(out)  :: xmin,ymin,zmin
-    real(c_float),intent(out) :: xmax,ymax,zmax
-    type(d3) :: min, max
-
-    call polyhedron_bbox(ptree, min, max) 
-    xmin = real(min%x,c_float)
-    ymin = real(min%y,c_float)
-    zmin = real(min%z,c_float)
-    xmax = real(max%x,c_float)
-    ymax = real(max%y,c_float)
-    zmax = real(max%z,c_float)
-  end subroutine
-
-  subroutine cgal_polyhedron_bbox_d(ptree, xmin,ymin,zmin, xmax,ymax,zmax)
-    type(c_ptr),intent(in) :: ptree
-    real(c_double),intent(out)  :: xmin,ymin,zmin
-    real(c_double),intent(out) :: xmax,ymax,zmax
-    type(d3) :: min, max
-
-    call polyhedron_bbox(ptree, min, max) 
-    xmin = min%x
-    ymin = min%y
-    zmin = min%z
-    xmax = max%x
-    ymax = max%y
-    zmax = max%z
-  end subroutine
-
+  ! ---------------------------------------------------------
   function cgal_polyhedron_point_inside_s(poly, xq,yq,zq) result(res)
     logical :: res
-    type(c_ptr),intent(in) :: poly
-    real(c_float),intent(in) :: xq,yq,zq
+    type(c_ptr), intent(in) :: poly
+    real(c_float), intent(in) :: xq,yq,zq
     type(d3) :: query
 
     query = d3(real(xq,c_double),real(yq,c_double),real(zq,c_double))
     res = polyhedron_point_inside(poly, query)
   end function
 
+  ! ---------------------------------------------------------
   function cgal_polyhedron_point_inside_d(poly, xq,yq,zq) result(res)
     logical :: res
-    type(c_ptr),intent(in) :: poly
-    real(c_double),intent(in) :: xq,yq,zq
+    type(c_ptr), intent(in) :: poly
+    real(c_double), intent(in) :: xq,yq,zq
     type(d3) :: query
 
     query = d3(xq,yq,zq)
     res = polyhedron_point_inside(poly, query)
   end function
 
-!  function cgal_polyhedron_intersects_ray_s(ptree, xc,yc,zc, a,b,c) result(res)
-!    logical :: res
-!    type(c_ptr),intent(in) :: ptree
-!    real(c_float),intent(in) :: xc,yc,zc, a,b,c
-!    type(d3) :: origin, vec
-!
-!    origin = d3(real(xc,c_double),real(yc,c_double),real(zc,c_double))
-!    vec = d3(real(a,c_double),real(b,c_double),real(c,c_double))
-!    res = polyhedron_intersects_ray(ptree, origin, vec)
-!  end function
-
-!  function cgal_polyhedron_intersects_ray_d(ptree, xc,yc,zc, a,b,c) result(res)
-!    logical :: res
-!    type(c_ptr),intent(in) :: ptree
-!    real(c_double),intent(in) :: xc,yc,zc, a,b,c
-!    type(d3) :: origin, vec
-!
-!    origin = d3(xc,yc,zc)
-!    vec = d3(a,b,c)
-!    res = polyhedron_intersects_ray(ptree, origin, vec)
-!  end function
-
+  ! ---------------------------------------------------------
   subroutine cgal_polyhedron_finalize(ptree)
-    type(c_ptr),intent(inout) :: ptree
+    type(c_ptr), intent(inout) :: ptree
 
     call polyhedron_finalize(ptree)
   end subroutine
-
 
 end module cgal_polyhedra_oct_m
--- src/basic/cgal_polyhedra_low.cc
+++ src/basic/cgal_polyhedra_low.cc
@@ -24,8 +24,6 @@ typedef CGAL::Side_of_triangle_mesh<Polyhedron, K> Point_inside;
 typedef CGAL::Bbox_3 Bbox_3;
 
 typedef struct {double x,y,z;} d3;
-
-
 typedef struct {Polyhedron *poly; Tree *tree;} Polytree;
 
 using std::cout;
@@ -34,10 +32,10 @@ using std::endl;
 extern "C" int debuglevel;
 
 extern "C" {
- 
+
   void polyhedron_from_file (Polyhedron **poly, const char *fname, int verbose, int * const ierr){
     Polyhedron *polyhedron = new Polyhedron;
-    
+
     std::ifstream in(fname);
 
     if (verbose) {cout << " Reading file " << fname << " " << endl;}
@@ -49,34 +47,22 @@ extern "C" {
       *ierr = 2;
       return;
     }
-    
+
     if (verbose) {
       cout << " facets: " << polyhedron->size_of_facets() << endl;
       cout << " halfedges: " << polyhedron->size_of_halfedges() << endl;
       cout << " vertices: " << polyhedron->size_of_vertices() << endl;
     }
-    
+
     if (polyhedron->size_of_facets()==0 ||
         polyhedron->size_of_halfedges()==0 ||
         polyhedron->size_of_vertices()==0){
           *ierr = 1;
           return;
         };
-    
+
     *poly = polyhedron;
-    
     *ierr = 0;
-    
-  }
-  
-  void polyhedron_closest (const Polytree *ptree, const d3 *query, d3 *near){
-    Point query_point(query->x,query->y,query->z);
-    
-    Point closest = ptree->tree->closest_point(query_point);
-    
-    near->x = closest.x();
-    near->y = closest.y();
-    near->z = closest.z();
   }
 
   bool polyhedron_point_inside(Polyhedron *polyhedron, Point *query) {
@@ -90,16 +76,10 @@ extern "C" {
     return inside_tester(*query) == CGAL::ON_BOUNDED_SIDE || inside_tester(*query) == CGAL::ON_BOUNDARY;
   }
 
-  void polyhedron_bbox(const Polytree *ptree, d3 *const min, d3 *const max){
-    Bbox_3 bbox = ptree->tree->bbox();
-    *min = {bbox.xmin(), bbox.ymin(), bbox.zmin()};
-    *max = {bbox.xmax(), bbox.ymax(), bbox.zmax()};
-  }
-  
   void polyhedron_finalize(Polytree **pptree){
     delete (*pptree)->tree; (*pptree)->tree = NULL;
     delete (*pptree)->poly; (*pptree)->poly = NULL;
     delete *pptree; *pptree = NULL;
   }
- 
+
 }
