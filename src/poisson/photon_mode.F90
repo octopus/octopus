d69318fb54d146e6ebbb50f7aa8038c5c4719e03
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2020-11-17 17:18:51 +0100

    Add author line

--- src/poisson/photon_mode.F90
+++ src/poisson/photon_mode.F90
@@ -1,5 +1,6 @@
 !! Copyright (C) 2017 Johannes Flick
 !! Copyright (C) 2019 F. Buchholz, M. Oliveira
+!! Copyright (C) 2020 S. Ohlmann
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
