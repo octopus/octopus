ba1a66999409c91c997faadd393097e7eb4029cd
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2021-01-22 13:37:37 +0100

    Changed type components from pointers to allocatables in mesh_cube_parallel_map_t.

--- src/grid/mesh_cube_parallel_map.F90
+++ src/grid/mesh_cube_parallel_map.F90
@@ -46,10 +46,10 @@ module mesh_cube_parallel_map_oct_m
     type(partition_transfer_t) :: m2c
     integer :: m2c_nsend !< How many points will this process send to the cube partition
     integer :: m2c_nrec  !< How many points will this process receive from the mesh partition
-    integer, pointer :: m2c_mf_order(:)   !< How the points of the mesh function should be ordered to execute 
+    integer, allocatable :: m2c_mf_order(:)   !< How the points of the mesh function should be ordered to execute 
                                           !! the transfer from the mesh partition to the cube partition. 
                                           !! These are *local* indices.
-    integer, pointer :: m2c_cf_order(:,:) !< How the points of the mesh function are ordered after executing 
+    integer, allocatable :: m2c_cf_order(:,:) !< How the points of the mesh function are ordered after executing 
                                           !! the transfer from the mesh partition to the cube partition with 
                                           !! respect to the *local* cube indices.
 
@@ -57,10 +57,10 @@ module mesh_cube_parallel_map_oct_m
     type(partition_transfer_t) :: c2m
     integer :: c2m_nsend !< How many points will this process send to the mesh partition
     integer :: c2m_nrec  !< How many points will this process receive from the cube partition
-    integer, pointer :: c2m_cf_order(:,:) !< How the points of the mesh function should be ordered to execute
+    integer, allocatable :: c2m_cf_order(:,:) !< How the points of the mesh function should be ordered to execute
                                           !! the transfer from the cube partition to the mesh partition with 
                                           !! respect to the *local* cube indices.
-    integer, pointer :: c2m_mf_order(:)   !< How the points of the mesh function are ordered after executing 
+    integer, allocatable :: c2m_mf_order(:)   !< How the points of the mesh function are ordered after executing 
                                           !! the transfer from the cube partition to the mesh partition. 
                                           !! These are *local* indices.
   end type mesh_cube_parallel_map_t
@@ -239,10 +239,10 @@ contains
 
     PUSH_SUB(mesh_cube_parallel_map_end)
 
-    SAFE_DEALLOCATE_P(this%m2c_mf_order)
-    SAFE_DEALLOCATE_P(this%m2c_cf_order)
-    SAFE_DEALLOCATE_P(this%c2m_mf_order)
-    SAFE_DEALLOCATE_P(this%c2m_cf_order)
+    SAFE_DEALLOCATE_A(this%m2c_mf_order)
+    SAFE_DEALLOCATE_A(this%m2c_cf_order)
+    SAFE_DEALLOCATE_A(this%c2m_mf_order)
+    SAFE_DEALLOCATE_A(this%c2m_cf_order)
     call partition_transfer_end(this%m2c)
     call partition_transfer_end(this%c2m)
 
