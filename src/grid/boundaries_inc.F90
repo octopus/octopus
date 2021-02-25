360cedf7d4594d2927943384e7ed7a7626c99777
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2021-02-25 09:07:20 +0100

    Adapt some copyright notices

--- src/grid/boundaries_inc.F90
+++ src/grid/boundaries_inc.F90
@@ -1,4 +1,5 @@
 !! Copyright (C) 2005-2020 Florian Lorenzen, Heiko Appel, Martin Lueders
+!! Copyright (C) 2021 Sebastian Ohlmann
 !!
 !! This program is free software; you can redistribute it and/or modify
 !! it under the terms of the GNU General Public License as published by
