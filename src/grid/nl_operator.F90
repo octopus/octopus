10bf5746626fb08a44c2e543caa039d5d812c4ec
Sebastian Ohlmann <sebastian.ohlmann@mpcdf.mpg.de>
2021-03-04 17:38:27 +0100

    Remove experimental nomap kernel
    This kernel is experimental and not widely used. However, it touches the
    internals of the mesh (lxyz_inv) that will change in the future.

--- src/grid/nl_operator.F90
+++ src/grid/nl_operator.F90
@@ -233,8 +233,6 @@ contains
       !% The standard implementation ported to OpenCL.
       !%Option map 2
       !% A different version, more suitable for GPUs.
-      !%Option nomap 3
-      !% (Experimental) This version does not use a map.
       !%End
       call parse_variable(namespace, 'OperateAccel',  OP_MAP, function_opencl)
 
@@ -566,8 +564,6 @@ contains
         call accel_kernel_build(op%kernel, 'operate.cl', 'operate', flags)
       case(OP_MAP)
         call accel_kernel_build(op%kernel, 'operate.cl', 'operate_map', flags)
-      case(OP_NOMAP)
-        call accel_kernel_build(op%kernel, 'operate.cl', 'operate_nomap', flags)
       end select
 
       call accel_create_buffer(op%buff_ri, ACCEL_MEM_READ_ONLY, TYPE_INTEGER, op%nri*op%stencil%size)
@@ -620,49 +616,6 @@ contains
           SAFE_DEALLOCATE_A(all_points)
   
         end if
-                
-      case(OP_NOMAP)
-
-        ASSERT(op%mesh%sb%dim == 3)
-        ASSERT(.not. op%mesh%parallel_in_domains)
-
-        call accel_create_buffer(op%buff_map, ACCEL_MEM_READ_ONLY, TYPE_INTEGER, pad(op%mesh%np, accel_max_workgroup_size()))
-        call accel_write_buffer(op%buff_map, op%mesh%np, (op%rimap - 1)*op%stencil%size)
-        
-        SAFE_ALLOCATE(stencil(1:op%mesh%sb%dim, 1:op%stencil%size + 1))
-
-        stencil(1:op%mesh%sb%dim, 1:op%stencil%size) = op%stencil%points(1:op%mesh%sb%dim, 1:op%stencil%size)
-
-        stencil(1, op%stencil%size + 1) = 1
-        stencil(2, op%stencil%size + 1) = mesh%idx%nr(2, 1) - mesh%idx%nr(1, 1) + 1
-        stencil(3, op%stencil%size + 1) = stencil(2, op%stencil%size + 1)*(mesh%idx%nr(2, 2) - mesh%idx%nr(1, 2) + 1)
-
-        call accel_create_buffer(op%buff_stencil, ACCEL_MEM_READ_ONLY, TYPE_INTEGER, op%mesh%sb%dim*(op%stencil%size + 1))
-        call accel_write_buffer(op%buff_stencil, op%mesh%sb%dim*(op%stencil%size + 1), stencil)
-        
-        SAFE_DEALLOCATE_A(stencil)
-
-        size = product(mesh%idx%nr(2, 1:op%mesh%sb%dim) - mesh%idx%nr(1, 1:op%mesh%sb%dim) + 1)
-
-        call accel_create_buffer(op%buff_xyz_to_ip, ACCEL_MEM_READ_ONLY, TYPE_INTEGER, size)
-        call accel_write_buffer(op%buff_xyz_to_ip, size, op%mesh%idx%lxyz_inv - 1)
-
-        SAFE_ALLOCATE(stencil(1:op%mesh%sb%dim, 1:mesh%np_part))
-
-        do ip = 1, mesh%np_part
-          call mesh_local_index_to_coords(op%mesh, ip, idx)
-          do jj = 1, op%mesh%sb%dim
-            stencil(jj, ip) = idx(jj) - mesh%idx%nr(1, jj)
-          end do
-        end do
-
-        ASSERT(minval(stencil) == 0)
-
-        call accel_create_buffer(op%buff_ip_to_xyz, ACCEL_MEM_READ_ONLY, TYPE_INTEGER, op%mesh%np_part*op%mesh%sb%dim)
-        call accel_write_buffer(op%buff_ip_to_xyz, op%mesh%np_part*op%mesh%sb%dim, stencil)
-
-        SAFE_DEALLOCATE_A(stencil)
-
       end select
     end if
 
